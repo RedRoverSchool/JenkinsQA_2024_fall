cc529eb32bfd0e36a02a04b9c9581ab98871e607
pakhomova_ev <pakhomova.e@gmail.com>
2024-10-16 20:00:10 +0300

    add test log in Saucedemo.com

---
+++ src/test/java/PakhomovaElenaTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PakhomovaElenaTest {
+    @Test
+    public void testLogInStandardUser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        String textLogoPage = driver.findElement(By.cssSelector("span[data-test='title']")).getText();
+
+        Assert.assertEquals(textLogoPage, "Products");
+
+        driver.quit();
+    }
+
+}
