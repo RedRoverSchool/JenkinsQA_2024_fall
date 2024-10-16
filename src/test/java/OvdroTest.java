c6cbb9128b4b10043a81fb9e0008f2e14cc5b08c
ovdro <ovdro.ovdro@gmail.com>
2024-10-16 14:42:01 +0300

    WIP assert check

--- src/test/java/OvdroTest.java
+++ src/test/java/OvdroTest.java
@@ -1,8 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -10,7 +15,7 @@ import java.time.Duration;
 public class OvdroTest {
 
     @Test
-    public void testAutomationToolsForm() {
+    public void testAutomationToolsFormRegistrationValidFields() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-search-engine-choice-screen");
         WebDriver driver = new ChromeDriver(options);
@@ -19,9 +24,38 @@ public class OvdroTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
 
+        String firstNameField = "//*[@id=\"firstName\"]";
+        String lastNameField = "//*[@id=\"lastName\"]";
+        String userEmailField = "//*[@id=\"userEmail\"]";
+        String genderRadiobutton = "//*[@id=\"gender-radio-1\"]";
+        String mobileNumberField = "//*[@id=\"userNumber\"]";
+        String mobileNumber = "1234567890";
+        String submitButton = "//*[@id=\"submit\"]";
+        String closeButton = "//*[@id=\"closeLargeModal\"]";
+        // String closeButtonText = "class org.openqa.selenium.remote.RemoteWebElement";
+
+
+        String firstName = "FirstName";
+        String lastName = "LastName";
+        String email = "FirstNameLastName@test.test";
+
+        driver.findElement(By.xpath(firstNameField)).sendKeys(firstName);
+        driver.findElement(By.xpath(lastNameField)).sendKeys(lastName);
+        driver.findElement(By.xpath(userEmailField)).sendKeys(email);
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.xpath(genderRadiobutton))).click().perform();
+
+        driver.findElement(By.xpath(mobileNumberField)).sendKeys(mobileNumber);
+        actions.moveToElement(driver.findElement(By.xpath(submitButton))).click().perform();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement closeModalButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(closeButton)));
+        Assert.assertTrue(closeModalButton.isDisplayed(), "true");
+
+        closeModalButton.click();
 
         driver.quit();
-
 
     }
 
