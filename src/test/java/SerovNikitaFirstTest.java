b622c51ab2024a608fa6dc31960608c50ce071f8
NikitaS-95 <nserov95@gmail.com>
2024-10-17 09:56:47 +0500

    Add checkbox test

--- src/test/java/SerovNikitaFirstTest.java
+++ src/test/java/SerovNikitaFirstTest.java
@@ -3,14 +3,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SerovNikitaFirstTest {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setDriver() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
 
     @Test
     public void testRemove() {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
 
         WebElement addElement = driver.findElement(By.xpath("//*[@id=\"content\"]/div/button"));
@@ -20,5 +32,18 @@ public class SerovNikitaFirstTest {
                 .getText(), "Delete");
 
         driver.findElement(By.xpath("//*[@id=\"elements\"]/button")).click();
+    }
+    @Test
+    public void selectCheckboxes() {
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+        WebElement checkbox1 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]"));
+
+        checkbox2.isSelected();
+
+        checkbox1.click();
+
+        checkbox1.isSelected();
     }
 }
