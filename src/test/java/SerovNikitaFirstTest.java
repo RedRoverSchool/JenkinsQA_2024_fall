7d133b8aa903f3d07f9671eb22d496118175a253
NikitaS-95 <nserov95@gmail.com>
2024-10-17 21:12:19 +0500

    right click + drug and drop tests

--- src/test/java/SerovNikitaFirstTest.java
+++ src/test/java/SerovNikitaFirstTest.java
@@ -1,7 +1,6 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -22,7 +21,7 @@ public class SerovNikitaFirstTest {
     }
 
     @Test
-    public void testRemove() {
+    public void testRemoveTest() {
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
 
         WebElement addElement = driver.findElement(By.xpath("//*[@id=\"content\"]/div/button"));
@@ -34,7 +33,7 @@ public class SerovNikitaFirstTest {
         driver.findElement(By.xpath("//*[@id=\"elements\"]/button")).click();
     }
     @Test
-    public void selectCheckboxes() {
+    public void selectCheckboxesTest() {
         driver.get("https://the-internet.herokuapp.com/checkboxes");
 
         WebElement checkbox1 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
@@ -45,5 +44,31 @@ public class SerovNikitaFirstTest {
         checkbox1.click();
 
         checkbox1.isSelected();
+    }
+    @Test
+    public void contextMenuTest() {
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+
+        Actions action = new Actions(driver);
+
+        WebElement contextItem = driver.findElement(By.id("hot-spot"));
+        action.contextClick(contextItem).build().perform();
+
+        Alert displayMessage = driver.switchTo().alert();
+
+        Assert.assertEquals(displayMessage.getText(),"You selected a context menu");
+    }
+
+    @Test
+    public void drugAndDropTest() {
+        driver.get("https://the-internet.herokuapp.com/drag_and_drop");
+
+        WebElement elementA = driver.findElement(By.id("column-a"));
+        WebElement elementB = driver.findElement(By.id("column-b"));
+
+        Actions action = new Actions(driver);
+        action.dragAndDrop(elementA,elementB).build().perform();
+
+        Assert.assertEquals(elementA.getText(), "B");
     }
 }
