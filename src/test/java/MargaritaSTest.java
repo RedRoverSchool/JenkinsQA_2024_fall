0c42e523e4a6c178b882a2650292418c25ffd415
MapGary <eresskigal@gmail.com>
2024-10-17 15:06:39 +0200

    Rita's commit

--- src/test/java/MargaritaSTest.java
+++ src/test/java/MargaritaSTest.java
@@ -2,39 +2,57 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+
 public class MargaritaSTest {
 
-        @Test
-        public void test() throws InterruptedException {
+    @Test
+    public void test() throws InterruptedException {
 
-            WebDriver driver = new ChromeDriver();
-//        WebDriver driver = new FirefoxDriver();
-//        WebDriver driver = new EdgeDriver();
+        WebDriver driver = new ChromeDriver();
+////        WebDriver driver = new FirefoxDriver();
+////        WebDriver driver = new EdgeDriver();
 
-            driver.get("https://ru.pinterest.com/");
-            Thread.sleep(15000);
-            WebElement show = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[1]/div/div[1]/div[2]/a/div/div/span"));
 
-            Thread.sleep(5000);
+        driver.get("https://practice-automation.com/form-fields/");
+        Thread.sleep(3000);
 
-//        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        WebElement name = driver.findElement(By.id("name-input"));
+        name.sendKeys("Test");
+        Thread.sleep(3000);
 
-            show.click();
-            Thread.sleep(5000);
+        WebElement drink1 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[8]"));
+        WebElement drink2 = driver.findElement(By.xpath("//label[text()='Water']"));
+        WebElement drink3 = driver.findElement(By.id("drink3"));
+        drink1.click();
+        drink2.click();
+        drink3.click();
+        Thread.sleep(3000);
 
-            WebElement searchBox = driver.findElement(By.cssSelector("#searchBoxContainer > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > input:nth-child(2)"));
+        WebElement color = driver.findElement(By.cssSelector("#color2"));
+        color.click();
+        Thread.sleep(3000);
 
-            searchBox.sendKeys("Selenium");
-            searchBox.sendKeys(Keys.RETURN);
+        WebElement list = driver.findElement(By.id("automation"));
+        list.click();
+        WebElement yes = driver.findElement(By.cssSelector("#automation > option:nth-child(2)"));
+        yes.click();
+        Thread.sleep(3000);
 
-            Thread.sleep(10000);
+        WebElement tools = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div/main/div/article/div/form/ul/li[1]"));
+        System.out.println(tools.getText());
 
-            WebElement pin = driver.findElement(By.xpath("//div[text()='THE MINERAL SELENIUM.']"));
-            pin.click();
-//        message.getText();
-            Thread.sleep(5000);
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("test@test.com");
+        Thread.sleep(3000);
 
-            driver.quit();
-        }
+        WebElement message = driver.findElement(By.cssSelector("#message"));
+        message.sendKeys("test message");
+        Thread.sleep(3000);
 
+        WebElement button = driver.findElement(By.id("submit-btn"));
+        button.click();
+
+        driver.quit();
+
+    }
 }
