3c646b38ff1eb5103e22defeb00642219235356e
vivanishak <92497475+vivanishak@users.noreply.github.com>
2024-10-16 23:01:19 -0400

    Add login test. (#18)

---
+++ src/test/java/VitaliiITest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class VitaliiITest {
+
+    @Test
+    public void testLogin() {
+
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement login = driver.findElement(By.id("submit"));
+
+        username.sendKeys("student");
+        password.sendKeys("Password123");
+        login.click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("practicetestautomation.com/logged-in-successfully/"));
+
+        driver.quit();
+        }
+}
