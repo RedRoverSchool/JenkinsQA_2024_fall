fee4fc504ef70ac8c1eb3bb791991421a64cf418
JulieVPetrova <m.smn103@gmail.com>
2024-10-18 17:17:46 +0300

    added new test

--- src/test/java/JulieVPTest.java
+++ src/test/java/JulieVPTest.java
@@ -19,7 +19,7 @@ public class JulieVPTest {
         WebElement submitButton = driver.findElement(By.xpath("//*[@id='container-1e52aa8d39']/div/div[2]/div/div/div[2]/div/div[2]/div/nav/ul/li[1]/button"));
         submitButton.click();
 
-        Thread.sleep(500);
+        Thread.sleep(1000);
 
         driver.quit();
 
