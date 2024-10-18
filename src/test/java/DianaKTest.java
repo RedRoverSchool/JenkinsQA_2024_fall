2229d40623c4703e4986d1239fd849c66142e75e
Diana <krukava.qamanual@gmail.com>
2024-10-18 17:11:24 +0300

    change tests

--- src/test/java/DianaKTest.java
+++ src/test/java/DianaKTest.java
@@ -5,65 +5,53 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class DianaKTest {
+    WebDriver driver = new ChromeDriver();
     @Test
-    public void testGarfild() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    public void testGarfild() {
+
         driver.get("https://garfield.by/?utm_source=google&utm_medium=cpc&utm_campaign=17619287198&utm_content=&utm_term=&gad_source=1&gclid=CjwKCAjw9p24BhB_EiwA8ID5BlycoY_gj8tdIObtHyJdlX0EleOFbNVrF1vokbRJm7b6uhC1n2gh9hoCTCwQAvD_BwE");
+        driver.findElement(By.className("icon-open")).click();
 
-        WebElement menu = driver.findElement(By.className("icon-open"));
-        menu.click();
-        Thread.sleep(500);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.findElement(By.partialLinkText("Для щенков")).click();
 
-        WebElement puppies = driver.findElement(By.partialLinkText("Для щенков"));
-        puppies.click();
+        driver.findElement(By.partialLinkText("Сухие корма")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
-        WebElement food = driver.findElement(By.partialLinkText("Сухие корма"));
-        food.click();
-        Thread.sleep(500);
-
-        WebElement grandorf = driver.findElement(By.xpath("//div[@class='manufacturer__default']//p[@class='catalog-filter-content-checkbox__label'][normalize-space()='Grandorf']"));
-        grandorf.click();
-        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@class='manufacturer__default']//p[@class='catalog-filter-content-checkbox__label'][normalize-space()='Grandorf']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         WebElement filter = driver.findElement(By.id("line_arrFilterCat_124_3205278765"));
         Assert.assertEquals(filter.getText(), "Grandorf");
-
         driver.quit();
     }
 
 
     @Test
-    public void testRunin() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    public void testRunin() {
         driver.get("https://runin.by/");
-
-        WebElement find = driver.findElement(By.xpath("//*[@id=\"sub-header\"]/div[2]/div/div/a[1]"));
-        find.click();
+        driver.findElement(By.xpath("//*[@id=\"sub-header\"]/div[2]/div/div/a[1]")).click();
 
         WebElement search = driver.findElement(By.name("search"));
         search.click();
         search.sendKeys("Минский полумарафон");
-        Thread.sleep(500);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
-        WebElement checkbox = driver.findElement(By.name("allSeasons"));
-        checkbox.click();
-        Thread.sleep(2000);
+        driver.findElement(By.name("allSeasons")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
-        WebElement param = driver.findElement(By.xpath("//*[@id=\"app\"]/main/section/div/div/div[1]/div[1]/div[1]/div[3]/div/div[2]/div[2]/div/div/div/div[5]/div/div[1]/a/div/div[1]"));
-        param.click();
-        Thread.sleep(500);
+        driver.findElement(By.xpath("//*[@id=\"app\"]/main/section/div/div/div[1]/div[1]/div[1]/div[3]/div/div[2]/div[2]/div/div/div/div[5]/div/div[1]/a/div/div[1]")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
-        WebElement cookie = driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div/div[2]/div/div/button"));
-        cookie.click();
-        Thread.sleep(1500);
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div/div[2]/div/div/button")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
-        WebElement button = driver.findElement(By.xpath("//div[@class='event-intro__actions']//button[@type='button']"));
-        button.click();
-
-        WebElement result = driver.findElement(By.xpath("//*[@id=\"app\"]/main/div[8]/div[2]/div/div[2]/div/table/tbody/tr[1]/td[4]/div/a\n"));
-        result.click();
-        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@class='event-intro__actions']//button[@type='button']")).click();
+        driver.findElement(By.xpath("//*[@id=\"app\"]/main/div[8]/div[2]/div/div[2]/div/table/tbody/tr[1]/td[4]/div/a\n")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         WebElement distance21 = driver.findElement(By.partialLinkText("Результаты"));
         Assert.assertEquals(distance21.getText(),"Результаты");
@@ -74,27 +62,20 @@ public class DianaKTest {
 
 
     @Test
-    public void testKufar() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    public void testKufar() {
+
         driver.get("https://re.kufar.by/");
-
-        WebElement cookie = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[3]/div/div/div/div/button"));
-        cookie.click();
-
-        WebElement appart = driver.findElement(By.id("let_long"));
-        appart.click();
-
-        WebElement money = driver.findElement(By.xpath("//button[contains(text(),'Цена за месяц')]"));
-        money.click();
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[3]/div/div/div/div/button")).click();
+        driver.findElement(By.id("let_long")).click();
+        driver.findElement(By.xpath("//button[contains(text(),'Цена за месяц')]")).click();
 
         WebElement moneymax = driver.findElement(By.xpath("//input[@id='prc.upper']"));
         moneymax.click();
-        money.sendKeys("500");
-        Thread.sleep(1000);
+        moneymax.sendKeys("500");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
-        WebElement search = driver.findElement(By.xpath("//button[@type='submit']"));
-        search.click();
-        Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         WebElement name = driver.findElement(By.xpath("//h1[contains(text(),'Аренда квартир на длительный срок в Минске')]"));
         Assert.assertEquals(name.getText(),"Аренда квартир на длительный срок в Минске");
