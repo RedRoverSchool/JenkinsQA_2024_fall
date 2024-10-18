a73a11c5f083665c75318a7e525ef67774a7fcaf
Ihor Voloshyn <testbox.ua@gmail.com>
2024-10-18 23:41:50 +0200

    main google page tests added

---
+++ src/test/java/IhorVoloshynTest.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class IhorVoloshynTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    private void initDriver() throws InterruptedException {
+        this.driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(1000);
+    }
+
+    @AfterMethod
+    private void quitDriver() {
+        driver.quit();
+    }
+
+    @Test
+    public void testSearchBoxIsPresent() {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        Assert.assertTrue(searchBox.isDisplayed(), "Search box should be displayed");
+    }
+
+    @Test
+    public void testGoogleLogoIsDisplayed() {
+        WebElement googleLogo = driver.findElement(By.xpath("//img[@alt='Google']"));
+        Assert.assertTrue(googleLogo.isDisplayed(), "Google logo should be displayed");
+    }
+
+}
