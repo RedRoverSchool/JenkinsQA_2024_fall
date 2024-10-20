745883b26841c7f33d0d0fc82f3b6914c45b7e54
OlesyaNay <110473421+OlesyaNay@users.noreply.github.com>
2024-10-20 09:07:10 -0300

    add new class OlesyaNayTest and add new TestRatatype

---
+++ src/test/java/OlesyaNayTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class OlesyaNayTest {
+
+    WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("incognito", "start-maximized"));
+
+    @Test
+    public void TestRatatype() {
+        driver.get("https://www.ratatype.com/");
+        driver.findElement(By.xpath("//*[text()='Test your speed']")).click();
+        WebElement title = driver.findElement(By.xpath("//*[@class='h2']"));
+
+        Assert.assertEquals(title.getText(), "Typing Certification Test");
+    }
+
+}
