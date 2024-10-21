6d044d5b572fd24f1b0d9bd85548cbb7f35110cf
Diana <krukava.qamanual@gmail.com>
2024-10-21 12:34:34 +0300

    change tests

--- src/test/java/DianaKTest.java
+++ src/test/java/DianaKTest.java
@@ -3,30 +3,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class DianaKTest {
+
     WebDriver driver = new ChromeDriver();
+
+    @BeforeTest
+    public void avto(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterMethod
+    public void clouse(){
+        driver.quit();
+    }
+
+
     @Test
     public void testGarfild() {
 
         driver.get("https://garfield.by/?utm_source=google&utm_medium=cpc&utm_campaign=17619287198&utm_content=&utm_term=&gad_source=1&gclid=CjwKCAjw9p24BhB_EiwA8ID5BlycoY_gj8tdIObtHyJdlX0EleOFbNVrF1vokbRJm7b6uhC1n2gh9hoCTCwQAvD_BwE");
         driver.findElement(By.className("icon-open")).click();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.findElement(By.partialLinkText("Для щенков")).click();
-
         driver.findElement(By.partialLinkText("Сухие корма")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-
         driver.findElement(By.xpath("//div[@class='manufacturer__default']//p[@class='catalog-filter-content-checkbox__label'][normalize-space()='Grandorf']")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         WebElement filter = driver.findElement(By.id("line_arrFilterCat_124_3205278765"));
         Assert.assertEquals(filter.getText(), "Grandorf");
-        driver.quit();
     }
 
 
@@ -38,25 +47,17 @@ public class DianaKTest {
         WebElement search = driver.findElement(By.name("search"));
         search.click();
         search.sendKeys("Минский полумарафон");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         driver.findElement(By.name("allSeasons")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-
         driver.findElement(By.xpath("//*[@id=\"app\"]/main/section/div/div/div[1]/div[1]/div[1]/div[3]/div/div[2]/div[2]/div/div/div/div[5]/div/div[1]/a/div/div[1]")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[5]/div/div[2]/div/div/button")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         driver.findElement(By.xpath("//div[@class='event-intro__actions']//button[@type='button']")).click();
         driver.findElement(By.xpath("//*[@id=\"app\"]/main/div[8]/div[2]/div/div[2]/div/table/tbody/tr[1]/td[4]/div/a\n")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         WebElement distance21 = driver.findElement(By.partialLinkText("Результаты"));
         Assert.assertEquals(distance21.getText(),"Результаты");
-
-        driver.quit();
     }
 
 
@@ -72,14 +73,9 @@ public class DianaKTest {
         WebElement moneymax = driver.findElement(By.xpath("//input[@id='prc.upper']"));
         moneymax.click();
         moneymax.sendKeys("500");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-
         WebElement name = driver.findElement(By.xpath("//h1[contains(text(),'Аренда квартир на длительный срок в Минске')]"));
         Assert.assertEquals(name.getText(),"Аренда квартир на длительный срок в Минске");
-
-        driver.quit();
     }
 }
