9bc3422a62cfc6b2c108591e63a2e60d0af21ebc
IrinaLTest <lyuartem2004@gmail.com>
2024-10-22 14:15:20 -0400

    Add new test

---
+++ src/test/java/IrinaLTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class IrinaLTest {
+    @Test
+    public void testAutomation() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.automationexercise.com/");
+        WebElement Button1 = driver.findElement(By.xpath("/html/body/section[2]/div/div/div[2]/div[1]/div[2]/div/div[2]/ul/li/a"));
+        Button1.click();
+
+        WebElement Button = driver.findElement(By.xpath("//section/div/div/div[2]/div[2]/div[2]/div/span/button"));
+        Button.click();
+
+        Thread.sleep(1000);
+
+        WebElement itemTitle = driver.findElement(By.xpath("//section/div/div/div[2]/div[1]/div/div/div[1]/h4"));
+        Assert.assertEquals(itemTitle.getText(), "Added!");
+
+        driver.quit();
+
+    }
+}
