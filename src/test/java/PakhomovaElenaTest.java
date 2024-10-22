5eb456096a2a147590d86baa5fb8d7ea3cc610fa
pakhomova_ev <pakhomova.e@gmail.com>
2024-10-22 20:38:17 +0300

    add full test

--- src/test/java/PakhomovaElenaTest.java
+++ src/test/java/PakhomovaElenaTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,10 +12,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.List;
+
 
 public class PakhomovaElenaTest {
-    WebDriver driver;
+    private WebDriver driver;
 
     @BeforeMethod
     public void setDriver() {
@@ -225,5 +226,30 @@ public class PakhomovaElenaTest {
         Assert.assertTrue(selectDropDown1.isSelected());
 
         }
+
+        @Test
+        public void testMouseActionDropDown() throws InterruptedException {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/dropdown-menu.html");
+
+        Actions actions = new Actions(driver);
+
+        WebElement dropDown2 = driver.findElement(By.id("my-dropdown-2"));
+        WebElement contextMenu2 = driver.findElement(By.id("context-menu-2"));
+        actions.contextClick(dropDown2).perform();
+        Assert.assertTrue(contextMenu2.isDisplayed());
+
+        WebElement dropDown3 = driver.findElement(By.id("my-dropdown-3"));
+        WebElement contextMenu3 = driver.findElement(By.id("context-menu-3"));
+        actions.doubleClick(dropDown3).perform();
+        Assert.assertTrue(contextMenu3.isDisplayed());
+
+        WebElement dropDown1 = driver.findElement(By.id("my-dropdown-1"));
+        actions.click(dropDown1).perform();
+
+        WebElement contextMenu1 = driver.findElement(By.xpath("//ul[@class='dropdown-menu show']"));
+        Assert.assertTrue(contextMenu1.isDisplayed());
+        }
+
+
 
 }
