6a7ff6ea605d0b389cd9d4ca28672754de2b9d3d
Julia Marchenko <yuliamarche@gmail.com>
2024-10-22 18:48:44 +0300

    Update test demoqa form

--- src/test/java/JuliaMSTest.java
+++ src/test/java/JuliaMSTest.java
@@ -1,27 +1,15 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.util.List;
 
 public class JuliaMSTest {
 
     @Test
-    void fillShortFormTest() {
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://demoqa.com/text-box");
-        driver.findElement(By.id("userName")).sendKeys("Julia");
-        driver.findElement(By.id("userEmail")).sendKeys("julia@mail.com");
-        driver.findElement(By.id("currentAddress")).sendKeys("12 Warwickshire Mansions");
-        driver.findElement(By.id("permanentAddress")).sendKeys("12 Warwickshire Mansions");
-        driver.findElement(By.id("submit")).click();
-
-        driver.quit();
-    }
-
-    @Test
-    void fillLongFormTest() throws InterruptedException {
+    public void testLongForm() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
         WebDriver driver = new ChromeDriver(chromeOptions);
@@ -34,14 +22,16 @@ public class JuliaMSTest {
         driver.findElement(By.xpath ("//*[contains(text(),'Female')]")).click();
         driver.findElement(By.id("userNumber")).sendKeys("78479857847");
         driver.findElement(By.id("dateOfBirthInput")).click();
-        driver.findElement(By.className("react-datepicker__month-select").xpath("//*[contains(text(),'June')]")).click();
-        driver.findElement(By.className("react-datepicker__year-select").xpath("//*[contains(text(),'1994')]")).click();
+        WebElement selectmonth = driver.findElement(By.className("react-datepicker__month-select"));
+        Select valuemonth = new Select(selectmonth);
+        valuemonth.selectByValue("5");
+        WebElement selectyear = driver.findElement(By.className("react-datepicker__year-select"));
+        Select valueyear = new Select(selectyear);
+        valueyear.selectByValue("1994");
         driver.findElement(By.className("react-datepicker__day--019")).click();
-
         WebElement subjects = driver.findElement(By.id("subjectsInput"));
         subjects.sendKeys("English");
         subjects.sendKeys(Keys.ENTER);
-
         driver.findElement(By.xpath ("//*[contains(text(),'Reading')]")).click();
         driver.findElement(By.id("currentAddress")).sendKeys("12 Warwickshire Mansions");
 
@@ -54,6 +44,12 @@ public class JuliaMSTest {
 
         WebElement finaltext = driver.findElement(By.id("example-modal-sizes-title-lg"));
         Assert.assertEquals(finaltext.getText(), "Thanks for submitting the form");
+        final List<String> expectedfields = List.of("Julia Ivanova", "juju@mail.com", "Female",
+                "7847985784", "19 June,1994", "English", "Reading", "", "12 Warwickshire Mansions", "");
+        List<WebElement> fieldselements = driver.findElements(By.xpath("//tbody/tr/td[2]"));
+        List<String> actualfields = fieldselements.stream().map(el -> el.getText()).toList();
+        Assert.assertEquals(actualfields, expectedfields);
+
 
         driver.quit();
     }
