490b6e5a787f355c8b11dac8d6aefaf80d3faf7b
NataliaKalugina <koshoshka@gmail.com>
2024-10-23 13:42:49 +0300

    Added test for drop down menu. Added test for finding element in a table

--- src/test/java/NataliaKaluginaTest.java
+++ src/test/java/NataliaKaluginaTest.java
@@ -4,28 +4,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class NataliaKaluginaTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void beforTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
     @Test
     public void testA1WinterSemesterPrice(){
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.srpski-strani.com/pocetninivo1_eng.php");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
         WebElement textPrice1 = driver.findElement(By.xpath("/html/body/div/div[3]/div/table[2]/tbody/tr[1]/td[4]"));
         String foundResult = textPrice1.getText();
         Assert.assertEquals(foundResult, "26.400 RSD (224 €)");
-
-        driver.quit();
     }
     @Test
     public void testMainMenuA1Program() {
-        WebDriver driver = new ChromeDriver();
+
         driver.get("https://www.srpski-strani.com/index_eng.php");
-        driver.manage().window().maximize();
+
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
 
         WebElement menuProgramme = driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div/div/ul/li[3]/a"));
@@ -42,7 +47,9 @@ public class NataliaKaluginaTest {
         WebElement result = driver.findElement(By.xpath("/html/body/div/div[3]/div/p[1]"));
         result.getText();
         Assert.assertEquals(result.getText(), "AIM AND SYLLABUS");
-
+    }
+    @AfterTest
+    public void afterTest(){
         driver.quit();
     }
 
