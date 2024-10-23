292b8d46b80beb47e45c7a08f395fbb2319b652e
Azamat <shazamat009@gmail.com>
2024-10-22 22:38:00 -0500

    rename class

--- src/test/java/AzamatSTest.java
+++ src/test/java/AzamatSTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class AzamatS {
+public class AzamatSTest {
 
     private static final String URL = "https://openweather.co.uk/";
     private static final String PRODUCT_BUTTON_LOCATOR = "//*[@id='desktop-menu']/ul/li[1]/a";
