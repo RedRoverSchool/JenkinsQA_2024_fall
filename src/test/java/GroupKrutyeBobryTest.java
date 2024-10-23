0d0bfbc1c561f04e5ea7c9f0412aac1e29aefbf7
pakhomova_ev <pakhomova.e@gmail.com>
2024-10-23 17:38:24 +0300

    add test dropdown

--- src/test/java/GroupKrutyeBobryTest.java
+++ src/test/java/GroupKrutyeBobryTest.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -21,6 +23,34 @@ public class GroupKrutyeBobryTest {
 
         Boolean textInp = wait.until(ExpectedConditions.textToBe(By.id("text"), "Done!"));
         Assert.assertEquals(textInp, true);
+
+        driver.quit();
+    }
+
+    @Test
+    public void testMouseActionDropDown() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/dropdown-menu.html");
+
+        Actions actions = new Actions(driver);
+
+        WebElement dropDown2 = driver.findElement(By.id("my-dropdown-2"));
+        WebElement contextMenu2 = driver.findElement(By.id("context-menu-2"));
+        actions.contextClick(dropDown2).perform();
+        Assert.assertTrue(contextMenu2.isDisplayed());
+
+        WebElement dropDown3 = driver.findElement(By.id("my-dropdown-3"));
+        WebElement contextMenu3 = driver.findElement(By.id("context-menu-3"));
+        actions.doubleClick(dropDown3).perform();
+        Assert.assertTrue(contextMenu3.isDisplayed());
+
+        WebElement dropDown1 = driver.findElement(By.id("my-dropdown-1"));
+        actions.click(dropDown1).perform();
+
+        WebElement contextMenu1 = driver.findElement(By.xpath("//ul[@class='dropdown-menu show']"));
+        Assert.assertTrue(contextMenu1.isDisplayed());
 
         driver.quit();
     }
