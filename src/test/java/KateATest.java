90ab36cfd2658fb9d06561848b9b2a2b3a14cb62
Katea <atrashkovaekaterina@gmail.com>
2024-10-23 17:28:28 +0200

    Delete test

--- src/test/java/KateATest.java
+++ src/test/java/KateATest.java
@@ -1,17 +1,4 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import java.time.Duration;
-import java.util.List;
 
 public class KateATest {
+
 }
