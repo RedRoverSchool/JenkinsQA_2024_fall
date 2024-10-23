3a36c40d1480d5bc2f8032948db41c7417165d98
Sergey.D <sergei.demyanenko@gmail.com>
2024-10-22 23:25:28 -0400

    Add NoGroupTest

--- src/test/java/NoGroupTest.java
+++ src/test/java/NoGroupTest.java
@@ -1,4 +1,3 @@
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,33 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
-
-public class MainTest {
-
-    @Test
-    public void testSelenium() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-
-        WebDriver driver = new ChromeDriver(options);
-
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-        //driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        textBox.sendKeys("Selenium");
-        submitButton.click();
-
-        WebElement message = driver.findElement(By.id("message"));
-        Assert.assertEquals(message.getText(), "Received!");
-
-        driver.quit();
-    }
+public class NoGroupTest {
 
     @Test
     public void testZara() throws InterruptedException {
