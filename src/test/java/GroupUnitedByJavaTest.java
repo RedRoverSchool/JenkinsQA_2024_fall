0e7bf75ba5ae65d675ba9a472c4773f1103cbc2c
MaksimSavinov <161170566+SavinovMA@users.noreply.github.com>
2024-10-24 22:56:32 +0300

    Перенос тестов в класс группы (#210)
    * Перенос тестов в класс группы
    
    * Перенос тестов в класс группы

--- src/test/java/GroupUnitedByJavaTest.java
+++ src/test/java/GroupUnitedByJavaTest.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class GroupUnitedByJavaTest {
+
+    private static final String URL_SELENIUM_TEST_PAGE = "https://www.selenium.dev/selenium/web/web-form.html";
 
     @Test
     public void testDoubleClick() {
@@ -31,6 +37,97 @@ public class GroupUnitedByJavaTest {
         String doubleClickMessage = driver.findElement(By.id("doubleClickMessage")).getText();
 
         Assert.assertEquals(doubleClickMessage, "You have done a double click");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testText() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL_SELENIUM_TEST_PAGE);
+
+        WebElement selectDropdown = driver.findElement(By.name("my-select"));
+        selectDropdown.click();
+
+        WebElement selectOption = driver.findElement(By.cssSelector("[value='2']"));
+        selectOption.click();
+
+        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+        submitButton.click();
+
+        WebElement messageText = driver.findElement(By.id("message"));
+
+        Assert.assertEquals(messageText.getText(),"Received!");
+        driver.quit();
+    }
+
+    @Test
+    public void testCheck() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL_SELENIUM_TEST_PAGE);
+
+        WebElement checkInput = driver.findElement(By.id("my-check-2"));
+        checkInput.click();
+
+        Assert.assertEquals(checkInput.getDomProperty("checked"),"true");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testSelectingOption() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(URL_SELENIUM_TEST_PAGE);
+
+        WebElement dropdownSelect = driver.findElement(By.cssSelector("[name='my-select']"));
+        Select select = new Select(dropdownSelect);
+        select.selectByVisibleText("Three");
+
+        WebElement threeOption = driver.findElement(By.cssSelector("[value='3']"));
+        Assert.assertTrue(threeOption.isSelected());
+
+        driver.quit();
+    }
+
+    @Test
+    public void testSortingPrice() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement loginInput = driver.findElement(By.id("user-name"));
+        loginInput.sendKeys("standard_user");
+
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys("secret_sauce");
+
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+
+        WebElement sortingSelect = driver.findElement(By.className("product_sort_container"));
+        sortingSelect.click();
+
+        WebElement lohiOption = driver.findElement(By.cssSelector("[value='lohi']"));
+        lohiOption.click();
+
+        List<WebElement> priceList = driver.findElements(By.className("inventory_item_price"));
+
+        List<Double> prices = new ArrayList<>();
+
+        for (WebElement priceElement: priceList) {
+            String priceText = priceElement.getText().replace("$", "");
+            prices.add(Double.parseDouble(priceText));
+        }
+
+        List<Double> sortedPrices = new ArrayList<>(prices);
+        Collections.sort(sortedPrices);
+
+        Assert.assertEquals(prices,sortedPrices);
 
         driver.quit();
     }
