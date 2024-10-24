5ef5279790ad63a7be5dcacf7c8af2e508abc4b0
Denis Novicov <dema28ster@gmail.com>
2024-10-24 17:02:04 +0200

    New Test Text Box

--- src/test/java/AutoamigosGroupTest.java
+++ src/test/java/AutoamigosGroupTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.Set;
 
 import static java.lang.Thread.sleep;
@@ -51,4 +52,55 @@ public class AutoamigosGroupTest {
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://demoqa.com/");
     }
+
+    @Test(description = "Практика заполнение Text Box https://demoqa.com/text-box ")
+    public void testTextBox() throws InterruptedException {
+
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+
+        WebElement fullName = driver.findElement(By.xpath("//*[@id='userName']"));
+        fullName.sendKeys("Max");
+
+        WebElement email = driver.findElement(By.xpath("//*[@id='userEmail']"));
+        email.sendKeys("max@mail.ru");
+
+        WebElement currentAddress = driver.findElement(By.xpath("//*[@id='currentAddress']"));
+        currentAddress.sendKeys("Balti, Index:3120, Republic of Moldova, str. Alecu Ruso, ap. 36");
+
+        WebElement permanentAddress = driver.findElement(By.xpath("//*[@id='permanentAddress']"));
+        permanentAddress.sendKeys("Balti, Index:3120, Republic of Moldova, str. Alecu Ruso, ap. 36");
+
+        // Прокрутка к элементу, найденному по XPath
+        WebElement element = driver.findElement(By.xpath("//*[@id='submit']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+
+        sleep(500);
+
+        WebElement buttonSubmit = driver.findElement(By.xpath("//*[@id='submit']"));
+        buttonSubmit.click();
+
+        sleep(500);
+
+        WebElement fieldName = driver.findElement(By.xpath("//*[@id='name']"));
+        String name = fieldName.getText();
+        Assert.assertEquals(name, "Name:Max");
+
+        WebElement fieldEmail = driver.findElement(By.xpath("//*[@id='inputEmail']"));
+        String inputEmail = fieldEmail.getText();
+        Assert.assertEquals(inputEmail, "Email:max@mail.ru");
+
+        WebElement fieldCurrentAddress = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[2]/form/div[6]/div/p[3]"));
+        String inputCurrentAddress = fieldCurrentAddress.getText();
+        Assert.assertEquals(inputCurrentAddress,"Current Address :Balti, Index:3120, Republic of Moldova, str. Alecu Ruso, ap. 36");
+
+        WebElement fieldPermanentAddress = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[2]/form/div[6]/div/p[4]"));
+        String inputPermanentAddress = fieldPermanentAddress.getText();
+        Assert.assertEquals(inputPermanentAddress, "Permananet Address :Balti, Index:3120, Republic of Moldova, str. Alecu Ruso, ap. 36");
+
+    }
+
+
 }
