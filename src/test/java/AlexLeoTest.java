7777a837a58181387a2b68b8fb9992650f55ff12
Anna <am-rabota@mail.ru>
2024-10-23 21:26:12 -0700

    Reefactor to group test

--- src/test/java/AlexLeoTest.java
+++ src/test/java/AlexLeoTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -83,5 +84,37 @@ public class AlexLeoTest {
         driver.quit();
 
     }
+    @Test
+    public void testSendMessage() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
 
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://automationintesting.online/");
+        Thread.sleep(500);
+
+        WebElement nameInput = driver.findElement(By.id("name"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", nameInput);
+        Thread.sleep(500);
+        nameInput.sendKeys("John");
+
+        WebElement emailInput = driver.findElement(By.xpath("//input[@id = 'email']"));
+        emailInput.sendKeys("js@gmail.com");
+
+        WebElement phoneInput = driver.findElement(By.id("phone"));
+        phoneInput.sendKeys("12345678900");
+
+        WebElement subjectInput = driver.findElement(By.xpath("//input[@id = 'subject']"));
+        subjectInput.sendKeys("Double queen room");
+
+        WebElement messageInput = driver.findElement(By.cssSelector("textarea"));
+        messageInput.sendKeys("Hello! I would like to reserve a room");
+
+        WebElement submitButton = driver.findElement(By.xpath("//button[@id = 'submitContact']"));
+        submitButton.click();
+
+        Thread.sleep(5000);
+        String confirmationMessage = driver.findElement(By.xpath("//div[@class = 'row contact']//h2")).getText();
+        Assert.assertEquals(confirmationMessage, "Thanks for getting in touch John!");
+    }
 }
