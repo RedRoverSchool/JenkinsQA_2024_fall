890fdef12b40dcd1703e80e1da58bcbf320d82ac
A-Petrovsky <apetrovsky@datamola.com>
2024-10-24 14:44:02 +0300

    A-PETROVSKY HW17 - junit hotfix

--- src/test/java/groupStarshipTroopersTest.java
+++ src/test/java/groupStarshipTroopersTest.java
@@ -1,10 +1,11 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -13,14 +14,17 @@ import static org.testng.AssertJUnit.assertEquals;
 public class groupStarshipTroopersTest {
 
     WebDriver driver;
-
-    @BeforeEach
+/*
+    @BeforeTest
     public void setup() {
         driver = new ChromeDriver();
     }
 
+ */
+
     @Test
     public void eightComponents() {
+        driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         driver.get("https://www.21vek.by/");
@@ -40,11 +44,16 @@ public class groupStarshipTroopersTest {
         WebElement message = driver.findElement(By.xpath("//*[@id=\"header\"]/div[1]/div[5]/div/div[2]/div[3]/div[1]/div[1]/div[1]/a[2]/span"));
         String value = message.getText();
         assertEquals("Холодильники", value);
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+           driver.quit();
     }
 
-    @AfterEach
+    /*
+    @AfterTest
     public void teardown() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
-        driver.quit();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+     //   driver.quit();
     }
+
+     */
 }
