4868f15ccf05c6efcbde99ff752c52aad49f5aa7
Pavel Khvorov <pacozone@icloud.com>
2024-10-25 12:10:56 +0200

    Conflict education 2.0

--- src/test/java/KhPaulConflictTest.java
+++ src/test/java/KhPaulConflictTest.java
@@ -14,24 +14,13 @@ public class KhPaulConflictTest {
 
     private static WebDriver driver;
     private static final String validLogin = "standard_user";
-    private static final String lockedOutUser = "locked_out_user";
     private static final String validPassword = "secret_sauce";
-    private static final String invalidLogin = "";
-    private static final String invalidPassword = "123";
     private static final String URL = "https://www.saucedemo.com/";
     private static final By LOGIN_FIELD = By.id("user-name");
     private static final By PASSWORD_FIELD = By.id("password");
     private static final By LOGIN_BUTTON = By.id("login-button");
-    private static final By BURGER_MENU = By.id("react-burger-menu-btn");
-    private static final By LOGOUT_BUTTON = By.id("logout_sidebar_link");
-    private static final By PRODUCTS = By.xpath("//span[contains(text(), 'Products')]");
-    private static final By ERROR_MESSAGE_BLOCKED_USER = By.xpath("//h3[contains(text(),'Epic sadface: Sorry, this user has been locked out')]");
-    private static final By ERROR_MESSAGE_USER = By.xpath("//h3[contains(text(),'Epic sadface: Username is required')]");
-    private static final By ERROR_MESSAGE_USER_AND_PASSWORD = By.xpath("//h3[contains(text(),'Epic sadface: Username and password do not match any user in this service')]");
+
     private static final By SAUCE_LABS_BACKPACK = By.xpath("//div[contains(text(),'Sauce Labs Backpack')]");
-    private static final By ADD_TO_CART_BUTTON = By.id("add-to-cart");
-    private static final By SHOPPING_CART = By.className("shopping_cart_link");
-    private static final By PRODUCT_COUNTER = By.xpath("//span[@class='shopping_cart_badge']");
     private static final By ERROR_BUTTON = By.className("error-button");
 
 
@@ -43,9 +32,7 @@ public class KhPaulConflictTest {
 
     @AfterTest
     public void closeWebDriver() {
-        if (driver != null) {
             driver.quit();
-        }
     }
 
     public void timeSleep() {
@@ -65,7 +52,7 @@ public class KhPaulConflictTest {
 
         timeSleep();
 
-        WebElement result = driver.findElement(PRODUCTS);
+        WebElement result = driver.findElement(By.xpath("//span[contains(text(), 'Products')]"));
         Assert.assertEquals(result.getText(), "Products");
     }
 
@@ -76,11 +63,11 @@ public class KhPaulConflictTest {
 
         timeSleep();
 
-        driver.findElement(BURGER_MENU).click();
+        driver.findElement(By.id("react-burger-menu-btn")).click();
 
         timeSleep();
 
-        driver.findElement(LOGOUT_BUTTON).click();
+        driver.findElement(By.id("logout_sidebar_link")).click();
 
         timeSleep();
 
@@ -96,13 +83,15 @@ public class KhPaulConflictTest {
 
         timeSleep();
 
-        driver.findElement(LOGIN_FIELD).sendKeys(lockedOutUser);
+        driver.findElement(LOGIN_FIELD).sendKeys("locked_out_user");
         driver.findElement(PASSWORD_FIELD).sendKeys(validPassword);
         driver.findElement(LOGIN_BUTTON).click();
 
         timeSleep();
 
-        Assert.assertEquals(driver.findElement(ERROR_MESSAGE_BLOCKED_USER).getText(), "Epic sadface: Sorry, this user has been locked out.");
+        Assert.assertEquals(driver.findElement(
+                By.xpath("//h3[contains(text(),'Epic sadface: Sorry, this user has been locked out')]"))
+                .getText(), "Epic sadface: Sorry, this user has been locked out.");
         driver.findElement(ERROR_BUTTON).click();
     }
 
@@ -118,13 +107,13 @@ public class KhPaulConflictTest {
         WebElement imgSauceLabsBackpack = driver.findElement(By.xpath("//img[@alt='Sauce Labs Backpack']"));
         Assert.assertEquals(imgSauceLabsBackpack.getAttribute("alt"), "Sauce Labs Backpack");
 
-        driver.findElement(ADD_TO_CART_BUTTON).click();
+        driver.findElement(By.id("add-to-cart")).click();
 
         timeSleep();
 
-        Assert.assertEquals(driver.findElement(PRODUCT_COUNTER).getText(), "1");
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText(), "1");
 
-        driver.findElement(SHOPPING_CART).click();
+        driver.findElement(By.className("shopping_cart_link")).click();
 
         Assert.assertEquals(driver.findElement(SAUCE_LABS_BACKPACK).getText(), "Sauce Labs Backpack");
 
@@ -137,13 +126,15 @@ public class KhPaulConflictTest {
 
         timeSleep();
 
-        driver.findElement(LOGIN_FIELD).sendKeys(invalidLogin);
+        driver.findElement(LOGIN_FIELD).sendKeys("");
         driver.findElement(PASSWORD_FIELD).sendKeys(validPassword);
         driver.findElement(LOGIN_BUTTON).click();
 
         timeSleep();
 
-        Assert.assertEquals(driver.findElement(ERROR_MESSAGE_USER).getText(), "Epic sadface: Username is required");
+        Assert.assertEquals(driver.findElement(
+                By.xpath("//h3[contains(text(),'Epic sadface: Username is required')]"))
+                .getText(), "Epic sadface: Username is required");
         driver.findElement(ERROR_BUTTON).click();
     }
 
@@ -155,12 +146,14 @@ public class KhPaulConflictTest {
         timeSleep();
 
         driver.findElement(LOGIN_FIELD).sendKeys(validLogin);
-        driver.findElement(PASSWORD_FIELD).sendKeys(invalidPassword);
+        driver.findElement(PASSWORD_FIELD).sendKeys("123");
         driver.findElement(LOGIN_BUTTON).click();
 
         timeSleep();
 
-        Assert.assertEquals(driver.findElement(ERROR_MESSAGE_USER_AND_PASSWORD).getText(), "Epic sadface: Username and password do not match any user in this service");
+        Assert.assertEquals(driver.findElement(
+                By.xpath("//h3[contains(text(),'Epic sadface: Username and password do not match any user in this service')]"))
+                .getText(), "Epic sadface: Username and password do not match any user in this service");
         driver.findElement(ERROR_BUTTON).click();
     }
 
