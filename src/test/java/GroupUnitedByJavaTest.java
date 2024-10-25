930fac78a6cbb4c251336ce162257e6f4a263917
NatalyaVoronova <loginova2024.1@gmail.com>
2024-10-25 20:11:07 +0500

    Test attach Natalya V

--- src/test/java/GroupUnitedByJavaTest.java
+++ src/test/java/GroupUnitedByJavaTest.java
@@ -434,4 +434,51 @@ public class GroupUnitedByJavaTest {
         driver.quit();
 
     }
+
+    @Test
+    public void testFormRequiredFields() throws InterruptedException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        //chromeOptions.addArguments("--headless"); // запуск без отображения окна
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://demoqa.com/");
+
+        WebElement forms = driver.findElement(By.xpath("//div/div/div[2]/div/div[2]"));
+        forms.click();
+
+        WebElement pracriceForms = driver.findElement(By.cssSelector(".element-list.collapse.show"));
+        pracriceForms.click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://demoqa.com/automation-practice-form");
+
+        String titleForm = driver.findElement(By.cssSelector(".text-center")).getText();
+        Assert.assertEquals(titleForm, "Practice Form");
+
+        WebElement firstname = driver.findElement(By.id("firstName"));
+        firstname.sendKeys("Nata");
+
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        lastName.sendKeys("fff");
+
+        driver.findElement(By.xpath("//label[contains(text(), 'Male')]")).click();
+        driver.findElement(By.id("userNumber")).sendKeys("1234567890");
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(1000);
+
+        String modalTitle = driver.findElement(By.id("example-modal-sizes-title-lg")).getText();
+        Assert.assertEquals("Thanks for submitting the form", modalTitle);
+
+        String firstNameValue = driver.findElement(By.xpath("//td[text()='Student Name']/following-sibling::td")).getText();
+        Assert.assertEquals("Nata fff", firstNameValue);
+
+        String phoneValue = driver.findElement(By.xpath("//td[text()='Mobile']/following-sibling::td")).getText();
+        Assert.assertEquals("1234567890", phoneValue);
+        //Thread.sleep(500); //если хочется посмотреть что получилось в браузере
+        driver.quit();
+
+    }
+
 }
