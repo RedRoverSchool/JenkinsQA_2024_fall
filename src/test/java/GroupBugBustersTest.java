b962d70ba75b436e1b5b23c70e9e15b8ec294095
ivanofyork <ivanfrom90210@gmail.com>
2024-10-26 18:01:15 +0200

    Updated Den's tests

--- src/test/java/GroupBugBustersTest.java
+++ src/test/java/GroupBugBustersTest.java
@@ -11,17 +11,24 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class GroupBugBustersTest {
+public class GroupBugBustersTest{
 
-    WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
 
-    @Test
+    @BeforeTest // Den
+    public void baseURL() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox", "--start-maximized");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @Test //Den
     public void testSearchCheeseInVkusville(){
-        ChromeDriver driver = new ChromeDriver();
         driver.get("https://vkusvill.ru/");
 
         WebElement search = driver.findElement(By.cssSelector(".HeaderSearchBlock__Input.js-vv21-search__search-input.js-search-autotyping"));
-
         search.sendKeys("Сыр");
 
         WebElement searchButton = driver.findElement(By.cssSelector(".HeaderSearchBlock__BtnSearchImg.js-vv21-seacrh__icon-btn"));
@@ -29,35 +36,28 @@ public class GroupBugBustersTest {
 
         WebElement title = driver.findElement(By.cssSelector(".VV21_SearchPage__Title.h4_desktop.h4_tablet.js-search-total-num-container"));
         Assert.assertEquals(title.getText().substring(title.getText().indexOf("Сыр")), "Сыр»");
-        driver.quit();
     }
 
-    @BeforeTest // Den
-    public void baseURL() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-
-        driver.get("https://tasnet.uz"); // - Открыть страницу
-        driver.manage().window().maximize(); // - Открывает всегда во весь экран
-    }
 
     @Test //Den
     public void test() {
+        driver.get("https://tasnet.uz"); // - Открыть страницу
         String pageTitle = driver.getTitle();
-        System.out.println(pageTitle);
+        Assert.assertEquals(pageTitle, "Tasnet - Wi-Fi authorization via SMS and social networks Hotspot", "Tittle doesn't match");
     }
     @Test //Den
-    public void test2() {
+    public void test2(){
+        driver.get("https://tasnet.uz"); // - Открыть страницу
         WebElement cost = driver.findElement(By.xpath("//*[@id=\"__next\"]/main/div[1]/ul/li[3]/a"));
         cost.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        // regex для проверки ссылки сайта. Включает в себя https://tasnet.uz/pricing и https://tasnet.uz/en/pricing
+        Assert.assertTrue(driver.getCurrentUrl().matches("^https://tasnet.uz(/en)?/pricing"), "Wrong page");
     }
 
     @Test //Den
     public void test3() {
-
         driver.get("https://tasnet.uz/business");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         WebElement searchEmail = driver.findElement(By.xpath("//*[@id=\"email\"]"));
         searchEmail.sendKeys("Java", Keys.ENTER);
     }
