a71b0d3421c7dd7aef03c56b9217d76cb9c32a36
Yasmin-Jaborova <yjaborova.qaautomation@gmail.com>
2024-10-26 13:37:26 +0500

    Moved 2 tests, regarding for adding item to the cart and verifying cart count

--- src/test/java/GroupUnitedByJavaTest.java
+++ src/test/java/GroupUnitedByJavaTest.java
@@ -2,12 +2,16 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -493,4 +497,101 @@ public class GroupUnitedByJavaTest {
         driver.quit();
     }
 
+    public static class YasminJaborovaTest {
+
+        @DataProvider(name = "userData")
+        public Object[][] getUserData(){
+            List<String[]> users = Arrays.asList(
+                    new String[]{"standard_user", "secret_sauce"},
+                    new String[]{"problem_user", "secret_sauce"},
+                    new String[]{"error_user", "secret_sauce"},
+                    new String[]{"visual_user", "secret_sauce"}
+            );
+            return users.toArray(new Object[users.size()][]);
+        }
+
+        @Test (dataProvider = "userData")
+        public void testAuthorizationWithMultipleUsers(String username, String password) throws InterruptedException {
+            //1.Launch new tab in browser
+            WebDriver driver = new ChromeDriver();
+            //2.Follow the link
+            driver.get("https://www.saucedemo.com/");
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+
+            //3.Fill out all required fields
+            WebElement usernameField = driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
+            WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+            WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+
+            usernameField.sendKeys(username);
+            passwordField.sendKeys(password);
+            Thread.sleep(1000);
+            loginButton.click();
+
+            //4.Compare current and expected behaviors
+            WebElement headerOfPage = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span"));
+            Assert.assertEquals(headerOfPage.getText(), "Products");
+
+            driver.quit();
+            }
+
+    }
+
+    public static class YasminJaborovaSecondTest {
+
+        @Test
+        public void testCartFunctionality() {
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+
+            //1.Follow the link
+            driver.get("https://magento.softwaretestingboard.com/");
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            //2. Hover over 'Women' dropdown.
+            WebElement womenDropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"ui-id-4\"]")));
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].scrollIntoView(true);", womenDropdown);
+            Actions actions = new Actions(driver);
+            actions.moveToElement(womenDropdown).perform();
+
+            //Hover over 'Tops' button
+            WebElement topsButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"ui-id-9\"]/span[2]")));
+            actions.moveToElement(topsButton).perform();
+
+            //Select 'Jackets'
+            WebElement jacketsButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"ui-id-11\"]/span")));
+            jacketsButton.click();
+
+            //Select 1 item - jacket
+            WebElement item = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"maincontent\"]/div[3]/div[1]/div[3]/ol/li[3]/div/div/strong/a")));
+            item.click();
+
+            //Select size and color of item, and add it to the cart
+            WebElement sizeOfItemL = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"option-label-size-143-item-169\"]")));
+            WebElement colorOfItem = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"option-label-color-93-item-49\"]")));
+            WebElement addToCartButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"product-addtocart-button\"]")));
+            js.executeScript("arguments[0].scrollIntoView(true);", addToCartButton);
+            sizeOfItemL.click();
+            colorOfItem.click();
+            addToCartButton.click();
+
+            //Go to Shopping cart
+            WebElement shoppingCartButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"maincontent\"]/div[1]/div[2]/div/div/div/a")));
+            shoppingCartButton.click();
+
+            //Proceed to Checkout
+            WebElement proceedToCheckoutButton =  wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"maincontent\"]/div[3]/div/div[2]/div[1]/ul/li[1]/button")));
+            js.executeScript("arguments[0].scrollIntoView(true);", proceedToCheckoutButton);
+            proceedToCheckoutButton.click();
+
+            //Compare current and expected results
+            WebElement quantityOfItemsInCart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"opc-sidebar\"]/div[1]/div/div[1]/strong/span[1]")));
+            Assert.assertEquals(quantityOfItemsInCart.getText(), "1");
+
+            driver.quit();
+
+        }
+    }
 }
