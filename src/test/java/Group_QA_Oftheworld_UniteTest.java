475f229aaa093fd4d5c2c8a1a456d0ad78962a1a
JuliaBekker <u19841985@gmail.com>
2024-10-27 15:19:38 +0700

    Add class Group_QA_Oftheworld_UniteTest и комментарий, изменила текст

--- src/test/java/Group_QA_Oftheworld_UniteTest.java
+++ src/test/java/Group_QA_Oftheworld_UniteTest.java
@@ -21,7 +21,7 @@ public class Group_QA_Oftheworld_UniteTest {
 
             // поиск поля для ввода текста
             WebElement textBox1 = driver.findElement(By.className("antiplagiat-block_text-container")); // в этой строке происходит поиск веб-элемента
-            textBox1.sendKeys("Любопытно, что прошепчет Тутошкин кому-нибудь обо мне; я будто бы сколочен без особого изъяна и лицо в норме, но очкарик, без очков двух шагов не ступлю.");
+            textBox1.sendKeys("Любопытно, что прошепчет Тутошкин кому-нибудь обо мне");
 
             // поиск кнопки Проверить уникальность  и нажатие на неё
             WebElement submitButton1 = driver.findElement(By.className("antiplagiat-block_submit t-btn t-btn--fill"));
