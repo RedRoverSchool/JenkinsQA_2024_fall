376277b437d39ac556bf342b59dc375663e3a5d8
qa <grekrimimp@gmail.com>
2024-10-28 21:16:56 +0300

    task 17

---
+++ src/test/java/GromovDenisTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GromovDenisTest {
+
+    @Test
+    public void Test5() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        Thread.sleep(1000);
+
+//        driver.findElement(By.xpath("//*[@id=\"W0wltc\"]")).click();
+        WebElement text = driver.findElement(By.className("gLFyf"));
+        text.sendKeys("Selenim");
+//        Thread.sleep(1000);
+
+        WebElement button = driver.findElement(By.className("gNO89b"));
+        button.click();
+
+//        Thread.sleep(1000);
+
+
+        WebElement textSel = driver.findElement(By.xpath(
+                "//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[2]/div/span/em"));
+
+        Assert.assertEquals(textSel.getText(), "Selenium");
+        driver.quit();
+
+    }
+}
+
