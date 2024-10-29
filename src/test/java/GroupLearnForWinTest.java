cca3d4361392499de0fb3b1bfd0f0acbee64426e
Yuliyash1106 <yuliyash1106@gmail.com>
2024-10-28 21:33:16 -0400

    added testABTestControl

--- src/test/java/GroupLearnForWinTest.java
+++ src/test/java/GroupLearnForWinTest.java
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class GroupLearnForWinTest {
@@ -202,6 +203,33 @@ public class GroupLearnForWinTest {
 
         driver.quit();
 
+    }
+
+    @Test
+
+    public  void testABTestControl() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/abtest");
+
+        String originalWindow = driver.getWindowHandle();
+
+        driver.findElement(By.xpath("//a[@target='_blank']")).click();
+
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String windowHandle : allWindows) {
+            if (!windowHandle.equals(originalWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+        String elementalSelenium = driver.findElement(By.xpath("//h1[@class= 'hero__title']")).getText();
+
+        Assert.assertEquals(elementalSelenium, "Elemental Selenium" );
+
+        driver.quit();
     }
 
 }
