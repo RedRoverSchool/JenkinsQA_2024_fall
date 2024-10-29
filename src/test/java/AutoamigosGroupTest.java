3103b0a9e3edb4cdce248206b5c9ed88ec8649d7
AnnaLimutina <zip1061@mail.ru>
2024-10-29 18:31:14 -0500

    add new tests and methods

--- src/test/java/AutoamigosGroupTest.java
+++ src/test/java/AutoamigosGroupTest.java
@@ -3,11 +3,16 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import static java.lang.Thread.sleep;
@@ -21,7 +26,7 @@ public class AutoamigosGroupTest {
         driver = new ChromeDriver();
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown(){
         driver.quit();
     }
@@ -50,5 +55,47 @@ public class AutoamigosGroupTest {
         }
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://demoqa.com/");
+    }
+
+    @Test
+    public void testSelectAllCheckBoxes() throws InterruptedException {
+
+        driver.get("https://demoqa.com/checkbox");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 500);");
+
+        sleep(1000);
+
+        WebElement buttonExpandAll = driver.findElement(By.xpath("//button[@title='Expand all']"));
+        buttonExpandAll.click();
+
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//span[@class='rct-checkbox']"));
+        checkboxes.get(0).click();
+        sleep(10000);
+
+        for (WebElement el: checkboxes){
+            WebElement childEl = driver.findElement(By.xpath("//span[@class='rct-checkbox']/*"));
+            boolean isSelected = childEl.getAttribute("class").contains("check");
+            Assert.assertTrue(isSelected);
+        }
+    }
+
+    @Test
+    public void testRadioButtons() throws InterruptedException {
+
+        driver.get("https://demoqa.com/radio-button");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 500);");
+
+        sleep(1000);
+
+        WebElement radiobuttonYes = driver.findElement(By.xpath("//input[@id='yesRadio']/following-sibling::label"));
+        radiobuttonYes.click();
+
+        WebElement textSelectedButton = driver.findElement(By.xpath("//span[@class='text-success']"));
+
+        Assert.assertEquals(textSelectedButton.getText(), "Yes");
     }
 }
