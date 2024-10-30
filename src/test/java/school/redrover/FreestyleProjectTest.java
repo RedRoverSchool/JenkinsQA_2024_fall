128c66dac40fd00b78bf7a3643f9fd152646ad67
YevgenyN <evgenii.tst@gmail.com>
2024-10-30 22:44:50 +0300

    Test create Freestyle project with valid name

---
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -0,0 +1,32 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class FreestyleProjectTest extends BaseTest {
+
+    @Test
+    public void testCreateFreestyleProjectWithValidName() throws InterruptedException {
+        String nameProject = "My first freestyle project";
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys(nameProject);
+
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+
+        getDriver().findElement(By.id("ok-button")).click();
+
+        Thread.sleep(2000);
+
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+
+        Assert.assertEquals(getDriver()
+                .findElement(By.xpath("//span[text()='My first freestyle project']"))
+                .getText(), nameProject);
+    }
+
+}
+
