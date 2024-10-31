248a175a37580325c29ae4f7e0508f5a6e46e056
VyacheslavBeliaev <vbelyaev86@gmail.com>
2024-11-01 00:55:01 +0300

    Check  switching status condition Enable/Diasble by button

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -23,5 +24,28 @@ public class MultibranchPipelineTest extends BaseTest {
         String actualDescription = getDriver().findElement(By.id("view-message")).getText();
 
         Assert.assertEquals(actualDescription, expectedDescription);
+    }
+
+    @Test
+    public void testVerifyStatusToSwitchingEnableButton() {
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys("Muiltibranch Pipeline project");
+
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+        getDriver().findElement(By.cssSelector("[class$='MultiBranchProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.cssSelector("#toggle-switch-enable-disable-project > label")).click();
+
+        JavascriptExecutor js1 = (JavascriptExecutor) getDriver();
+        js1.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+        getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
+        getDriver().findElement(By.xpath("//*[@id='enable-project']/button")).click();
+
+        String foundText = getDriver().findElement(By.xpath("//*[@id='disable-project']/button")).getText();
+        Assert.assertEquals(foundText, "Disable Multibranch Pipeline");
     }
 }
