b00e40c838104029c5fdc4c5918819315a61376f
Diana <krukava.qamanual@gmail.com>
2024-11-01 12:41:05 +0300

    add version test

---
+++ src/test/java/school/redrover/VersionTest.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class VersionTest extends BaseTest {
+    @Test
+    public void CheckVersionTest() {
+        getDriver().manage().window().maximize();
+
+        WebElement version = getDriver().findElement(By.xpath("//button[@type='button']"));
+        new Actions(getDriver())
+                .scrollToElement(version)
+                .perform();
+        version.click();
+
+        getDriver().findElement(By.xpath("//a[normalize-space()='About Jenkins']")).click();
+
+        Assert.assertEquals(getDriver()
+                .findElement(By.className("app-about-version"))
+                .getText(), "Version 2.462.3");
+    }
+
+}
