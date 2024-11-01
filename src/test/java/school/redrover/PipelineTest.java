feeca612cb4dfa5944984cd7f2b6c1de8aad8b42
VyacheslavBeliaev <vbelyaev86@gmail.com>
2024-11-02 00:14:54 +0300

    Check switching status condition Enable/Diasble by button

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -25,9 +25,23 @@ public class PipelineTest extends BaseTest {
                 "//table[@id='projectstatus']/tbody/tr/td/a/span")).getText();
 
         Assert.assertEquals(actualJobName, "Pipeline1");
-
-
     }
 
+    @Test
+    public void testVerifySwitchingEnableButton() {
+        getDriver().findElement(By.xpath("//a[@href='newJob']")).click();
+        getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys("New Pipeline");
+
+        getDriver().findElement(By.cssSelector("[class$='WorkflowJob']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.cssSelector("#toggle-switch-enable-disable-project > label")).click();
+
+        getDriver().findElement(By.cssSelector(".jenkins-submit-button")).click();
+        getDriver().findElement(By.cssSelector("#enable-project > button")).click();
+
+        String foundText = getDriver().findElement(By.cssSelector("#pipeline-box > div")).getText();
+        Assert.assertEquals(foundText, "No data available. This Pipeline has not yet run.");
+    }
 
 }
