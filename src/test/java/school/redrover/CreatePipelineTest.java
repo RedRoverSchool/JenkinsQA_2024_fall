1d8b844fd835f2357bf0fcf7be20e208d9eeb75e
MapGary <eresskigal@gmail.com>
2024-11-01 16:25:33 +0100

    add test

--- src/test/java/school/redrover/CreatePipelineTest.java
+++ src/test/java/school/redrover/CreatePipelineTest.java
@@ -16,6 +16,8 @@ public class CreatePipelineTest extends BaseTest {
         getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
+//        getDriver().findElement(By.xpath("//*[@id='main-panel']/form/div[1]/div[2]/div/div[2]/textarea")).click();
+//        getDriver().findElement(By.xpath("//*[@id='main-panel']/form/div[1]/div[2]/div/div[2]/textarea")).sendKeys("Test Pipeline");
         getDriver().findElement(By.xpath("//label[contains(text(), 'Do not allow the pipeline to resume if the controller restarts')]")).click();
         getDriver().findElement(By.xpath("//label[contains(text(), 'Throttle builds')]")).click();
         getDriver().findElement(By.xpath("//*[@id='main-panel']/form/div[1]/section[1]/div[18]/div[4]/div[1]/div[2]/input")).sendKeys(Keys.DELETE);
@@ -24,8 +26,10 @@ public class CreatePipelineTest extends BaseTest {
         getDriver().findElement(By.name("Submit")).click();
 
         String pipelineName = getDriver().findElement(By.xpath("//*[@id='main-panel']/div[1]/div[1]/h1")).getText();
+//        String pipelineDescription = getDriver().findElement(By.xpath("//div[2]/div[2]/div[2]/div")).getText();
 
         Assert.assertEquals(pipelineName, "test");
+//        Assert.assertEquals(pipelineDescription, "Test Pipeline");
 
     }
 }
