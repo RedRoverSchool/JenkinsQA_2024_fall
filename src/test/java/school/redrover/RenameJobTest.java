6246870de4ebaf9db4bcaf6d8a24c9decf81f41c
ovdro <ovdro.ovdro@gmail.com>
2024-11-02 03:16:57 +0200

    refactored methods createJob, renameJob

--- src/test/java/school/redrover/RenameJobTest.java
+++ src/test/java/school/redrover/RenameJobTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -13,34 +12,33 @@ public class RenameJobTest extends BaseTest {
     @Test
     public void tesRenameJob() {
 
-        createJob(getDriver());
+        createJob("TestBuild");
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='job_TestBuild']/td[3]/a/span")).getText(),"TestBuild");
 
-        renameJob(getDriver());
+        renameJob("TestBuild_NewName");
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//*[@class='jenkins-table__link model-link inside']"))
                 .getText(), "TestBuild_NewName");
 
     }
 
-    public void createJob(WebDriver driver) {
+    public void createJob( String projectName) {
 
         getDriver().findElement(By.xpath("//*[@id='main-panel']/div[2]/div/section[1]/ul/li/a")).click();
-        getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys("TestBuild");
+        getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys(projectName);
         getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[1]")).click();
         getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
         getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
         getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[1]/a")).click();
-
     }
 
-    public void renameJob(WebDriver driver){
+    public void renameJob(String testBuildNewName){
         getDriver().findElement(By.xpath("//*[@id='job_TestBuild']/td[3]/a/span")).click();
         getDriver().findElement(By.xpath("//*[@id='tasks']/div[7]/span/a")).click();
         WebElement inputField = getDriver().findElement(By.xpath("//*[@id='main-panel']/form/div[1]/div[1]/div[2]/input"));
         inputField.clear();
-        inputField.sendKeys("TestBuild_NewName");
+        inputField.sendKeys(testBuildNewName);
         WebElement renameButton = getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button"));
         Actions actionsRenameButton = new Actions(getDriver());
         actionsRenameButton.moveToElement(renameButton).click().perform();
