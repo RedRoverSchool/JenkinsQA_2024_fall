ff18c8ce04ab381838bb98376d7554ff6820a7e0
ovdro <ovdro.ovdro@gmail.com>
2024-11-02 03:05:28 +0200

    added method createJob

--- src/test/java/school/redrover/RenameJobTest.java
+++ src/test/java/school/redrover/RenameJobTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -12,12 +13,7 @@ public class RenameJobTest extends BaseTest {
     @Test
     public void tesRenameJob() {
 
-        getDriver().findElement(By.xpath("//*[@id='main-panel']/div[2]/div/section[1]/ul/li/a")).click();
-        getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys("TestBuild");
-        getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[1]")).click();
-        getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
-        getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
-        getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[1]/a")).click();
+        createJob(getDriver());
         getDriver().findElement(By.xpath("//*[@id='job_TestBuild']/td[3]/a/span")).click();
         getDriver().findElement(By.xpath("//*[@id='tasks']/div[7]/span/a")).click();
         WebElement inputField = getDriver().findElement(By.xpath("//*[@id='main-panel']/form/div[1]/div[1]/div[2]/input"));
@@ -31,6 +27,15 @@ public class RenameJobTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(By.xpath("//*[@class='jenkins-table__link model-link inside']"))
                 .getText(), "TestBuild_NewName");
 
+    }
+
+    public void createJob(WebDriver driver) {
+        getDriver().findElement(By.xpath("//*[@id='main-panel']/div[2]/div/section[1]/ul/li/a")).click();
+        getDriver().findElement(By.xpath("//*[@id='name']")).sendKeys("TestBuild");
+        getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[1]")).click();
+        getDriver().findElement(By.xpath("//*[@id='ok-button']")).click();
+        getDriver().findElement(By.xpath("//*[@id='bottom-sticker']/div/button[1]")).click();
+        getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[1]/a")).click();
     }
 
 }
