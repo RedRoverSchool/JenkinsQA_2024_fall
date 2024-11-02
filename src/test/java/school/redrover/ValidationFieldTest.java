5a8b9d7b17c80e1bf75d3cc53671fdd90b95af4c
NikitaS-95 <nserov95@gmail.com>
2024-11-02 10:24:57 +0500

    Add new tests and refactor code

--- src/test/java/school/redrover/ValidationFieldTest.java
+++ src/test/java/school/redrover/ValidationFieldTest.java
@@ -8,53 +8,90 @@ import school.redrover.runner.BaseTest;
 
 public class ValidationFieldTest extends BaseTest {
 
+    private static final String CREATE_BUTTON = "//a[@it = 'hudson.model.Hudson@185982f1']";
+    private static final String SUBMIT_BUTTON = "//button[@type = 'submit']";
+    private static final String INPUT_FIELD = "//input[@class = 'jenkins-input']";
+    private static final String SET_FOLDER_TYPE = "//div/ul/li[@class = 'com_cloudbees_hudson_plugins_folder_Folder']";
+
     @Test
     public void testFieldIsEmpty() {
 
-        getDriver().findElement(By.xpath("//div[@class = \"task \"] [1]"))
+        getDriver().findElement(By.xpath(CREATE_BUTTON))
                 .click();
 
-        WebElement submitButton = getDriver().findElement(By.xpath("//button[@type = \"submit\"]"));
-        submitButton.click();
+        getDriver().findElement(By.xpath(SUBMIT_BUTTON))
+                .click();
 
         WebElement validationMessage = getDriver().findElement(By.xpath("//div[@class = \"input-validation-message\"]"));
 
-        Assert.assertEquals(validationMessage.getText(),"» This field cannot be empty, please enter a valid name");
+        Assert.assertEquals(validationMessage.getText(), "» This field cannot be empty, please enter a valid name");
     }
 
     @Test
     public void testInvalidCharacters() {
 
-       getDriver().findElement(By.xpath("//section[1]/ul/li[1]"))
-               .click();
+        getDriver().findElement(By.xpath(CREATE_BUTTON))
+                .click();
 
-       WebElement inputField = getDriver().findElement(By.xpath("//input[@class = \"jenkins-input\"]"));
-       inputField.sendKeys("#!@$!%");
+        getDriver().findElement(By.xpath(INPUT_FIELD))
+                .sendKeys("#!@$!%");
 
-       WebElement invalidMessage = getDriver().findElement(By.xpath("//div[@id = \"itemname-invalid\"]"));
+        WebElement invalidMessage = getDriver().findElement(By.xpath("//div[@id = \"itemname-invalid\"]"));
 
-       Assert.assertEquals(invalidMessage.getText(),"» ‘#’ is an unsafe character");
+        Assert.assertEquals(invalidMessage.getText(), "» ‘#’ is an unsafe character");
     }
 
     @Test
     public void testMaxCharacters() {
 
-        getDriver().findElement(By.xpath("//a/span [text() = \"Create a job\"]"))
+        getDriver().findElement(By.xpath(CREATE_BUTTON))
                 .click();
 
-        WebElement inputField = getDriver().findElement(By.xpath("//input[@class = \"jenkins-input\"]"));
-        inputField.sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor nisl eu est viverra, " +
-                "quis molestie nibh blandit. In lectus felis, iaculis mauris Lorem ipsum dolor sit amet, consectetur adipiscing elit " +
-                "est Lorem ipsum dolor sit amet, consectetur adipiscing elit est");
+        getDriver().findElement(By.xpath(INPUT_FIELD))
+                .sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor nisl eu est viverra, " +
+                        "quis molestie nibh blandit. In lectus felis, iaculis mauris Lorem ipsum dolor sit amet, consectetur adipiscing elit " +
+                        "est Lorem ipsum dolor sit amet, consectetur adipiscing elit est");
 
-        WebElement folderButton = getDriver().findElement(By.xpath("//ul/li[@class = \"com_cloudbees_hudson_plugins_folder_Folder\"]"));
-        folderButton.click();
+        getDriver().findElement(By.xpath(SET_FOLDER_TYPE))
+                .click();
 
-        WebElement submitButton = getDriver().findElement(By.xpath("//button[@type = \"submit\"]"));
-        submitButton.click();
+        getDriver().findElement(By.xpath(SUBMIT_BUTTON))
+                .click();
 
         WebElement errorDescription = getDriver().findElement(By.xpath("//div/h2"));
 
-        Assert.assertEquals(errorDescription.getText(),"A problem occurred while processing the request");
+        Assert.assertEquals(errorDescription.getText(), "A problem occurred while processing the request");
+    }
+
+    @Test
+    public void testExistedProject() {
+
+        getDriver().findElement(By.xpath(CREATE_BUTTON))
+                .click();
+
+        getDriver().findElement(By.xpath(INPUT_FIELD))
+                .sendKeys("Folder");
+
+        getDriver().findElement(By.xpath(SET_FOLDER_TYPE))
+                .click();
+
+        getDriver().findElement(By.xpath(SUBMIT_BUTTON))
+                .click();
+
+        WebElement applyButton = getDriver().findElement(By.xpath("//button[@name = 'Apply']"));
+        applyButton.click();
+
+        WebElement jenkinsLogo = getDriver().findElement(By.xpath("//div/a[@id = 'jenkins-home-link']"));
+        jenkinsLogo.click();
+
+        getDriver().findElement(By.xpath(CREATE_BUTTON))
+                .click();
+
+        getDriver().findElement(By.xpath(INPUT_FIELD))
+                .sendKeys("Folder");
+
+        WebElement validationMessage = getDriver().findElement(By.xpath("//div[@id = 'itemname-invalid']"));
+
+        Assert.assertEquals(validationMessage.getText(), "» A job already exists with the name ‘Folder’");
     }
 }
