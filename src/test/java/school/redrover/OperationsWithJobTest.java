f2016099ca2f929f79be18ecc7d1272628b8679f
ovdro <ovdro.ovdro@gmail.com>
2024-11-03 23:34:16 +0200

    added testWorkspaceJob

--- src/test/java/school/redrover/OperationsWithJobTest.java
+++ src/test/java/school/redrover/OperationsWithJobTest.java
@@ -13,6 +13,22 @@ import java.time.Duration;
 
 public class OperationsWithJobTest extends BaseTest {
 
+    @Test(description = "create and setup workspace job")
+    public void testWorkspaceJob(){
+
+        createJob("TestBuild");
+
+        Assert.assertEquals(getDriver()
+                .findElement(By.xpath("//*[@id='job_TestBuild']/td[3]/a/span"))
+                .getText(),"TestBuild");
+
+        navigateToManageJob();
+
+        Assert.assertEquals(getDriver()
+                .findElement(By.xpath("//*[@id='no-builds']"))
+                .getText(),"No builds");
+
+    }
     @Test(description = "create and build job")
     public void testBuildJob(){
 
