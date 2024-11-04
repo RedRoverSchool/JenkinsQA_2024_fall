72f374ab6f52185958e0e6a4290ae41fb8d4d21f
serhio <serhiosereda@gmail.com>
2024-11-04 17:46:17 +0200

    Add test for dropdown items

--- src/test/java/school/redrover/DashboardItemsDropdownTest.java
+++ src/test/java/school/redrover/DashboardItemsDropdownTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class DashboardItemsDropdownTest extends BaseTest {
 
     private WebDriverWait setWait() {
-        return new WebDriverWait(getDriver(), Duration.ofSeconds(12));
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(30));
     }
 
     @Test
