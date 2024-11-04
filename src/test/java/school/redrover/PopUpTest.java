86eb420ac26365aada47f4378bff163c1a2ad0be
polina.safronova <safronovap92@gmail.com>
2024-11-04 15:55:12 +0400

    PopUp close widget test

--- src/test/java/school/redrover/PopUpTest.java
+++ src/test/java/school/redrover/PopUpTest.java
@@ -1,4 +1,3 @@
-
 package school.redrover;
 
 import org.openqa.selenium.By;
@@ -6,29 +5,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
 
-
-public class PopUpTest extends BaseTest {
-
-    @Test
-    public void PopUptest() {
+public class PopUpTest {
+@Test
+    public void testPopUpExitButton() {
+        // Initialize the Chrome WebDriver
         WebDriver driver = new ChromeDriver();
 
+        // Open the website
         driver.get("https://candymapper.com/");
 
-        driver.getTitle();
-
+        // Implicit wait to allow elements to load
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
 
-
-        WebElement exitButton = driver.findElement((By.id("popup-widget62629-close-icon")));
-
+        // Locate the exit button on the popup by its ID and click to close it
+        WebElement exitButton = driver.findElement(By.id("popup-widget62629-close-icon"));
         exitButton.click();
 
+        // Quit the driver, closing all associated windows
         driver.quit();
     }
 }
-
