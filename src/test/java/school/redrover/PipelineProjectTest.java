89eafe4d294f417ac953bb00bf4d82aad1203157
AllaBird <allavorobieva@yahoo.com>
2024-11-03 19:58:42 -0500

    edit wait

--- src/test/java/school/redrover/PipelineProjectTest.java
+++ src/test/java/school/redrover/PipelineProjectTest.java
@@ -118,9 +118,11 @@ public class PipelineProjectTest extends BaseTest {
 
         WebElement projectElement = getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//a[@href='job/" + encodeSpacesForURL(PIPELINE_NAME) + "/']")));
-        actions.moveToElement(projectElement).perform();
+        actions.moveToElement(projectElement).moveByOffset(10, 5).perform();
 
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", projectElement);
+        //actions.moveByOffset(15, 5).click().perform();
+
+       // ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", projectElement);
 
         WebElement chevronButton = getWebDriverWait().until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//a[@href='job/" + encodeSpacesForURL(PIPELINE_NAME) + "/']//button[@class='jenkins-menu-dropdown-chevron']")));
