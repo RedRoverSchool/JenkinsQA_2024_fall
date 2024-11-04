cbbd7b6a6921d15ec569022b7ebd62aa5ad7c107
serhio <serhiosereda@gmail.com>
2024-11-04 14:53:47 +0200

    Add test for dropdown items

--- src/test/java/school/redrover/DashboardItemsDropdownTest.java
+++ src/test/java/school/redrover/DashboardItemsDropdownTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class DashboardItemsDropdownTest extends BaseTest {
 
     private WebDriverWait setWait() {
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(90));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(30));
         return wait;
 
     }
@@ -30,15 +30,22 @@ public class DashboardItemsDropdownTest extends BaseTest {
         Actions actions = new Actions(getDriver());
         actions.moveToElement(dashboardButton).perform();
 
-        WebElement buttonDropdown = getDriver().findElement(
+        WebElement buttonOpen = getDriver().findElement(
                 By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button"));
-        actions.moveToElement(buttonDropdown).click().perform();
+        actions.moveToElement(buttonOpen).click().perform();
 
-        List<WebElement> dropDownList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+        List<WebElement> dropDownVisible = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.xpath("//*[@id=\"tippy-3\"]/div/div/div")));
+        Assert.assertFalse(dropDownVisible.isEmpty(), "Dropdown - empty");
+
+        WebElement listFistSection = dropDownVisible.get(0);
+        actions.moveToElement(listFistSection).perform();
+
+        List<WebElement> itemsList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
                 By.xpath("//*[@id=\"tippy-3\"]/div/div/div/a")));
-        Assert.assertFalse(dropDownList.isEmpty(), "Dropdown - empty");
+        Assert.assertFalse(itemsList.isEmpty(), "Items not found");
 
-        WebElement newItem = dropDownList.get(0);
+        WebElement newItem = itemsList.get(0);
         newItem.click();
     }
 }
