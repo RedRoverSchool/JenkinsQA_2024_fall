5713d8c70c634b7693a4db6c40709466a21ebdde
Pavel Khalilov <pavel.khalilov.qa@gmail.com>
2024-11-04 14:10:01 -0500

    added test for MultiConfiguration Project

--- src/test/java/school/redrover/CreatingNewItemTest.java
+++ src/test/java/school/redrover/CreatingNewItemTest.java
@@ -5,28 +5,65 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import school.redrover.runner.ProjectUtils;
 
 public class CreatingNewItemTest extends BaseTest {
 
     @Test
     public void testCreateFreestyleProject() throws InterruptedException {
+        ProjectUtils.log("Create a new project");
         WebElement newItemButton = getDriver().findElement(By.cssSelector("[href='/view/all/newJob']"));
         newItemButton.click();
 
+        ProjectUtils.log("Enter the name of the project");
+        String jobName = "Some name for Freestyle project";
         WebElement inputItemNameField = getDriver().findElement(By.className("jenkins-input"));
-        inputItemNameField.sendKeys("Some name of Freestyle project");
+        inputItemNameField.sendKeys(jobName);
 
+        ProjectUtils.log("Select the project type");
         WebElement freestyleProjectButton = getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject"));
         freestyleProjectButton.click();
 
+        ProjectUtils.log("Push OK button to save the project");
         WebElement okButton = getDriver().findElement(By.cssSelector("#ok-button"));
         okButton.click();
 
+        ProjectUtils.log("Going to the main page");
         WebElement dashboardButtonInBreadcrumbs = getDriver().findElement(By.xpath("//a[@class='model-link' and text()='Dashboard']"));
         dashboardButtonInBreadcrumbs.click();
 
         String jobNameText = getDriver().findElement(By.xpath("//tr/td/a/span")).getText();
 
-        Assert.assertEquals(jobNameText, "Some name of Freestyle project");
+        ProjectUtils.log("Verifying the job name");
+        Assert.assertEquals(jobNameText, jobName);
+    }
+
+    @Test
+    public void testMultiConfigurationProject() throws InterruptedException {
+        ProjectUtils.log("Create a new project");
+        WebElement newItemButton = getDriver().findElement(By.cssSelector("[href='/view/all/newJob']"));
+        newItemButton.click();
+
+        ProjectUtils.log("Enter the name of the project");
+        String jobName = "Some name for Multi-configuration Project";
+        WebElement inputItemNameField = getDriver().findElement(By.className("jenkins-input"));
+        inputItemNameField.sendKeys(jobName);
+
+        ProjectUtils.log("Select the project type");
+        WebElement multiConfigurationProjectButton = getDriver().findElement(By.xpath("//li[@class='hudson_matrix_MatrixProject']"));
+        multiConfigurationProjectButton.click();
+
+        ProjectUtils.log("Push OK button to save the project");
+        WebElement okButton = getDriver().findElement(By.xpath("//button[@type='submit']"));
+        okButton.click();
+
+        ProjectUtils.log("Going to the main page");
+        WebElement dashboardButtonInBreadcrumbs = getDriver().findElement(By.xpath("//a[@class='model-link' and text()='Dashboard']"));
+        dashboardButtonInBreadcrumbs.click();
+
+        ProjectUtils.log("Verifying the job name");
+        String jobNameText = getDriver().findElement(By.xpath("//td/a/span")).getText();
+        Assert.assertEquals(jobNameText, jobName);
+
     }
 }
