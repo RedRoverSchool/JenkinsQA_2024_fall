286cc7caf60527b48fff7d0010e273691d03dd0c
NikitaS-95 <nserov95@gmail.com>
2024-11-05 10:18:57 +0500

    Change sleep to wait

--- src/test/java/school/redrover/DropDownTest.java
+++ src/test/java/school/redrover/DropDownTest.java
@@ -1,11 +1,15 @@
 package school.redrover;
 
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import school.redrover.runner.BaseTest;
+
+import java.time.Duration;
 
 public class DropDownTest extends BaseTest {
 
@@ -13,15 +17,15 @@ public class DropDownTest extends BaseTest {
     private static final String BREAD_CRUMBS = "//ol/li[@class = 'jenkins-breadcrumbs__list-item'] [3]";
 
     @Test
-    public void testBuild() throws InterruptedException {
+    public void testBuild() {
 
         WebElement dropdownChevron = getDriver().findElement(By.xpath(HEADER_DROPDOWN));
         Actions actions = new Actions(getDriver());
         actions.moveToElement(dropdownChevron).click().perform();
 
-        Thread.sleep(1000);
-
         WebElement buildButton = getDriver().findElement(By.xpath("//div/a[@class = 'jenkins-dropdown__item '] [1]"));
+        WebDriverWait w = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        w.until(ExpectedConditions.visibilityOfAllElements(buildButton));
         buildButton.click();
 
         WebElement thirdCrumb = getDriver().findElement(By.xpath(BREAD_CRUMBS));
@@ -30,15 +34,15 @@ public class DropDownTest extends BaseTest {
     }
 
     @Test
-    public void testConfigure() throws InterruptedException {
+    public void testConfigure() {
 
         WebElement dropdownChevron = getDriver().findElement(By.xpath(HEADER_DROPDOWN));
         Actions actions = new Actions(getDriver());
         actions.moveToElement(dropdownChevron).click().perform();
 
-        Thread.sleep(1000);
-
         WebElement configureButton = getDriver().findElement(By.xpath("//div/a[@class = 'jenkins-dropdown__item '] [2]"));
+        WebDriverWait w = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        w.until(ExpectedConditions.visibilityOfAllElements(configureButton));
         configureButton.click();
 
         WebElement thirdCrumb = getDriver().findElement(By.xpath(BREAD_CRUMBS));
