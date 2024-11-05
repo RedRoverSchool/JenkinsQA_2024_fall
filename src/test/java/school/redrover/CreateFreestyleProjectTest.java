7172b5166060b792f71c756f9bdf635bc550947b
Yasmin-Jaborova <yjaborova.qaautomation@gmail.com>
2024-11-05 09:20:25 +0500

    Add test for verifying Freestyle Project in My Views page

--- src/test/java/school/redrover/CreateFreestyleProjectTest.java
+++ src/test/java/school/redrover/CreateFreestyleProjectTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+
 public class CreateFreestyleProjectTest extends BaseTest {
     @Test
     public void testCreateFreestyleProject() {
@@ -21,5 +22,28 @@ public class CreateFreestyleProjectTest extends BaseTest {
 
         Assert.assertEquals(newProjectName, expectedProjectName);
 
+    }
+
+    @Test
+    public void testVerifyFreeStyleProjectInMyViewsPage () throws InterruptedException {
+        final String EXPECTED_FREE_STYLE_PROJECT_NAME ="My First Free Style Project";
+
+        getDriver().findElement(By.cssSelector(".task-link-wrapper")).click();
+
+        getDriver().findElement(By.cssSelector(".jenkins-input")).sendKeys(EXPECTED_FREE_STYLE_PROJECT_NAME);
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+
+        getDriver().findElement(By.cssSelector("textarea[name='description']")).sendKeys("Lorem ipsum dolor sit amet");
+        getDriver().findElement(By.cssSelector("button[name='Submit']")).click();
+
+        Thread.sleep(2000);
+
+        getDriver().findElement(By.cssSelector(".jenkins-breadcrumbs__list-item")).click();
+        getDriver().findElement(By.xpath("//a[@href='/me/my-views']"));
+
+        String actualFreeStyleProjectName = getDriver().findElement(By.xpath("//span[text()='My First Free Style Project']")).getText();
+
+        Assert.assertEquals(EXPECTED_FREE_STYLE_PROJECT_NAME, actualFreeStyleProjectName);
     }
 }
