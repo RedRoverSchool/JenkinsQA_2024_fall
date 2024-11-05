60359572b83c90118a2d992545da481a2f1c9248
pakhomova_ev <pakhomova.e@gmail.com>
2024-11-05 11:10:18 +0300

    add test  - uable to create a job without a nameÐ

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -18,6 +19,7 @@ public class PipelineTest extends BaseTest {
 
         getDriver().findElement(By.id("ok-button")).click();
 
+
         getDriver().findElement(By.cssSelector(".jenkins-submit-button")).click();
         getDriver().findElement(By.id("jenkins-home-link")).click();
 
@@ -25,9 +27,30 @@ public class PipelineTest extends BaseTest {
                 "//table[@id='projectstatus']/tbody/tr/td/a/span")).getText();
 
         Assert.assertEquals(actualJobName, "Pipeline1");
+    }
 
+    @Test
+    public void testCreateWithEmptyName() {
+        String expectErrorMessage = "» This field cannot be empty, please enter a valid name";
+        getDriver().findElement(By.xpath("//a[@href ='newJob']")).click();
+
+        getDriver().findElement(By.xpath(
+                "//li[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
+        WebElement buttonSubmit = getDriver().findElement(By.id("ok-button"));
+
+        WebElement actualErrorMessage = getDriver().findElement(By.id("itemname-required"));
+
+        Assert.assertFalse(buttonSubmit.isEnabled());
+        Assert.assertEquals(actualErrorMessage.getText(), expectErrorMessage);
+    }
+    @Test
+    public void testJenkinsVologda() {
+        WebElement logo = getDriver().findElement(By.xpath("//img[@id='jenkins-name-icon']"));
+        Assert.assertTrue(logo.isDisplayed(), "Jenkins logo is not displayed in the header.");
 
     }
 
-
 }
+}
+
+
