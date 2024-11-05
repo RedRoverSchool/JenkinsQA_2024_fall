b88f79ea8917cb273ef99bd8d2e78484edde2c1d
KorobovS <korobovs33@yandex.ru>
2024-11-05 17:54:51 +0300

    add new test class GetAllBuildsTest

--- src/test/java/school/redrover/GetAllBuildsTest.java
+++ src/test/java/school/redrover/GetAllBuildsTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
+import org.openqa.selenium.interactions.Actions;
 
 public class GetAllBuildsTest extends BaseTest {
 
@@ -14,5 +15,20 @@ public class GetAllBuildsTest extends BaseTest {
         String title = getDriver().findElement(By.xpath("//div[@class='jenkins-app-bar__content']")).getText();
 
         Assert.assertEquals(title, "Build History of Jenkins");
+    }
+
+    @Test
+    public void testConfigurePage() throws InterruptedException {
+
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(getDriver().findElement(By.xpath("//div[@class='login page-header__hyperlinks']/a/button"))).click().perform();
+        Thread.sleep(500);
+
+        getDriver().findElement(By.xpath("//div[@class='jenkins-dropdown']/a[2]")).click();
+        Thread.sleep(500);
+
+        String str = getDriver().findElement(By.xpath("//div[@class='jenkins-breadcrumbs']/ol/li[5]")).getText();
+
+        Assert.assertEquals(str, "Configure");
     }
 }
