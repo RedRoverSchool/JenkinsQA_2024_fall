cef87581f6bbf5487440052dac87d32c185ebe10
polina.safronova <safronovap92@gmail.com>
2024-11-05 13:26:23 +0400

    Simple test for Jenkins Page

--- src/test/java/school/redrover/ItemTest.java
+++ src/test/java/school/redrover/ItemTest.java
@@ -1,12 +1,13 @@
 package school.redrover;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class ItemTest extends BaseTest {
     @Test
-    public void NewItem() {
+    public void newItem() {
         String ItemElement = getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a")).getText();
         Assert.assertEquals(ItemElement, "New Item");
     }
