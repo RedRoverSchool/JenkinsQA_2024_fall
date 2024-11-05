bfd089ebe7be8a24aac27c68b0c85e09cf23309c
polina.safronova <safronovap92@gmail.com>
2024-11-05 12:13:00 +0400

    Simple test for J Page

--- src/test/java/school/redrover/PageJenTest.java
+++ src/test/java/school/redrover/PageJenTest.java
@@ -9,8 +9,8 @@ import school.redrover.runner.BaseTest;
 public class PageJenTest extends BaseTest {
     @Test
     public void PageJTest() {
-        String newItemElement = getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a")).getText();
-        Assert.assertEquals(newItemElement, "New Item");
+        String ItemElement = getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a")).getText();
+        Assert.assertEquals(ItemElement, "New Item");
         getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a")).click();
     }
 
