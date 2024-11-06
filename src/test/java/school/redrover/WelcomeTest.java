d46da45ff97946c66aff650ba58a59ff89b0511e
Max13Tester <legem1306@yandex.ru>
2024-11-06 20:36:22 +0300

    welcome test get.Text

--- src/test/java/school/redrover/WelcomeTest.java
+++ src/test/java/school/redrover/WelcomeTest.java
@@ -10,7 +10,7 @@ public class WelcomeTest extends BaseTest {
     public void testWelcome() {
         String welcomeStr = getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText();
 
-        Assert.assertEquals(welcomeStr, "Добро пожаловать в Jenkins!");
+        Assert.assertEquals(welcomeStr, "Welcome to Jenkins!");
     }
 }
 
