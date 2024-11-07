5f18c0c955b3e7ab8a56881d84f1d4cec22f4744
ArtsiomAzaranka <azarenkoav@gmail.com>
2024-11-07 22:45:00 +0300

    fix testCheckLinksSidePanel

--- src/test/java/school/redrover/StartPageTest.java
+++ src/test/java/school/redrover/StartPageTest.java
@@ -1,12 +1,10 @@
 package school.redrover;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import java.time.Duration;
