5b24e5484557f2b2d68490542abefce27bf03632
Pavel Khalilov <pavel.khalilov.qa@gmail.com>
2024-11-06 21:23:22 -0500

    added test for DashboardDropdownMenu increase waiting time for dropdown list

--- src/test/java/school/redrover/VerifyingDashboardBreadcrumbsTest.java
+++ src/test/java/school/redrover/VerifyingDashboardBreadcrumbsTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -27,11 +28,39 @@ public class VerifyingDashboardBreadcrumbsTest extends BaseTest {
         ProjectUtils.log("Hover over 'Dashboard' in breadcrumbs");
         actions.moveToElement(dashboard).perform();
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         ProjectUtils.log("Click on the dropdown icon");
         WebElement dropdownIcon = getDriver().findElement(By.xpath("//ol/li/a[@href='/']/button[@class='jenkins-menu-dropdown-chevron']"));
-        dropdownIcon.click();
+//        dropdownIcon.click();
+        actions.moveToElement(dropdownIcon).click().perform();
+
+        try {
+            // JavaScript для визуализации нажатия
+            String script = "function visualizeClick(x, y) {" +
+                    "var indicator = document.createElement('div');" +
+                    "indicator.style.position = 'absolute';" +
+                    "indicator.style.width = '5px';" +
+                    "indicator.style.height = '5px';" +
+                    "indicator.style.backgroundColor = 'red';" +
+                    "indicator.style.borderRadius = '150%';" +
+                    "indicator.style.left = x + 'px';" +
+                    "indicator.style.top = y + 'px';" +
+                    "indicator.style.zIndex = 1000;" +
+                    "document.body.appendChild(indicator);" +
+                    "setTimeout(function() { document.body.removeChild(indicator); }, 2000);" +
+                    "}" +
+                    "visualizeClick(100, 79);";
+            // Выполнение JavaScript через Selenium
+            JavascriptExecutor js = (JavascriptExecutor) getDriver();
+            js.executeScript(script);
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("warning");;
+        }
 
         Thread.sleep(3000);
 
