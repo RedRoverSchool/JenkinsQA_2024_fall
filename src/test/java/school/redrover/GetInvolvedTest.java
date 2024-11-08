20b7a2be6ef70aea8f3284fce85e0321691e7b31
Oksana Vladimirova <42274574+0keana@users.noreply.github.com>
2024-11-08 20:50:05 +0300

    test connect text (#612)

---
+++ src/test/java/school/redrover/GetInvolvedTest.java
@@ -0,0 +1,20 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+public class GetInvolvedTest extends BaseTest {
+
+    @Test
+
+    public void testConnect() {
+
+        getDriver().findElement(By.xpath("//button[@type='button']")).click();
+        getDriver().findElement(By.xpath( "//*[@id='tippy-1']/div/div/div/a[2]")).click();
+        String textConnect = getDriver().findElement(By.xpath("/html/body/div/div/a[1]/h3")).getText();
+
+        Assert.assertEquals(textConnect, "Connect");
+    }
+}
