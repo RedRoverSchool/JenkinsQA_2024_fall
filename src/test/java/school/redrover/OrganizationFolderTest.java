907e4a94fa42774f87822e81bc53cdc45a0d2798
AliakseiKashkevich <aliaksei.kashkevich@gmail.com>
2024-11-08 14:41:24 +0300

    разделил на отдельные тесты, добавил новые

--- src/test/java/school/redrover/OrganizationFolderTest.java
+++ src/test/java/school/redrover/OrganizationFolderTest.java
@@ -2,28 +2,21 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ByIdOrName;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-import java.time.Duration;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
-
 public class OrganizationFolderTest extends BaseTest {
-    private static final String ITEMNAME = "Item Name";
+    private static final String ITEM_NAME = "Item Name";
     private static String DISPLAYNAME = "Display Name";
     private static String DESCRIPTION = "Description";
 
     @Test
     public void testTitle() {
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
@@ -36,7 +29,7 @@ public class OrganizationFolderTest extends BaseTest {
     @Test
     public void testTooltipGeneralEnabled() {
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
@@ -52,7 +45,7 @@ public class OrganizationFolderTest extends BaseTest {
     public void testDescriptionPreview() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
@@ -68,7 +61,7 @@ public class OrganizationFolderTest extends BaseTest {
     public void testDescriptionPreviewHide() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
@@ -88,7 +81,7 @@ public class OrganizationFolderTest extends BaseTest {
         Actions actions = new Actions(getDriver());
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
@@ -105,7 +98,7 @@ public class OrganizationFolderTest extends BaseTest {
         Actions actions = new Actions(getDriver());
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
@@ -120,14 +113,14 @@ public class OrganizationFolderTest extends BaseTest {
     public void testItemNameAfterCreation() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
         getDriver().findElement(By.xpath("//div/button[@type='submit']")).click();
         getDriver().findElement(By.xpath("//div/input[@name='_.displayNameOrNull']")).sendKeys(DISPLAYNAME);
         getDriver().findElement(By.xpath("//div/button[@class='jenkins-button jenkins-submit-button jenkins-button--primary ']")).click();
-        String itemNameExpected = "Folder name: " + ITEMNAME;
+        String itemNameExpected = "Folder name: " + ITEM_NAME;
         getDriver().findElement(By.xpath("//div[2]/div[2]")).getText();
 
         String itemNameActual = getDriver().findElement(By.id("main-panel")).getText();
@@ -142,7 +135,7 @@ public class OrganizationFolderTest extends BaseTest {
     public void testDescriptionAfterCreation() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
@@ -157,7 +150,7 @@ public class OrganizationFolderTest extends BaseTest {
     public void testChangeItemName() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
@@ -166,13 +159,10 @@ public class OrganizationFolderTest extends BaseTest {
         getDriver().findElement(By.id("jenkins-home-link")).click();
         getDriver().findElement(By.xpath("//td/a[@class='jenkins-table__link model-link inside']")).click();
         getDriver().findElement(By.xpath("//div[7]/span/a")).click();
-        String newName = ITEMNAME + " NEW";
+        String newName = ITEM_NAME + " NEW";
         getDriver().findElement(By.xpath("//div/input[@checkdependson='newName']")).click();
         getDriver().findElement(By.xpath("//div/input[@checkdependson='newName']")).sendKeys(" NEW");
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
-
-        System.out.println(newName);
-        System.out.println(getDriver().findElement(By.xpath("//div/h1")).getText());
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//div/h1")).getText(), newName);
 
