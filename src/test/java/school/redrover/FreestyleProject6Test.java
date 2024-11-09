4b9a61e9988bdaf076cd44a8a4361559142536bd
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2024-11-09 11:02:15 +0300

    CreateAndDeleteFreestyleJobTest was renamed to FreestyleProject6Test … (#617)
    * CreateAndDeleteFreestyleJobTest was renamed to FreestyleProject6Test and refactored
    
    * CreateAndDeleteFreestyleJobTest was renamed to FreestyleProject6Test and refactored
    
    * CreateAndDeleteFreestyleJobTest was renamed to FreestyleProject6Test and refactored

--- src/test/java/school/redrover/FreestyleProject6Test.java
+++ src/test/java/school/redrover/FreestyleProject6Test.java
@@ -1,25 +1,37 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-public class CreateAndDeleteFreestyleJobTest extends BaseTest {
+public class FreestyleProject6Test extends BaseTest {
 
-    @Test
-    public void testCreateAndDeleteFreestyleProject () {
+    final String projectName = "MyJob";
 
+    public void createFreestyleProject() {
         getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/div[2]/div/section[1]/ul/li/a")).click();
-        getDriver().findElement(By.className("jenkins-input")).sendKeys("MyJob");
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(projectName);
         getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/section[1]/div[3]/div[1]/div/span/label")).click();
         getDriver().findElement(By.className("jenkins-submit-button")).click();
+    }
+
+    @Test
+    public void testCreateProject() {
+
+        createFreestyleProject();
+
         String jobName = getDriver().findElement(By.className("job-index-headline")).getText();
 
-        Assert.assertEquals(jobName, "MyJob");
+        Assert.assertEquals(jobName, projectName);
+    }
+
+    @Test
+    public void testDeleteProject() {
+
+        createFreestyleProject();
 
         getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[6]/span/a")).click();
         getDriver().findElement(By.xpath("//*[@id=\"jenkins\"]/dialog/div[3]/button[1]")).click();
