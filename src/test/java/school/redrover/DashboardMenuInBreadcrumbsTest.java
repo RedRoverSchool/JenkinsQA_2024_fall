32c274e19d1b9863febab0c0d71d642411f2979f
Pavel Khalilov <pavel.khalilov.qa@gmail.com>
2024-11-09 19:55:55 -0500

    change test for DashboardDropdownMenu

--- src/test/java/school/redrover/DashboardMenuInBreadcrumbsTest.java
+++ src/test/java/school/redrover/DashboardMenuInBreadcrumbsTest.java
@@ -17,54 +17,53 @@ import java.util.List;
 public class DashboardMenuInBreadcrumbsTest extends BaseTest {
 
     @Test
-    @Ignore
     public void testDashboardDropdownMenu() throws InterruptedException {
 
         Actions actions = new Actions(getDriver());
 
         ProjectUtils.log("Find 'Dashboard' element");
-        WebElement dashboard = getDriver().findElement(By.xpath("//ol/li/a[@href='/']"));
-
-        Thread.sleep(1000);
+        WebElement dashboard = getDriver().findElement(By.xpath("//li[@class='jenkins-breadcrumbs__list-item']"));
+        System.out.println(dashboard.getSize());
 
         ProjectUtils.log("Hover over 'Dashboard' in breadcrumbs");
-        actions.moveToElement(dashboard).perform();
+        actions
+                .moveToElement(dashboard)
+                .moveByOffset(55, 0)
+                .click()
+                .perform();
+        System.out.println(dashboard.getSize());
 
-        Thread.sleep(3000);
+//        ProjectUtils.log("Click on the dropdown icon");
+//        WebElement dropdownIcon = getDriver().findElement(By.xpath("//ol/li/a[@href='/']/button[@class='jenkins-menu-dropdown-chevron']"));
+////        dropdownIcon.click();
+//        actions.moveToElement(dropdownIcon).click().perform();
 
-        ProjectUtils.log("Click on the dropdown icon");
-        WebElement dropdownIcon = getDriver().findElement(By.xpath("//ol/li/a[@href='/']/button[@class='jenkins-menu-dropdown-chevron']"));
-//        dropdownIcon.click();
-        actions.moveToElement(dropdownIcon).click().perform();
-
-        try {
-            // JavaScript для визуализации нажатия
-            String script = "function visualizeClick(x, y) {" +
-                    "var indicator = document.createElement('div');" +
-                    "indicator.style.position = 'absolute';" +
-                    "indicator.style.width = '5px';" +
-                    "indicator.style.height = '5px';" +
-                    "indicator.style.backgroundColor = 'red';" +
-                    "indicator.style.borderRadius = '150%';" +
-                    "indicator.style.left = x + 'px';" +
-                    "indicator.style.top = y + 'px';" +
-                    "indicator.style.zIndex = 1000;" +
-                    "document.body.appendChild(indicator);" +
-                    "setTimeout(function() { document.body.removeChild(indicator); }, 2000);" +
-                    "}" +
-                    "visualizeClick(100, 79);";
-            // Выполнение JavaScript через Selenium
-            JavascriptExecutor js = (JavascriptExecutor) getDriver();
-            js.executeScript(script);
-            Thread.sleep(10000);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("warning");;
-        }
-
-        Thread.sleep(3000);
+//        try {
+//            // JavaScript для визуализации нажатия
+//            String script = "function visualizeClick(x, y) {" +
+//                    "var indicator = document.createElement('div');" +
+//                    "indicator.style.position = 'absolute';" +
+//                    "indicator.style.width = '5px';" +
+//                    "indicator.style.height = '5px';" +
+//                    "indicator.style.backgroundColor = 'red';" +
+//                    "indicator.style.borderRadius = '150%';" +
+//                    "indicator.style.left = x + 'px';" +
+//                    "indicator.style.top = y + 'px';" +
+//                    "indicator.style.zIndex = 1000;" +
+//                    "document.body.appendChild(indicator);" +
+//                    "setTimeout(function() { document.body.removeChild(indicator); }, 2000);" +
+//                    "}" +
+//                    "visualizeClick(100, 79);";
+//            // Выполнение JavaScript через Selenium
+//            JavascriptExecutor js = (JavascriptExecutor) getDriver();
+//            js.executeScript(script);
+//            Thread.sleep(10000);
+//        }
+//        catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } finally {
+//            System.out.println("warning");;
+//        }
 
         ProjectUtils.log("Create list of dropdown elements");
         List<WebElement> dashboardDropdownMenuElements = getDriver().findElements(By.xpath("//a[@class='jenkins-dropdown__item ']"));
