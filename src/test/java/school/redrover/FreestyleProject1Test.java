93aa620b6cce4f578c13e17853e77d9ae729e6ba
Eugene Sh <48071321+Ev9eny@users.noreply.github.com>
2024-11-10 10:53:40 -0500

    modified testCreateFreestyleProject (#635)
    * Update FreestyleProject1Test.java
    
    * delete project test
    
    * Update BaseTest.java
    
    * added test rename freestyle project

--- src/test/java/school/redrover/FreestyleProject1Test.java
+++ src/test/java/school/redrover/FreestyleProject1Test.java
@@ -1,29 +1,60 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class FreestyleProject1Test extends BaseTest {
-
-    @Test
-    public void testCreateFreestyleProject() throws InterruptedException {
+    private static final String NEW_FREESTYLE_PROJECT_NAME = "New freestyle project";
+    private void createFreestyleProject() {
         getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']")).click();
-        getDriver().findElement(By.name("name")).sendKeys("New freestyle project");
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        js.executeScript("window.scrollBy(0, 500);");
+        getDriver().findElement(By.name("name")).sendKeys(NEW_FREESTYLE_PROJECT_NAME);
         getDriver().findElement(By.id("ok-button")).click();
-        Thread.sleep(1000);
-        js.executeScript("window.scrollBy(0, 1000);");
         getDriver().findElement(By.name("Submit")).click();
-        Thread.sleep(1000);
-        getDriver().findElement(By.id("jenkins-name-icon")).click();
 
-        WebElement element = getDriver().findElement(By.xpath("//span[contains(text(),'New freestyle project')]"));
+    }
+    @Test
+    public void testCreateFreestyleProject() throws InterruptedException {
+        createFreestyleProject();
+
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+        WebElement element = getDriver().findElement(By
+                .xpath("//span[contains(text(),'" + NEW_FREESTYLE_PROJECT_NAME + "')]"));
         Assert.assertTrue(element.isDisplayed());
+    }
+
+    @Test
+    public void testDeleteFreestyleProject() throws InterruptedException {
+        createFreestyleProject();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+        Actions actions = new Actions(getDriver());
+        WebElement element = getDriver().findElement(By
+                .xpath("//span[contains(text(),'" + NEW_FREESTYLE_PROJECT_NAME + "')]"));
+        actions.moveToElement(element).click().perform();
+        getDriver().findElement((By.xpath("//*[@data-title='Delete Project']"))).click();
+        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
+        String emptyDashboardHeader = getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText();
+        Assert.assertEquals(emptyDashboardHeader, "Welcome to Jenkins!");
+    }
+
+    @Test
+    public void testRenameFreestyleProject() throws InterruptedException {
+        createFreestyleProject();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+        Actions actions = new Actions(getDriver());
+        WebElement element = getDriver().findElement(By
+                .xpath("//span[contains(text(),'" + NEW_FREESTYLE_PROJECT_NAME + "')]"));
+        actions.moveToElement(element).click().perform();
+        getDriver().findElement((By.xpath("//*[contains(@href,'confirm-rename')]"))).click();
+        getDriver().findElement(By.xpath("//input[@name='newName']")).clear();
+        Thread.sleep(200);
+        getDriver().findElement(By.xpath("//input[@name='newName']")).sendKeys("Renamed freestyle project");
+        getDriver().findElement(By.name("Submit")).click();
+        String projectName = getDriver().findElement(By.xpath("//*[@class='job-index-headline page-headline']")).getText();
+        Assert.assertEquals(projectName, "Renamed freestyle project");
     }
 }
