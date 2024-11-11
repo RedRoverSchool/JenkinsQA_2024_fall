896a832bfaeb5ffcf68c7bd1e297c5108fc71225
VyacheslavBeliaev <vbelyaev86@gmail.com>
2024-11-12 00:24:42 +0300

    Verify can Delete Freestyle_Project From Inside exist Project

--- src/test/java/school/redrover/FreestyleProject2Test.java
+++ src/test/java/school/redrover/FreestyleProject2Test.java
@@ -7,6 +7,8 @@ import school.redrover.runner.BaseTest;
 
 public class FreestyleProject2Test extends BaseTest {
 
+    private static final String FREESTYLE_NAME = "Freestyle_first";
+
     public void createFreestyleProject(String nameFreestyleProject) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys(nameFreestyleProject);
@@ -14,13 +16,20 @@ public class FreestyleProject2Test extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
     }
 
+    private void returnToHomePage() {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+    }
+
+    private void clickJobByName(String nameFreestyleProject) {
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + nameFreestyleProject + "/']")).click();
+    }
+
     @Test
     public void testEditDescription(){
         final String PROJECT_DESCRIPTION = "my new build";
         final String editDescribe = "Create one more build apps";
 
-        String PROJECT_NAME = "Freestyle_first";
-        createFreestyleProject(PROJECT_NAME);
+        createFreestyleProject(FREESTYLE_NAME);
 
         getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/div[2]/div/div[2]/textarea")).sendKeys(PROJECT_DESCRIPTION);
         getDriver().findElement(By.name("Submit")).click();
@@ -34,4 +43,18 @@ public class FreestyleProject2Test extends BaseTest {
 
         Assert.assertTrue(getDriver().findElement(By.id("description")).getText().contains(editDescribe));
     }
+
+    @Test
+    public void testDeleteFromInsideProject() {
+        createFreestyleProject(FREESTYLE_NAME);
+        returnToHomePage();
+        clickJobByName(FREESTYLE_NAME);
+
+        getDriver().findElement(By.xpath("//a[@data-title='Delete Project']")).click();
+        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
+
+        String emptyHeader = getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText();
+        Assert.assertEquals(emptyHeader, "Welcome to Jenkins!");
+    }
+
 }
