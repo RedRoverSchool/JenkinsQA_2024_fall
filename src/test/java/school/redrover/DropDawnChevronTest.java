413d012e6453e3f4eac3b899dbdb7a357d6fe1c1
Denis Novicov <dema28ster@gmail.com>
2024-11-11 21:49:27 +0100

    DropDawnChevronNewItemDelMK10_Local_Passed

--- src/test/java/school/redrover/DropDawnChevronTest.java
+++ src/test/java/school/redrover/DropDawnChevronTest.java
@@ -1,6 +1,5 @@
 package school.redrover;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -40,15 +39,34 @@ public class DropDawnChevronTest extends BaseTest {
 
         WebElement hiddenButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='job_TestDropDawnChevron']/td[3]/a/button")));
         actions.moveToElement(hiddenButton)
-                .pause(java.time.Duration.ofSeconds(1)).click().perform();
+                .pause(java.time.Duration.ofSeconds(5)).click().perform();
 
-        WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='tippy-6']/div/div/div")));
-        actions.moveToElement(dropdown)
-                .pause(java.time.Duration.ofSeconds(1)).click().perform();
+        String searchText = "\n" +
+                "          Delete Pipeline\n" +
+                "                    \n" +
+                "          \n" +
+                "      ";
 
-        WebElement modal = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='jenkins']/dialog")));
-        actions.moveToElement(modal).pause(java.time.Duration.ofSeconds(2)).perform();
-        WebElement yesButton = modal.findElement(By.xpath("//*[@id='jenkins']/dialog/div[3]/button[1]"));
+        WebElement dropdown = (WebElement) js.executeScript(
+                "return Array.from(document.querySelectorAll('*')).find(element => " +
+                        "element.textContent.trim().replace(/\\s+/g, ' ') === arguments[0]);", searchText);
+
+        if (dropdown != null) {
+            actions.moveToElement(dropdown)
+                    .pause(java.time.Duration.ofSeconds(2))
+                    .click()
+                    .perform();
+        } else {
+            System.out.println("Элемент с текстом '" + searchText + "' не найден.");
+        }
+
+        WebElement dropDownDel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='tippy-6']/div/div/div")));
+        actions.moveToElement(dropDownDel)
+                .pause(java.time.Duration.ofSeconds(2)).click().perform();
+
+        WebElement modalWin = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='jenkins']/dialog")));
+        actions.moveToElement(modalWin).pause(java.time.Duration.ofSeconds(2)).perform();
+        WebElement yesButton = modalWin.findElement(By.xpath("//*[@id='jenkins']/dialog/div[3]/button[1]"));
         yesButton.click();
 
         boolean isElementInvisible = wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id='tippy-6']/div/div/div/button[2]")));
