a234c16aacc4cbfc68831999079f5a3eb193a343
Eugene Sh <48071321+Ev9eny@users.noreply.github.com>
2024-11-10 23:07:34 -0500

    adding project description tests (#652)
    * test_add_description
    
    * delete_project_description
    
    * preview

--- src/test/java/school/redrover/FreestyleProject1Test.java
+++ src/test/java/school/redrover/FreestyleProject1Test.java
@@ -1,5 +1,6 @@
 package school.redrover;
 
+import com.beust.ah.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -9,11 +10,24 @@ import school.redrover.runner.BaseTest;
 
 public class FreestyleProject1Test extends BaseTest {
     private static final String NEW_FREESTYLE_PROJECT_NAME = "New freestyle project";
+    private static final String DESCRIPTION = "Some description";
     private void createFreestyleProject() {
         getDriver().findElement(By.xpath("//*[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//*[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.name("name")).sendKeys(NEW_FREESTYLE_PROJECT_NAME);
         getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.name("Submit")).click();
+
+    }
+    private void createFreestyleProjectWithDescription() {
+        createFreestyleProject();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+        Actions actions = new Actions(getDriver());
+        WebElement element = getDriver().findElement(By
+                .xpath("//span[contains(text(),'" + NEW_FREESTYLE_PROJECT_NAME + "')]"));
+        actions.moveToElement(element).click().perform();
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.name("description")).sendKeys(DESCRIPTION);
         getDriver().findElement(By.name("Submit")).click();
 
     }
@@ -56,5 +70,37 @@ public class FreestyleProject1Test extends BaseTest {
         getDriver().findElement(By.name("Submit")).click();
         String projectName = getDriver().findElement(By.xpath("//*[@class='job-index-headline page-headline']")).getText();
         Assert.assertEquals(projectName, "Renamed freestyle project");
+    }
+
+    @Test
+    public void testAddFreestyleProjectDescription() {
+        createFreestyleProjectWithDescription();
+        String description = getDriver().findElement(By.id("description")).getText();
+        Assert.assertEquals(description, DESCRIPTION);
+    }
+
+    @Test
+    public void testDeleteFreestyleProjectDescription() {
+        createFreestyleProjectWithDescription();
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.name("description")).clear();
+        getDriver().findElement(By.name("Submit")).click();
+        String description = getDriver().findElement(By.id("description")).getText();
+        Assert.assertEquals(description, "");
+    }
+
+    @Test
+    public void testFreestyleProjectDescriptionPreview() {
+        createFreestyleProject();
+        getDriver().findElement(By.id("jenkins-name-icon")).click();
+        Actions actions = new Actions(getDriver());
+        WebElement element = getDriver().findElement(By
+                .xpath("//span[contains(text(),'" + NEW_FREESTYLE_PROJECT_NAME + "')]"));
+        actions.moveToElement(element).click().perform();
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.name("description")).sendKeys(DESCRIPTION);
+        getDriver().findElement(By.className("textarea-show-preview")).click();
+        String preview = getDriver().findElement(By.className("textarea-preview")).getText();
+        Assert.assertEquals(preview, DESCRIPTION);
     }
 }
