dc74f4988d8777af564c4260ec8f92278406ad4c
Mazooha <matveytetyushkin@mail.ru>
2024-11-11 15:03:08 +0500

    refactor CreatePipelineInFolderTest

--- src/test/java/school/redrover/CreatePipelineInFolderTest.java
+++ src/test/java/school/redrover/CreatePipelineInFolderTest.java
@@ -12,38 +12,30 @@ import java.time.Duration;
 public class CreatePipelineInFolderTest extends BaseTest {
 
     @Test
-    public void testCreateFolderAndPipeline() throws InterruptedException {
-        final String expectedFolderName = "FirstFolder";
-        final String expectedPipelineName = "FirstPipeline";
+    public void testCreateFolderAndPipeline() {
+        final String folderName = "FirstFolder";
+        final String pipelineName = "FirstPipeline";
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
 
         getDriver().findElement(By.xpath("//*[@id='main-panel']/div[2]/div/section[1]/ul/li/a")).click();
         getDriver().findElement(By.className("jenkins-input")).clear();
-        getDriver().findElement(By.className("jenkins-input")).sendKeys(expectedFolderName);
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(folderName);
         getDriver().findElement(By.xpath("//*[@id='j-add-item-type-nested-projects']/ul/li[1]")).click();
-//        Thread.sleep(2000);
         wait.until(ExpectedConditions.elementToBeClickable(By.id("ok-button"))).click();
-//        Thread.sleep(2000);
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='Submit']"))).click();
 
-        String folderName = getDriver().findElement(By.tagName("h1")).getText();
+        String actualFolderName = getDriver().findElement(By.tagName("h1")).getText();
 
         getDriver().findElement(By.className("content-block")).click();
         getDriver().findElement(By.className("jenkins-input")).clear();
-        getDriver().findElement(By.className("jenkins-input")).sendKeys(expectedPipelineName);
-        getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[2]")).click();
-//        Thread.sleep(2000);
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(pipelineName);
+        getDriver().findElement(By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li")).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.id("ok-button"))).click();
-//        getDriver().findElement(By.id("ok-button")).click();
-//        Thread.sleep(2000);
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='Submit']"))).click();
-//        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        String pipelineName = getDriver().findElement(By.tagName("h1")).getText();
+        String actualPipelineName = getDriver().findElement(By.tagName("h1")).getText();
 
-        Assert.assertEquals(folderName, expectedFolderName);
-        Assert.assertEquals(pipelineName, expectedPipelineName);
-
-//        Thread.sleep(5000);
+        Assert.assertEquals(actualFolderName, folderName);
+        Assert.assertEquals(actualPipelineName, pipelineName);
     }
 }
