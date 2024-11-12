d7846bdf613de800d954ebd3d5666386a60e3bd0
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-11-12 18:10:52 +0100

    Ignore testAddDescriptionOnProjectStatusPage

--- src/test/java/school/redrover/FreestyleProject3Test.java
+++ src/test/java/school/redrover/FreestyleProject3Test.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import java.time.Duration;
@@ -73,7 +74,8 @@ public class FreestyleProject3Test extends BaseTest {
         Assert.assertEquals(actualName, PROJECT_NAME);
     }
 
-    
+
+    @Ignore
     @Test
     public void testAddDescriptionOnProjectStatusPage() {
         createProjectViaSidebarMenu(PROJECT_NAME);
