924e5844bea55879f18f89b985ac03e571dbe2a9
Max13Tester <legem1306@yandex.ru>
2024-11-12 20:55:09 +0300

    add Thread.sleep

--- src/test/java/school/redrover/WelcomeTest.java
+++ src/test/java/school/redrover/WelcomeTest.java
@@ -7,10 +7,13 @@ import school.redrover.runner.BaseTest;
 
 public class WelcomeTest extends BaseTest {
     @Test
-    public void testWelcome() {
+    public void testWelcome() throws InterruptedException {
+
         String welcomeStr = getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText();
 
-        Assert.assertEquals(welcomeStr, "Welcome to Jenkins!");
+        Thread.sleep(2000);
+
+        Assert.assertEquals(welcomeStr, "Добро пожаловать в Jenkins!");
     }
 }
 
