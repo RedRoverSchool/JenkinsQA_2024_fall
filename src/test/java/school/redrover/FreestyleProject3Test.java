c08c1d658e7d06cecd444afd1e07c922c4ea2190
kas-113 <allakravtsova.113@gmail.com>
2024-11-13 01:36:29 +0300

    remove ignore and add wait for create method

--- src/test/java/school/redrover/FreestyleProject3Test.java
+++ src/test/java/school/redrover/FreestyleProject3Test.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import java.time.Duration;
@@ -23,7 +22,10 @@ public class FreestyleProject3Test extends BaseTest {
         getDriver().findElement(By.id("name")).sendKeys(projectName);
         getDriver().findElement(By.xpath("//li[contains(@class, 'FreeStyleProject')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//button[@name='Submit']"))).click();
     }
 
     private void addDescriptionOnProjectStatusPage(String description) {
@@ -76,8 +78,6 @@ public class FreestyleProject3Test extends BaseTest {
         Assert.assertEquals(actualName, PROJECT_NAME);
     }
 
-
-    @Ignore
     @Test
     public void testAddDescriptionOnProjectStatusPage() {
         createProjectViaSidebarMenu(PROJECT_NAME);
