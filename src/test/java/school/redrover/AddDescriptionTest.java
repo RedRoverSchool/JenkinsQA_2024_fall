261f0d972b39ee9d4fc86e5c28fe30c7aa19613a
Natasha <natashashimkiv@gmail.com>
2024-11-13 20:25:12 +0400

    Corrected the test 2.0

--- src/test/java/school/redrover/AddDescriptionTest.java
+++ src/test/java/school/redrover/AddDescriptionTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
@@ -12,10 +11,7 @@ public class AddDescriptionTest extends BaseTest {
     public void testDescription() {
 
         getDriver().findElement(By.id("description-link")).click();
-
-        WebElement textBox = getDriver().findElement(By.className("jenkins-input"));
-        textBox.sendKeys("Hi!");
-
+        getDriver().findElement(By.className("jenkins-input")).sendKeys("Hi!");
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
         Assert.assertEquals(getDriver().findElement(
