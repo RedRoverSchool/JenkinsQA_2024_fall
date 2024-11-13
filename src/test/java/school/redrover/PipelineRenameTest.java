d1185e4451ecc443529d7e7445742b8b6e178d93
alevtinasemeniuk <alewtinni@gmail.com>
2024-11-13 00:17:26 -0800

    Update testRenamePipelineViaChevron

--- src/test/java/school/redrover/PipelineRenameTest.java
+++ src/test/java/school/redrover/PipelineRenameTest.java
@@ -5,12 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
-
-import java.time.Duration;
 
 public class PipelineRenameTest extends BaseTest {
 
@@ -31,7 +28,7 @@ public class PipelineRenameTest extends BaseTest {
         createPipelineProject(NAME_OF_PROJECT);
 
         WebElement chevronButton = getDriver().findElement(By.xpath("//td//button[@aria-expanded='false']"));
-        WebElement pipelineProject = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='job/" + NAME_OF_PROJECT + "/']")));
+        WebElement pipelineProject = getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='job/" + NAME_OF_PROJECT + "/']")));
 
         new Actions(getDriver()).moveToElement(pipelineProject)
                 .pause(2000)
