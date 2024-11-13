ebd8223032e35a527c69e04a656b1bb05796deac
alevtinasemeniuk <alewtinni@gmail.com>
2024-11-13 00:52:35 -0800

    Update testRenamePipelineViaChevron

--- src/test/java/school/redrover/PipelineRenameTest.java
+++ src/test/java/school/redrover/PipelineRenameTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import net.bytebuddy.utility.RandomString;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -28,36 +29,29 @@ public class PipelineRenameTest extends BaseTest {
         createPipelineProject(NAME_OF_PROJECT);
 
         WebElement chevronButton = getDriver().findElement(By.xpath("//td//button[@aria-expanded='false']"));
-        WebElement pipelineProject = getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='job/" + NAME_OF_PROJECT + "/']")));
+        WebElement pipelineProject = getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='job/%s/']".formatted(NAME_OF_PROJECT))));
 
         new Actions(getDriver()).moveToElement(pipelineProject)
-                .pause(2000)
+                .pause(1000)
+                .moveToElement(chevronButton)
                 .perform();
 
-        new Actions(getDriver()).moveToElement(chevronButton)
-                        .pause(1000)
-                        .moveToElement(chevronButton)
-                        .pause(1000)
-                        .click()
-                        .perform();
+        ((JavascriptExecutor) getDriver()).executeScript(
+                "arguments[0].dispatchEvent(new MouseEvent('click', {bubbles: true, cancelable: true, view: window}));",
+                chevronButton);
 
         getWait5().until(ExpectedConditions.attributeToBe(chevronButton, "aria-expanded", "true"));
 
-        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"tippy-6\"]/div/div/div/a[4]"))).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/job/%s/confirm-rename']".formatted(NAME_OF_PROJECT)))).click();
 
         WebElement renameField = getDriver().findElement(By.xpath("//input[@checkdependson='newName']"));
         renameField.clear();
         renameField.sendKeys(NEW_NAME_OF_PROJECT);
 
-        new Actions(getDriver()).moveToElement(getDriver().findElement(By.xpath("//*[@id=\"bottom-sticker\"]/div/button")))
-                        .pause(1000)
-                        .click()
-                        .perform();
-
-
+        getDriver().findElement(By.name("Submit")).click();
         getDriver().findElement(By.id("jenkins-name-icon")).click();
 
-        String actualPipeLineName = getDriver().findElement(By.xpath("//span[text()='" + NEW_NAME_OF_PROJECT + "']")).getText();
+        String actualPipeLineName = getDriver().findElement(By.xpath("//span[text()='%s']".formatted(NEW_NAME_OF_PROJECT))).getText();
 
         Assert.assertEquals(actualPipeLineName, NEW_NAME_OF_PROJECT);
     }
