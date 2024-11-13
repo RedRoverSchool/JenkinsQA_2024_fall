6333ede8c95632c7c30f6a055945ca8304db8318
Anzhelika Lashko <120133812+AnzhelikaLa@users.noreply.github.com>
2024-11-13 10:54:23 -0600

    Refactoring (#697)

--- src/test/java/school/redrover/FreestyleProject4Test.java
+++ src/test/java/school/redrover/FreestyleProject4Test.java
@@ -6,10 +6,11 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class FreestyleProject4Test extends BaseTest {
+
     private static final String PROJECT_NAME = "New_Freestyle_Project";
     private static final String PROJECT_DESCRIPTION = "About my new freestyle project";
 
-    public void createNewFreestyleProject() {
+    private void createNewFreestyleProject() {
 
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.xpath("//input[@class='jenkins-input']")).sendKeys(PROJECT_NAME);
@@ -20,29 +21,26 @@ public class FreestyleProject4Test extends BaseTest {
 
     @Test
     public void testCheckProjectName() {
+
         createNewFreestyleProject();
 
-        String actualName = getDriver()
-                .findElement(By.xpath("//h1[@class='job-index-headline page-headline']"))
-                .getText();
-
-        Assert.assertEquals(actualName, PROJECT_NAME);
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//h1[@class='job-index-headline page-headline']")).getText(),
+                PROJECT_NAME);
     }
 
     @Test
     public void testAddDescription() {
+
         createNewFreestyleProject();
 
         getDriver().findElement(By.xpath("//a[@id='description-link']")).click();
-        getDriver().findElement(By.xpath("//textarea[@name='description']"))
-                .sendKeys(PROJECT_DESCRIPTION);
+        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys(PROJECT_DESCRIPTION);
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
-        String actualDescription = getDriver()
-                .findElement(By.xpath("//*[@id='description']/div"))
-                .getText();
-
-        Assert.assertEquals(actualDescription, PROJECT_DESCRIPTION);
+        Assert.assertEquals(
+                getDriver().findElement(By.xpath("//*[@id='description']/div")).getText(),
+                PROJECT_DESCRIPTION);
     }
 }
 
