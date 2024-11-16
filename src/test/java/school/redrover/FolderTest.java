236e7211b5be36afebbbc00b3d016ac9e8494d65
AnnaLimutina <zip1061@mail.ru .>
2024-11-15 18:08:30 -0600

    check the chevron

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -97,7 +97,6 @@ public class FolderTest extends BaseTest {
                 .sendKeys("description");
 
         getDriver().findElement(By.name("Submit")).click();
-
         goToDashboard();
 
         Thread.sleep(5000);
