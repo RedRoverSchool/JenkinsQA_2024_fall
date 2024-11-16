068f82fe803bf3ca0ee5f2920a4ad3e8a14b08e7
Valentin.P <val.podkova@gmail.com>
2024-11-16 10:23:51 +0200

    Delete ignore annotations and refactor test

--- src/test/java/school/redrover/OrganizationFolder2Test.java
+++ src/test/java/school/redrover/OrganizationFolder2Test.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
+
+
 public class OrganizationFolder2Test extends BaseTest {
 
     @Test
@@ -12,7 +14,7 @@ public class OrganizationFolder2Test extends BaseTest {
         getDriver().findElement(By.xpath("//a[.='New Item']")).click();
         getDriver().findElement(By.id("name")).click();
         getDriver().findElement(By.id("name")).sendKeys("New Organization Folder");
-        getDriver().findElement(By.xpath("//label[.='Organization Folder']")).click();
+        getDriver().findElement(By.xpath("//span[.='Organization Folder']")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).click();
 
