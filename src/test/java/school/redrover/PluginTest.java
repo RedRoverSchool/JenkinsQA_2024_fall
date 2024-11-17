fa71037b9003054f838f7c50216277869ef03395
ArtsiomAzaranka <azarenkoav@gmail.com>
2024-11-17 20:56:45 +0300

    Refactoring

--- src/test/java/school/redrover/PluginTest.java
+++ src/test/java/school/redrover/PluginTest.java
@@ -9,11 +9,17 @@ import java.util.List;
 
 public class PluginTest extends BaseTest {
 
-    @Test
-    public void testNumberOfUpdatePlugin() {
+    private void goTiPluginPage() {
 
         getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
         getWait10().until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//a[@href='pluginManager']")))).click();
+
+    }
+
+    @Test
+    public void testNumberOfUpdatePlugin() {
+
+        goTiPluginPage();
 
         List<WebElement> pluginsForUpdate = getDriver().findElements(By.className("app-plugin-manager__categories"));
 
@@ -25,8 +31,7 @@ public class PluginTest extends BaseTest {
     @Test
     public void testNubmerOfAllUpdatePlugin() {
 
-        getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//a[@href='pluginManager']")))).click();
+        goTiPluginPage();
         getWait10().until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//a[@href='/manage/pluginManager/available']")))).click();
 
         List<WebElement> pluginsForUpdate = getDriver().findElements(By.className("except"));
@@ -39,8 +44,7 @@ public class PluginTest extends BaseTest {
     @Test
     public void testSearchPluginViaTag() {
 
-        getDriver().findElement(By.xpath("//a[@href = '/manage']")).click();
-        getWait10().until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//a[@href='pluginManager']")))).click();
+        goTiPluginPage();
         getWait10().until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//a[@href='/manage/pluginManager/installed']")))).click();
 
         getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("filter-box"))).sendKeys("Theme Manager");
@@ -48,7 +52,7 @@ public class PluginTest extends BaseTest {
         Assert.assertTrue(getWait5().until(ExpectedConditions.visibilityOfElementLocated
                 (By.xpath("//tr[@data-plugin-name='Theme Manager']"))).isDisplayed());
 
-        Assert.assertFalse(getDriver().findElements(By.xpath("//tr[@data-plugin-name='Theme Manager']")).isEmpty());
+        Assert.assertEquals(getDriver().findElements(By.cssSelector(".plugin.has-dependents:not(.jenkins-hidden)")).size(), 1);
 
     }
 }
