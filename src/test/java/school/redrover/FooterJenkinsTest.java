27f170495b6c213fbbb9e392c19310e4a8144146
Elena <elkaustimenko@gmail.com>
2024-11-17 23:26:16 +0300

    Change FooterJenkinsTest after review

--- src/test/java/school/redrover/FooterJenkinsTest.java
+++ src/test/java/school/redrover/FooterJenkinsTest.java
@@ -3,10 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
