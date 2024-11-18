a0b4bf764922055b041663235dedd35de8249c2d
PavelSZh <psilenkov@gmail.com>
2024-11-18 23:17:24 +0400

    Tests to configure an existing item And Also I added a function to created any type of item to TestUtils: newItemsData

--- src/test/java/school/redrover/AnyExistingItemConfigureTest.java
+++ src/test/java/school/redrover/AnyExistingItemConfigureTest.java
@@ -3,17 +3,18 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import static school.redrover.runner.TestUtils.newItemsData;
 
 public class AnyExistingItemConfigureTest extends BaseTest {
 
+    @Ignore
     @Test
     public void testExistingFolderConfiguration () {
         newItemsData(this, "NewEmptyFolder", "//*[@id='j-add-item-type-nested-projects']/ul/li[1]/div[2]/div");
 
-        //getDriver().findElement(By.xpath("//*[@id='tasks']/div[2]/span/a")).click();
         getDriver().findElement(
                 By.xpath("//*[@id='main-panel']/form/div[1]/section[2]/section/section/div[3]/div[1]/div/button")).click();
         getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.name("_.name"))).sendKeys("TestName");
