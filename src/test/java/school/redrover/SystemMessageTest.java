3a0c20816caea1e0e2548ca3a242cce0e8089958
GolkaKodi <golkdakoldon17@gmail.com>
2024-11-19 17:56:27 -0800

    add testSystemMessagePreview()

--- src/test/java/school/redrover/SystemMessageTest.java
+++ src/test/java/school/redrover/SystemMessageTest.java
@@ -1,17 +1,16 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class SystemMessageTest extends BaseTest {
 
     @Test
-    public void testSystemMessagePreview() throws InterruptedException {
+    public void testSystemMessagePreview() {
 
         getDriver().findElement(By.xpath("//a[contains(@href, '/manage')]")).click();
         getWait10().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href, 'configureSecurity')]"))).click();
@@ -21,20 +20,15 @@ public class SystemMessageTest extends BaseTest {
                 .selectByVisibleText("Safe HTML");
 
         getDriver().findElement(By.name("Apply")).click();
-        getDriver().findElement(By.name("Submit")).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
 
         getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[contains(@href, 'configure')])[1]"))).click();
+
         getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@name = 'system_message']/.."))).click();
-        Thread.sleep(3000);
+        getDriver().findElement(By.xpath("//div[@class = 'CodeMirror']/div/textarea")).sendKeys("<h2>Hello</h2>");
 
-        //getDriver().findElement(By.xpath("//textarea[@name = 'system_message']")).sendKeys("<h2>Hello</h2>");
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@previewendpoint = '/markupFormatter/previewDescription']"))).click();
 
-        WebElement textarea = getDriver().findElement(By.xpath("//textarea[@name = 'system_message']"));
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-        js.executeScript("arguments[0].value = arguments[1];", textarea, "<h2>Hello</h2>");
-
-
-        Thread.sleep(3000);
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class = 'textarea-preview']/h2")).getText(), "Hello");
     }
-
 }
