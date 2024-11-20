e8ddfdd01aa2c451eeb4674a613f524791a9ec77
Anna V <annreznik134@gmail.com>
2024-11-21 00:09:17 +0300

    check

--- src/test/java/school/redrover/CheckItemsOnPages.java
+++ src/test/java/school/redrover/CheckItemsOnPages.java
@@ -16,12 +16,7 @@ public class CheckItemsOnPages extends BaseTest {
 
     @Test
     public void checkTitlesAndButtonsOnMainPage(){
-        verifyElementText(By.tagName("h1"), "Welcome to Jenkins!");
         verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[1]/div[2]/div[2]/a[1]"), "Add description");
-        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[1]/ul[1]/li[1]/a[1]/span[1]"), "Create a job");
-        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[1]/a[1]/span[1]"), "Set up an agent");
-        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[2]/a[1]/span[1]"), "Configure a cloud");
-        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[3]/a[1]/span[1]"), "Learn more about distributed builds");
         verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/span[1]/a[1]"), "New Item");
         verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/span[1]/a[1]/span[2]"), "Build History");
         verifyElementText(By.cssSelector("a[href='/manage']"), "Manage Jenkins");
