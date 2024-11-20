edc9afd2ff1fec77ff2f08b88a2ea76d6f0a9871
Sergey.D <sergei.demyanenko@gmail.com>
2024-11-20 01:45:43 -0500

    Add method queue

--- src/test/java/school/redrover/page/ProjectPage.java
+++ src/test/java/school/redrover/page/ProjectPage.java
@@ -2,6 +2,7 @@ package school.redrover.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.base.BasePage;
 
 public class ProjectPage extends BasePage {
@@ -14,6 +15,8 @@ public class ProjectPage extends BasePage {
         getDriver().findElement(By.id("description-link")).click();
         getDriver().findElement(By.name("description")).sendKeys(text);
         getDriver().findElement(By.name("Submit")).click();
+
+        getWait2().until(ExpectedConditions.textToBe(By.id("description"), text));
 
         return this;
     }
