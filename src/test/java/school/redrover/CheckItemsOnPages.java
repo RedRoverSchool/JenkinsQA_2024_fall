02eb3deb59370ac2dfcb0dac2ac3f286a9539664
Anna V <annreznik134@gmail.com>
2024-11-20 23:27:50 +0300

    fixed locators

--- src/test/java/school/redrover/CheckItemsOnPages.java
+++ src/test/java/school/redrover/CheckItemsOnPages.java
@@ -16,15 +16,15 @@ public class CheckItemsOnPages extends BaseTest {
 
     @Test
     public void checkTitlesAndButtonsOnMainPage(){
-        verifyElementText(By.tagName("h1"), "Добро пожаловать в Jenkins!");
-        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[1]/div[2]/div[2]/a[1]"), "добавить описание");
+        verifyElementText(By.tagName("h1"), "Welcome to Jenkins!");
+        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[1]/div[2]/div[2]/a[1]"), "Add description");
         verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[1]/ul[1]/li[1]/a[1]/span[1]"), "Create a job");
         verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[1]/a[1]/span[1]"), "Set up an agent");
         verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[2]/a[1]/span[1]"), "Configure a cloud");
         verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[3]/a[1]/span[1]"), "Learn more about distributed builds");
-        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/span[1]/a[1]"), "Создать Item");
-        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/span[1]/a[1]/span[2]"), "История сборок");
-        verifyElementText(By.cssSelector("a[href='/manage']"), "Настроить Jenkins");
+        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/span[1]/a[1]"), "New Item");
+        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/span[1]/a[1]/span[2]"), "Build History");
+        verifyElementText(By.cssSelector("a[href='/manage']"), "Manage Jenkins");
         verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[4]/span[1]/a[1]/span[2]"), "My Views");
   }
 
