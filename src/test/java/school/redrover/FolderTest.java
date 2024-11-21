7d847d2a82ebb6e7ff8172b84936a67f772a8b3a
AnnaLimutina <zip1061@mail.ru .>
2024-11-21 16:40:52 -0600

    add new pages

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.page.HomePage;
 import school.redrover.page.NewItemPage;
@@ -224,7 +225,7 @@ public class FolderTest extends BaseTest {
 
     }
 
-
+    @Ignore
     @Test
     public void openBuildHistoryByChevron() {
 
