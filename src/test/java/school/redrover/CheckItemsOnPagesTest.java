0ee8e07ac7fe5bb691559ef568983d29a77e8761
Anna V <annreznik134@gmail.com>
2024-11-21 22:35:49 +0300

    added other locators

--- src/test/java/school/redrover/CheckItemsOnPagesTest.java
+++ src/test/java/school/redrover/CheckItemsOnPagesTest.java
@@ -17,8 +17,12 @@ public class CheckItemsOnPagesTest extends BaseTest {
     @Test
     public void checkTitlesAndButtonsOnMainPage(){
         verifyElementText(By.cssSelector("#description-link"), "Add description");
+        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[1]/ul[1]/li[1]/a[1]/span[1]"), "Create a job");
+        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[1]/a[1]/span[1]"), "Set up an agent");
+        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[2]/a[1]/span[1]"), "Configure a cloud");
+        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/section[2]/ul[1]/li[3]/a[1]/span[1]"), "Learn more about distributed builds");
         verifyElementText(By.cssSelector("div[id='tasks'] div:nth-child(1) span:nth-child(1) a:nth-child(1) span:nth-child(2)"), "New Item");
-        verifyElementText(By.cssSelector("body > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1) > a:nth-child(1) > span:nth-child(2)"), "Build History");
+        verifyElementText(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/span[1]/a[1]/span[2]"), "Build History");
         verifyElementText(By.cssSelector("a[href='/manage']"), "Manage Jenkins");
         verifyElementText(By.cssSelector("a[href='/me/my-views']"), "My Views");
   }
