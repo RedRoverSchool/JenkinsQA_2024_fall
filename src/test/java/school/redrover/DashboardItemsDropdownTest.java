49d060d2008a6b4ecdfde108b313e8ff119afb54
serhio <serhiosereda@gmail.com>
2024-11-21 19:58:27 +0200

    Добавление элементов из списка MngJenkins >> System config

--- src/test/java/school/redrover/DashboardItemsDropdownTest.java
+++ src/test/java/school/redrover/DashboardItemsDropdownTest.java
@@ -106,6 +106,141 @@ public class DashboardItemsDropdownTest extends BaseTest {
     }
 
     @Test
+    public void testMngJenkinsTools() {
+        Actions actions = new Actions(getDriver());
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+        WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), buttonDropdown);
+
+        List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("#tippy-3 > div > div > div > a")));
+
+        WebElement mngJenkins = listDD.get(2);
+        actions.moveToElement(mngJenkins).perform();
+
+        List<WebElement> mngJenkinsList = getDriver().findElements(
+                By.cssSelector("#tippy-6 > div > div > div > a"));
+        WebElement tools = mngJenkinsList.get(1);
+        actions.moveToElement(tools).click().perform();
+
+        String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/manage/configureTools$"), "Page is invalid");
+    }
+
+    @Test
+    public void testMngJenkinsPlugins() {
+        Actions actions = new Actions(getDriver());
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+        WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), buttonDropdown);
+
+        List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("#tippy-3 > div > div > div > a")));
+
+        WebElement mngJenkins = listDD.get(2);
+        actions.moveToElement(mngJenkins).perform();
+
+        List<WebElement> mngJenkinsList = getDriver().findElements(
+                By.cssSelector("#tippy-6 > div > div > div > a"));
+        WebElement plugins = mngJenkinsList.get(2);
+        actions.moveToElement(plugins).click().perform();
+
+        String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/manage/pluginManager$"), "Page is invalid");
+    }
+
+    @Test
+    public void testMngJenkinsNodes() {
+        Actions actions = new Actions(getDriver());
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+        WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), buttonDropdown);
+
+        List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("#tippy-3 > div > div > div > a")));
+
+        WebElement mngJenkins = listDD.get(2);
+        actions.moveToElement(mngJenkins).perform();
+
+        List<WebElement> mngJenkinsList = getDriver().findElements(
+                By.cssSelector("#tippy-6 > div > div > div > a"));
+        WebElement nodes = mngJenkinsList.get(3);
+        actions.moveToElement(nodes).click().perform();
+
+        String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/manage/computer$"), "Page is invalid");
+    }
+
+    @Test
+    public void testMngJenkinsClouds() {
+        Actions actions = new Actions(getDriver());
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+        WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), buttonDropdown);
+
+        List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("#tippy-3 > div > div > div > a")));
+
+        WebElement mngJenkins = listDD.get(2);
+        actions.moveToElement(mngJenkins).perform();
+
+        List<WebElement> mngJenkinsList = getDriver().findElements(
+                By.cssSelector("#tippy-6 > div > div > div > a"));
+        WebElement clouds = mngJenkinsList.get(4);
+        actions.moveToElement(clouds).click().perform();
+
+        String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/manage/cloud$"), "Page is invalid");
+    }
+
+    @Test
+    public void testMngJenkinsAppearance() {
+        Actions actions = new Actions(getDriver());
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+        WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), buttonDropdown);
+
+        List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("#tippy-3 > div > div > div > a")));
+
+        WebElement mngJenkins = listDD.get(2);
+        actions.moveToElement(mngJenkins).perform();
+
+        List<WebElement> mngJenkinsList = getDriver().findElements(
+                By.cssSelector("#tippy-6 > div > div > div > a"));
+        WebElement appearance = mngJenkinsList.get(5);
+        actions.moveToElement(appearance).click().perform();
+
+        String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/manage/appearance$"), "Page is invalid");
+    }
+
+    @Test
     public void testMyViews() {
     Actions actions = new Actions(getDriver());
 
