4af0d9c93b6dd65cd995f5fbfa32b3faf451a2fd
serhio <serhiosereda@gmail.com>
2024-11-21 19:24:45 +0200

    Добавление других элементов из дропдауна. Элементы из списка дроп дауна Dashboard, #tippy3

--- src/test/java/school/redrover/DashboardItemsDropdownTest.java
+++ src/test/java/school/redrover/DashboardItemsDropdownTest.java
@@ -30,8 +30,74 @@ public class DashboardItemsDropdownTest extends BaseTest {
         WebElement newItem = listDD.get(0);
         actions.moveToElement(newItem).click().perform();
 
-        String expectedTitle = "New Item [Jenkins]";
-        String actualTitle = getDriver().getTitle();
-        Assert.assertEquals(actualTitle, expectedTitle, "Page is invalid");
+        String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/view/all/newJob$"), "Page is invalid");
     }
+
+    @Test
+    public void testBuildHistory() {
+        Actions actions = new Actions(getDriver());
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+        WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), buttonDropdown);
+
+        List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("#tippy-3 > div > div > div > a")));
+
+        WebElement buildHistory = listDD.get(1);
+        actions.moveToElement(buildHistory).click().perform();
+
+        String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/view/all/builds$"), "Page is invalid");
+    }
+
+    @Test
+    public void testManageJenkins() {
+        Actions actions = new Actions(getDriver());
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+        WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), buttonDropdown);
+
+        List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("#tippy-3 > div > div > div > a")));
+
+        WebElement mngJenkins = listDD.get(2);
+        actions.moveToElement(mngJenkins).click().perform();
+
+        String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/manage(/)?$"), "Page is invalid");
+    }
+
+    @Test
+    public void testMyViews() {
+    Actions actions = new Actions(getDriver());
+
+    WebElement dashboardButton = getDriver().findElement(
+            By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+    WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+            By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), buttonDropdown);
+
+    List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+            By.cssSelector("#tippy-3 > div > div > div > a")));
+
+    WebElement myViews = listDD.get(3);
+        actions.moveToElement(myViews).click().perform();
+
+    String actualUrl = getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.matches(".*\\/my-views/view/all(/)?$"), "Page is invalid");
+}
+
 }
