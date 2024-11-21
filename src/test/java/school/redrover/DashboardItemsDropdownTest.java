6df568b1f85741fac6e55427057e5301377916f2
serhio <serhiosereda@gmail.com>
2024-11-21 17:46:22 +0200

    Изменено ожидание по новым конфигам Ласт тест с помощью экшн без js

---
+++ src/test/java/school/redrover/DashboardItemsDropdownTest.java
@@ -0,0 +1,32 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
+
+import java.util.List;
+
+public class DashboardItemsDropdownTest extends BaseTest {
+
+    @Test
+    public void testNewItem() {
+        Actions actions = new Actions(getDriver());
+
+        WebElement dashboardButton = getDriver().findElement(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item"));
+        actions.moveToElement(dashboardButton).perform();
+
+        WebElement buttonDropdown = getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#breadcrumbs > li.jenkins-breadcrumbs__list-item > a > button")));
+        actions.moveToElement(buttonDropdown).click().perform();
+
+        List<WebElement> listDD = getWait10().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("#tippy-3 > div > div > div > a")));
+
+        WebElement newItem = listDD.get(0);
+        actions.moveToElement(newItem).click().perform();
+    }
+}
