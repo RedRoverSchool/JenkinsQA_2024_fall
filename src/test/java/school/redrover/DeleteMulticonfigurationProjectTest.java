25ff0245e53141fe46a35ad6e976e351b3a7bd3a
alevtinasemeniuk <alewtinni@gmail.com>
2024-11-22 11:25:24 -0800

    1.Added new instance into enum of NewItemPage 2.Added tests into DeleteMulticonfigurationProjectTest

--- src/test/java/school/redrover/DeleteMulticonfigurationProjectTest.java
+++ src/test/java/school/redrover/DeleteMulticonfigurationProjectTest.java
@@ -1,11 +1,73 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.page.HomePage;
+import school.redrover.page.NewItemPage;
+import school.redrover.runner.BaseTest;
+import school.redrover.runner.TestUtils;
 
-public class DeleteMulticonfigurationProjectTest {
+public class DeleteMulticonfigurationProjectTest extends BaseTest {
+
+    private static final String PROJECT_NAME = "MulticonfigurationProject";
+
+    private HomePage createProject(String projectName) {
+        HomePage homepage = new HomePage(getDriver())
+                .clickNewItem()
+                .enterItemName(projectName)
+                .selectProjectTypeAndSave(NewItemPage.ItemType.MULTICONFIGURATION_PROJECT)
+                .goToDashboard();
+        return homepage;
+    }
+
+    private void openDropdownViaChevron(String projectName) {
+        new Actions(getDriver()).moveToElement(getDriver().findElement(
+                        By.xpath("//a[@href='job/%s/']/span".formatted(projectName))))
+                .pause(1000)
+                .perform();
+
+        WebElement chevron = getDriver().findElement(By.xpath("//td//button[@aria-expanded='false']"));
+        TestUtils.moveAndClickWithJavaScript(getDriver(), chevron);
+        getWait5().until(ExpectedConditions.attributeToBe(chevron, "aria-expanded", "true"));
+    }
 
     @Test
-    public void test () {
+    public void testPopupForDeletionOnProjectPage() {
+        createProject(PROJECT_NAME).openProject(PROJECT_NAME);
 
+        getDriver().findElement(
+                By.xpath("//div[@id='side-panel']//span[text()='Delete Multi-configuration project']")).click();
+
+        WebElement deletionPopup = getWait5().until(ExpectedConditions.visibilityOf(getDriver().findElement(
+                By.xpath("//footer/following-sibling::dialog"))));
+
+        Assert.assertTrue(deletionPopup.isDisplayed());
     }
+
+    @Test(dependsOnMethods = "testPopupForDeletionOnProjectPage")
+    public void testPopupForDeletionOnMainPage() {
+        openDropdownViaChevron(PROJECT_NAME);
+        getDriver().findElement(By.xpath("//button[@href='/job/%s/doDelete']".formatted(PROJECT_NAME))).click();
+
+        WebElement deletionPopup = getWait5().until(ExpectedConditions.visibilityOf(getDriver().findElement(
+                By.xpath("//footer/following-sibling::dialog"))));
+        Assert.assertTrue(deletionPopup.isDisplayed());
+    }
+
+    @Test(dependsOnMethods = "testPopupForDeletionOnProjectPage")
+    public void testPopupForDeletionOnMyViews() {
+        getDriver().findElement(By.xpath("//a[@href='/me/my-views']")).click();
+
+        openDropdownViaChevron(PROJECT_NAME);
+        getDriver().findElement(By.xpath("//button[@href='/me/my-views/view/all/job/%s/doDelete']".formatted(PROJECT_NAME))).click();
+
+        WebElement deletionPopup = getWait5().until(ExpectedConditions.visibilityOf(getDriver().findElement(
+                By.xpath("//footer/following-sibling::dialog"))));
+        Assert.assertTrue(deletionPopup.isDisplayed());
+    }
+
 }
