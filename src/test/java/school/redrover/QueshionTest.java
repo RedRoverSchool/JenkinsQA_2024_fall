95ffc1f04f719fe3b5386578261b23d8f8224ef7
OlgaAn8 <o.antropova-skv@yandex.ru>
2024-11-23 19:43:07 +0300

    Add class QueshionTest.java

--- src/test/java/school/redrover/QueshionTest.java
+++ src/test/java/school/redrover/QueshionTest.java
@@ -2,28 +2,40 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
-    public class QueshionTest extends BaseTest {
+import java.time.Duration;
+
+import static java.time.Duration.ofSeconds;
+
+public class QueshionTest extends BaseTest {
 
         @Test
-        public void testDescription() {
+                public void testDescription() {
 
-            String descriptionStr = getDriver().findElement(By.cssSelector("#main-panel > div:nth-child(3) > div > p")).getText();
-            Assert.assertEquals(descriptionStr, "This page is where your Jenkins jobs will be displayed. To get started, you can set up distributed builds or start building a software project.");
+            String descriptionStr = getDriver().findElement(By.cssSelector("#main-panel > div:nth-child(3) > div > p"))
+                    .getText();
+            Assert.assertEquals(descriptionStr, "This page is where your Jenkins jobs will be displayed." +
+                    " To get started, you can set up distributed builds or start building a software project.");
         }
-
         @Test
-        public void testQueshion() throws InterruptedException{
+        public void testQueshion() throws InterruptedException {
 
-            WebElement search = getDriver().findElement(By.xpath("//*[@id=\"searchform\"]/a"));
-            search.click();
-            Thread.sleep(300);
-            String QueshionStr = getDriver().findElement(By.xpath("//*[@id=\"search-box\"]")).getText();
-            Assert.assertEquals(QueshionStr, "Search Box");
+            WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"searchform\"]/a")))
+                    .click();
+
+
+            Assert.assertEquals(
+                    getDriver().findElement(By.xpath("//*[@id=\"search-box\"]")).getText(),
+                    "Search Box");
+
         }
 
     }
