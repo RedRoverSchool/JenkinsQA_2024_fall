a5644c841d1fec0cd82c7d1e23ad0c30043c0239
AllaBird <146133797+AllaBird@users.noreply.github.com>
2024-11-25 01:43:05 -0500

    Refactor to POM (#998)

--- src/test/java/school/redrover/page/PipelineProjectPage.java
+++ src/test/java/school/redrover/page/PipelineProjectPage.java
@@ -2,6 +2,7 @@ package school.redrover.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.base.BasePage;
 
 public class PipelineProjectPage extends BasePage {
@@ -13,5 +14,15 @@ public class PipelineProjectPage extends BasePage {
         getDriver().findElement(By.id("jenkins-home-link")).click();
 
         return new HomePage(getDriver());
+    }
+
+    public String getWarningDisabledMessage() {
+        return getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//form[@id='enable-project']"))).getText().split("\n")[0];
+    }
+
+    public String getStatusButtonText() {
+        return  getWait2().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//form[@id='enable-project']/button[@name='Submit']"))).getText();
     }
 }
