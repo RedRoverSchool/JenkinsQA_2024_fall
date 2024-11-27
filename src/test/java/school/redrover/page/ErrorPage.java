a1b6e4f575eb95f45bfa4f285488216cd63dd08e
Roman Burlaka <burlakarv@gmail.com>
2024-11-27 21:06:58 +0100

    RF to POM

--- src/test/java/school/redrover/page/ErrorPage.java
+++ src/test/java/school/redrover/page/ErrorPage.java
@@ -2,6 +2,7 @@ package school.redrover.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.base.BasePage;
 
 public class ErrorPage extends BasePage {
@@ -11,6 +12,7 @@ public class ErrorPage extends BasePage {
     By errorMessage = By.tagName("p");
 
     public String getErrorMessage() {
+        getWait5().until(ExpectedConditions.visibilityOf(getDriver().findElement(errorMessage)));
         return getDriver().findElement(errorMessage).getText();
     }
 }
