18db58bfcc2d46e8cf5b1080b37d013913307d0f
YevgenyN <163114876+evgnick@users.noreply.github.com>
2024-11-28 06:50:10 +0300

    Refactoring BaseProjectPage (#1132)
    Co-authored-by: YevgenyN <evgenii.tst@gmail.com>

--- src/test/java/school/redrover/page/FreestyleProjectPage.java
+++ src/test/java/school/redrover/page/FreestyleProjectPage.java
@@ -3,15 +3,10 @@ package school.redrover.page;
 import org.openqa.selenium.WebDriver;
 import school.redrover.page.base.BaseProjectPage;
 
-public class FreestyleProjectPage extends BaseProjectPage {
+public class FreestyleProjectPage extends BaseProjectPage<FreestyleProjectPage> {
 
     public FreestyleProjectPage(WebDriver driver) {
         super(driver);
-    }
-
-    @Override
-    protected FreestyleProjectPage createProjectPage() {
-        return new FreestyleProjectPage(getDriver());
     }
 
 }
