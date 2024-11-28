064682ca1f4473867c6f9b4624bb420fd2f07023
Elena Pakhomova <140397499+pakhomova-ev@users.noreply.github.com>
2024-11-28 13:33:02 +0300

    RF: testWarningMessageOnRenameProjectPage -> POM (#1148)
    * RF: https://github.com/RedRoverSchool/JenkinsQA_2024_fall/issues/1107 to POM

--- src/test/java/school/redrover/page/PipelineRenamePage.java
+++ src/test/java/school/redrover/page/PipelineRenamePage.java
@@ -2,6 +2,7 @@ package school.redrover.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.base.BasePage;
 
 public class PipelineRenamePage extends BasePage {
@@ -27,5 +28,10 @@ public class PipelineRenamePage extends BasePage {
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
 
         return new ErrorPage(getDriver());
+    }
+
+    public String getWarningMessage() {
+        return getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//div[@class='validation-error-area validation-error-area--visible']"))).getText();
     }
 }
