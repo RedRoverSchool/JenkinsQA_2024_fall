817b94bfe83135a6368aaf6de85398e90082d85f
AllaBird <allavorobieva@yahoo.com>
2024-11-30 23:02:31 -0500

    Refactor ViewTest to POM

--- src/test/java/school/redrover/page/NewViewPage.java
+++ src/test/java/school/redrover/page/NewViewPage.java
@@ -2,7 +2,13 @@ package school.redrover.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.base.BasePage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class NewViewPage extends BasePage {
     public NewViewPage(WebDriver driver) {
@@ -27,5 +33,40 @@ public class NewViewPage extends BasePage {
     public boolean isCreateButtonEnabled() {
 
         return getDriver().findElement(By.cssSelector("button[id='ok']")).isEnabled();
+    }
+
+    public NewViewPage typeNameIntoInputField(String name) {
+        WebElement inputNameField = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='name']")));
+        inputNameField.sendKeys(name);
+
+        return this;
+    }
+
+    public NewViewPage selectListViewType() {
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='hudson.model.ListView']"))).click();
+
+        return this;
+    }
+
+    public ListViewConfigPage clickCreateButton() {
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        return new ListViewConfigPage(getDriver());
+    }
+
+    public Map<String, String> getTypeToDescriptionMap() {
+        List<WebElement> typeOptionList = getWait2().until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='jenkins-radio']"))
+        );
+
+        Map<String, String> typeToDescriptionMap = new HashMap<>();
+        for (WebElement option : typeOptionList) {
+            String optionText = option.findElement(By.xpath("./label")).getText();
+            String descriptionText = option.findElement(By.xpath("./div")).getText();
+
+            typeToDescriptionMap.put(optionText, descriptionText);
+        }
+
+        return typeToDescriptionMap;
     }
 }
