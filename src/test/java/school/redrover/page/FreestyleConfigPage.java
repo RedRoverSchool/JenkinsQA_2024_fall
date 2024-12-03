a540126c363f2344e525fb1a0d531ff135dbc18e
kas-113 <allakravtsova.113@gmail.com>
2024-12-03 11:33:26 +0300

    debugging with sleep testConfigureProjectAddBuildStepsExecuteShellCommand

--- src/test/java/school/redrover/page/FreestyleConfigPage.java
+++ src/test/java/school/redrover/page/FreestyleConfigPage.java
@@ -11,7 +11,7 @@ import school.redrover.runner.TestUtils;
 public class FreestyleConfigPage extends BaseConfigPage<FreestyleConfigPage, FreestyleProjectPage> {
 
     private static final By CODE_MIRROR_FIELD = By.xpath("//div[@class='CodeMirror']");
-
+//div[@class='CodeMirror']//textarea
     public FreestyleConfigPage(WebDriver driver) {
         super(driver);
     }
