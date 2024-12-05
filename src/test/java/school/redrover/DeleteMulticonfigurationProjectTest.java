f3073ca3cd5c2f1d77588d25fa727f7ea230e3be
YevgenyN <evgenii.tst@gmail.com>
2024-12-05 14:46:10 +0300

    Refactoring failed test https://github.com/RedRoverSchool/JenkinsQA_2024_fall/issues/1376

--- src/test/java/school/redrover/DeleteMulticonfigurationProjectTest.java
+++ src/test/java/school/redrover/DeleteMulticonfigurationProjectTest.java
@@ -77,7 +77,7 @@ public class DeleteMulticonfigurationProjectTest extends BaseTest {
 
         getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
 
-        Assert.assertTrue(getDriver().getPageSource().contains("Welcome to Jenkins"));
+        Assert.assertEquals(getDriver().findElement(By.cssSelector(".empty-state-block > h1")).getText(), "Welcome to Jenkins");
     }
 
     @Test
