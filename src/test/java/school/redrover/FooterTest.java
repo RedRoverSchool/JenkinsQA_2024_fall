e2d56a02bbacb35f85a364efc32556eff0f8010b
NikitaS-95 <nserov95@gmail.com>
2024-12-05 19:14:06 +0500

    Remove my old tests and refactor to POM 2 tests in FooterTest https://github.com/orgs/RedRoverSchool/projects/3/views/1?filterQuery=assignee%3A%40me&pane=issue&itemId=89861913

--- src/test/java/school/redrover/FooterTest.java
+++ src/test/java/school/redrover/FooterTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.page.HomePage;
@@ -32,31 +31,28 @@ public class FooterTest extends BaseTest {
     @Test
     public void checkVersionMainPage() {
         Assert.assertEquals(getDriver().findElement(By.xpath("//button[@type='button']")).
-                getText(), "Jenkins 2.462.3");
+                getText(), EXPECTED_JENKINS_VERSION);
     }
 
     @Test
     public void testVerifyTitleDescription() {
-        getDriver().findElement(By.xpath("//button[@type='button']")).click();
-        getDriver().findElement(By.xpath("//*[@id='tippy-1']/div/div/div/a[1]")).click();
+        String descriptionTitle = new HomePage(getDriver())
+                .clickJenkinsVersionButton()
+                .gotoAboutPage()
+                .getAboutDescription();
 
-        getDriver().findElement(By.xpath("//*[@id='main-panel']/p")).getText();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='main-panel']/p")).getText(),
+        Assert.assertEquals(descriptionTitle,
                 "The leading open source automation server which enables " +
                         "developers around the world to reliably build, test, and deploy their software.");
     }
 
     @Test
     public void testCheckNumberOfMavenDependencies() {
-        WebElement toAboutJenkins = getDriver().findElement(By.xpath("//button[@type='button']"));
-        toAboutJenkins.click();
+        Integer numberOfDependencies = new HomePage(getDriver())
+                .clickJenkinsVersionButton()
+                .gotoAboutPage()
+                .getNumberOfMavenDependencies();
 
-        WebElement fromDropDownMenuToAboutJenkins = getDriver().findElement(By.xpath("//a[@href='/manage/about']"));
-        fromDropDownMenuToAboutJenkins.click();
-
-        int count = getDriver().findElements(By.xpath("//*[@id='main-panel']/div[4]/table/tbody/tr")).size();
-
-        Assert.assertEquals(count, 88);
+        Assert.assertEquals(numberOfDependencies, 88);
     }
 }
