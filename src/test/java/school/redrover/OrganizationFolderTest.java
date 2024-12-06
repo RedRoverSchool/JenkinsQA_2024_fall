dce58b8f078a1720730f1566b8ee5e455e4a0b1d
AllaBird <146133797+AllaBird@users.noreply.github.com>
2024-12-06 02:36:01 -0500

    RF | Organization Folder>Delete duplicates>POM #1394 (#1395)

--- src/test/java/school/redrover/OrganizationFolderTest.java
+++ src/test/java/school/redrover/OrganizationFolderTest.java
@@ -2,7 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
-import school.redrover.page.CreateNewItemPage;
 import school.redrover.page.HomePage;
 import school.redrover.runner.BaseTest;
 
@@ -19,9 +18,24 @@ import java.time.Duration;
 import java.util.List;
 
 public class OrganizationFolderTest extends BaseTest {
-    private static final String ITEM_NAME = "Item Name";
-    private static final String DISPLAY_NAME = "Display Name";
+    private static final String FOLDER_NAME = "FolderName";
+    private static final String DISPLAY_NAME = "DisplayName";
     private static final String DESCRIPTION = "Description";
+    private static final String NEW_DISPLAY_NAME = "New Name Organization Folder";
+    private static final String NEW_DESCRIPTION = "New Description Organization Folder";
+
+    private void clickElement(By by) {
+        getDriver().findElement(by).click();
+    }
+
+    private String textElement(By by) {
+        return getDriver().findElement(by).getText();
+    }
+
+    private void goConfigure() {
+        clickElement(By.xpath("//td/a[@class='jenkins-table__link model-link inside']"));
+        clickElement(By.xpath("//a[@href='./configure']"));
+    }
 
     private void scrollPage() {
 
@@ -30,11 +44,100 @@ public class OrganizationFolderTest extends BaseTest {
     }
 
     @Test
+    public void testCreate() {
+        List<String> projectList = new HomePage(getDriver())
+                .clickCreateJob()
+                .enterItemName(FOLDER_NAME)
+                .selectOrganizationFolderAndClickOk()
+                .gotoHomePage()
+                .getItemList();
+
+        Assert.assertListContainsObject(projectList, FOLDER_NAME,
+                "Project is not created");
+    }
+
+    @Test(dependsOnMethods = "testCreate")
+    public void testAddDisplayName() {
+        String displayName = new HomePage(getDriver())
+                .clickItemName()
+                .clickConfigure()
+                .setDisplayName(DISPLAY_NAME)
+                .clickSaveButton()
+                .getName();
+
+        Assert.assertEquals(displayName, DISPLAY_NAME);
+    }
+
+    @Test(dependsOnMethods = {"testAddDisplayName"})
+    public void testEditDisplayName() {
+        String newDisplayName = new HomePage(getDriver())
+                .openOrganisationFolderProject(DISPLAY_NAME)
+                .clickConfigureSidebar(FOLDER_NAME)
+                .editDisplayName(NEW_DISPLAY_NAME)
+                .clickSaveButton()
+                .getName();
+
+        Assert.assertEquals(newDisplayName, NEW_DISPLAY_NAME);
+    }
+
+    @Test(dependsOnMethods = "testCreate")
+    public void testAddDescription() {
+        goConfigure();
+        getDriver().findElement(By.name("_.description")).sendKeys(DESCRIPTION);
+        clickElement(By.name("Submit"));
+
+        Assert.assertEquals(textElement(By.id("view-message")), DESCRIPTION);
+    }
+
+    @Test(dependsOnMethods = {"testAddDescription"})
+    public void testEditDescription() {
+        goConfigure();
+        getDriver().findElement(By.name("_.description")).sendKeys(Keys.LEFT_CONTROL + "a");
+        getDriver().findElement(By.name("_.description")).sendKeys(NEW_DESCRIPTION);
+        clickElement(By.name("Submit"));
+
+        Assert.assertEquals(textElement(By.id("view-message")), NEW_DESCRIPTION);
+    }
+
+    @Test(dependsOnMethods = {"testEditDescription", "testEditDisplayName"})
+    public void testDelete() {
+        clickElement(By.xpath("//td/a[@class='jenkins-table__link model-link inside']"));
+        clickElement(By.xpath("//a[@data-title='Delete Organization Folder']"));
+        clickElement(By.xpath("//button[@data-id='ok']"));
+
+        Assert.assertEquals(textElement(By.tagName("h1")), "Welcome to Jenkins!");
+    }
+
+    @Ignore
+    @Test
+    public void testConfigureTheProject() {
+        goConfigure();
+        getDriver().findElement(By.name("_.displayNameOrNull")).sendKeys(Keys.LEFT_CONTROL + "a");
+        getDriver().findElement(By.name("_.displayNameOrNull")).sendKeys(DISPLAY_NAME);
+        getDriver().findElement(By.name("_.description")).sendKeys(Keys.LEFT_CONTROL + "a");
+        getDriver().findElement(By.name("_.description")).sendKeys(DESCRIPTION);
+
+        ((JavascriptExecutor) getDriver()).executeScript("window.scrollTo(0, 922);");
+
+        final WebElement repositoryStrategy = getDriver().findElement(By.xpath(
+                "//div[@class='tr jenkins-!-margin-bottom-2']/div/div/div/select"));
+        Select selectRepositorySources = new Select(repositoryStrategy);
+        selectRepositorySources.selectByValue("1");
+        clickElement(By.xpath("//label[text()='Periodically if not otherwise run']"));
+        clickElement(By.xpath("//label[text()='Discard old items']"));
+        ((JavascriptExecutor) getDriver()).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        clickElement(By.name("Submit"));
+
+        Assert.assertEquals(textElement(By.tagName("h1")), DISPLAY_NAME);
+        Assert.assertEquals(textElement(By.id("view-message")), DESCRIPTION);
+    }
+
+    @Test
     public void testTitle() {
 
         new HomePage(getDriver())
                 .clickNewItem()
-                .enterItemName(ITEM_NAME)
+                .enterItemName(FOLDER_NAME)
                 .selectOrganizationFolderAndClickOk();
 
         Assert.assertEquals(
@@ -47,7 +150,7 @@ public class OrganizationFolderTest extends BaseTest {
 
         String tooltipText = new HomePage(getDriver())
                 .clickNewItem()
-                .enterItemName(ITEM_NAME)
+                .enterItemName(FOLDER_NAME)
                 .selectOrganizationFolderAndClickOk()
                 .getTooltipGeneralText();
 
@@ -61,7 +164,7 @@ public class OrganizationFolderTest extends BaseTest {
 
         new HomePage(getDriver())
                 .clickNewItem()
-                .enterItemName(ITEM_NAME)
+                .enterItemName(FOLDER_NAME)
                 .selectOrganizationFolderAndClickOk()
                 .enterName(DISPLAY_NAME)
                 .enterDescription(DESCRIPTION)
@@ -77,7 +180,7 @@ public class OrganizationFolderTest extends BaseTest {
 
         new HomePage(getDriver())
                 .clickNewItem()
-                .enterItemName(ITEM_NAME)
+                .enterItemName(FOLDER_NAME)
                 .selectOrganizationFolderAndClickOk()
                 .enterName(DISPLAY_NAME)
                 .enterDescription(DESCRIPTION)
@@ -95,7 +198,7 @@ public class OrganizationFolderTest extends BaseTest {
         Actions actions = new Actions(getDriver());
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(FOLDER_NAME);
         scrollPage();
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
         getDriver().findElement(By.xpath("//div/button[@type='submit']")).click();
@@ -110,29 +213,10 @@ public class OrganizationFolderTest extends BaseTest {
     }
 
     @Test
-    public void testDisplayNameAfterCreation() {
-        Actions actions = new Actions(getDriver());
-
-        getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
-        scrollPage();
-        getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
-        getDriver().findElement(By.xpath("//div/button[@type='submit']")).click();
-
-        getDriver().findElement(By.xpath("//div/input[@name='_.displayNameOrNull']")).sendKeys(DISPLAY_NAME);
-        getDriver().findElement(By.xpath("//div/button[@class='jenkins-button jenkins-submit-button jenkins-button--primary ']")).click();
-
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//div/h1")).getText(),
-                DISPLAY_NAME);
-    }
-
-
-    @Test
     public void testItemNameAfterCreation() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(FOLDER_NAME);
         scrollPage();
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
         getDriver().findElement(By.xpath("//div/button[@type='submit']")).click();
@@ -141,15 +225,15 @@ public class OrganizationFolderTest extends BaseTest {
         getDriver()
                 .findElement(By.xpath("//div/button[@class='jenkins-button jenkins-submit-button jenkins-button--primary ']"))
                 .click();
-        String itemNameExpected = "Folder name: " + ITEM_NAME;
+        String itemNameExpected = "Folder name: " + FOLDER_NAME;
 
         WebDriverWait driverWait = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
         driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("main-panel")));
 
         String itemNameActual = getDriver().findElement(By.id("main-panel")).getText();
         int indexNameStart = itemNameActual.indexOf("\n");
-        int indexNameEnd = itemNameActual.indexOf("\n", indexNameStart+1);
-        String itemNameActualCut = itemNameActual.substring(indexNameStart+1, indexNameEnd);
+        int indexNameEnd = itemNameActual.indexOf("\n", indexNameStart + 1);
+        String itemNameActualCut = itemNameActual.substring(indexNameStart + 1, indexNameEnd);
 
         Assert.assertEquals(itemNameActualCut, itemNameExpected);
     }
@@ -159,7 +243,7 @@ public class OrganizationFolderTest extends BaseTest {
     public void testDescriptionAfterCreation() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(FOLDER_NAME);
         scrollPage();
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
         getDriver().findElement(By.xpath("//div/button[@type='submit']")).click();
@@ -179,7 +263,7 @@ public class OrganizationFolderTest extends BaseTest {
     public void testChangeItemName() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(FOLDER_NAME);
         scrollPage();
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
         getDriver().findElement(By.xpath("//div/button[@type='submit']")).click();
@@ -195,7 +279,7 @@ public class OrganizationFolderTest extends BaseTest {
 
         Assert.assertEquals(
                 getDriver().findElement(By.xpath("//div/h1")).getText(),
-                ITEM_NAME + " NEW");
+                FOLDER_NAME + " NEW");
     }
 
     @Test
@@ -222,7 +306,7 @@ public class OrganizationFolderTest extends BaseTest {
     public void testFolderDelete() {
 
         getDriver().findElement(By.xpath("//span/a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(ITEM_NAME);
+        getDriver().findElement(By.xpath("//div/input[@class='jenkins-input']")).sendKeys(FOLDER_NAME);
         scrollPage();
         getDriver().findElement(By.className("jenkins_branch_OrganizationFolder")).click();
         getDriver().findElement(By.xpath("//div/button[@type='submit']")).click();
