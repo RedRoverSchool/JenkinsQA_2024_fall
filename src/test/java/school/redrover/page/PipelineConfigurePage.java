e28220fbb0a656b41a5be3acbe0d15a485c8427d
kas-113 <allakravtsova.113@gmail.com>
2024-12-10 00:57:20 +0300

    RF PipelineConfigurePage add FindBy, https://github.com/orgs/RedRoverSchool/projects/3/views/1?pane=issue&itemId=90261682&issue=RedRoverSchool%7CJenkinsQA_2024_fall%7C1454

--- src/test/java/school/redrover/page/PipelineConfigurePage.java
+++ src/test/java/school/redrover/page/PipelineConfigurePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.base.BaseConfigPage;
 import school.redrover.runner.TestUtils;
@@ -14,6 +15,27 @@ import java.util.List;
 import java.util.Map;
 
 public class PipelineConfigurePage extends BaseConfigPage<PipelineConfigurePage, PipelineProjectPage> {
+
+    @FindBy(xpath = "//div[@class='task']//span[2]")
+    private List<WebElement> sidebarOptions;
+
+    @FindBy(xpath = "//div[@hashelp = 'true']//label[@class='attach-previous ']")
+    private List<WebElement> checkboxWithQuestionMarkList;
+
+    @FindBy(xpath = "//div[@hashelp = 'true']//a[@class='jenkins-help-button']")
+    private List<WebElement> questionMarkTooltipTextList;
+
+    @FindBy(xpath = "//label[@data-title='Disabled']")
+    private WebElement enableDisableToggle;
+
+    @FindBy(css = "textarea[class='ace_text-input']")
+    private WebElement scriptField;
+
+    @FindBy(id = "workflow-editor-1")
+    private WebElement editor;
+
+    @FindBy(xpath = "//span[@class = 'ace_identifier']")
+    private WebElement scriptText;
 
     public PipelineConfigurePage(WebDriver driver) {
         super(driver);
@@ -25,16 +47,10 @@ public class PipelineConfigurePage extends BaseConfigPage<PipelineConfigurePage,
     }
 
     public List<String> getSidebarConfigurationOption() {
-        return getWait2().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
-                By.xpath("//div[@class='task']//span[2]"))).stream().map(WebElement::getText).toList();
+        return getWait2().until(ExpectedConditions.visibilityOfAllElements(sidebarOptions)).stream().map(WebElement::getText).toList();
     }
 
     public Map<String, String> getCheckboxWithTooltipTextMap() {
-        List<WebElement> checkboxWithQuestionMarkList = getDriver().findElements(
-                By.xpath("//div[@hashelp = 'true']//label[@class='attach-previous ']"));
-        List<WebElement> questionMarkTooltipTextList = getDriver().findElements(
-                By.xpath("//div[@hashelp = 'true']//a[@class='jenkins-help-button']"));
-
         Map<String, String> labelToTooltipTextMap = new HashMap<>();
         for (int i = 0; i < checkboxWithQuestionMarkList.size(); i++) {
             String checkboxText = checkboxWithQuestionMarkList.get(i).getText();
@@ -46,8 +62,7 @@ public class PipelineConfigurePage extends BaseConfigPage<PipelineConfigurePage,
     }
 
     public PipelineConfigurePage clickToggleToDisableOrEnableProject() {
-        getWait5().until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//label[@data-title='Disabled']"))).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(enableDisableToggle)).click();
 
         return this;
     }
@@ -55,22 +70,21 @@ public class PipelineConfigurePage extends BaseConfigPage<PipelineConfigurePage,
     public PipelineConfigurePage addScriptToPipeline(String script) {
 
         TestUtils.scrollToBottomWithJS(getDriver());
-        getDriver().findElement(By.cssSelector("textarea[class='ace_text-input']")).sendKeys(script);
+        scriptField.sendKeys(script);
 
         return this;
     }
 
     public PipelineConfigurePage enterScriptFromFile(String script) {
-        WebElement textArea = getDriver().findElement(By.xpath("//textarea[@class='ace_text-input']"));
-        TestUtils.pasteTextWithJavaScript(getDriver(), textArea, script);
+        TestUtils.pasteTextWithJavaScript(getDriver(), scriptField, script);
 
         return this;
     }
+
     public PipelineConfigurePage pasteScript() {
         TestUtils.scrollToBottomWithJS(getDriver());
 
-        getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.id("workflow-editor-1")))
-                .click();
+        getWait2().until(ExpectedConditions.visibilityOf(editor)).click();
         new Actions(getDriver())
                 .keyDown(Keys.CONTROL).sendKeys("v").keyUp(Keys.CONTROL).build().perform();
 
@@ -78,7 +92,7 @@ public class PipelineConfigurePage extends BaseConfigPage<PipelineConfigurePage,
     }
 
     public String getScriptText() {
-        return getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class = 'ace_identifier']")))
+        return getWait2().until(ExpectedConditions.visibilityOf(scriptText))
                 .getText();
     }
 }
