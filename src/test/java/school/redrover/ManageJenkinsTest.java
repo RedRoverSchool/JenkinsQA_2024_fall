c8643c8dec6a6560bd329999599515033c263668
AllaBird <allavorobieva@yahoo.com>
2024-12-08 20:01:55 -0500

    RF | Join classes #1441

--- src/test/java/school/redrover/ManageJenkinsTest.java
+++ src/test/java/school/redrover/ManageJenkinsTest.java
@@ -375,4 +375,46 @@ public class ManageJenkinsTest extends BaseTest {
 
         Assert.assertEquals(changedColor, initialColorTheme, "Color of theme did not changed");
     }
+
+    @Ignore
+    @Test
+    public void testThemesDark() {
+        getDriver().findElement(By.cssSelector("[href='/manage']")).click();
+        getDriver().findElement(By.cssSelector("[href='appearance']")).click();
+        getDriver().findElement(By.xpath("//label[@for='radio-block-0']")).click();
+
+        getDriver().findElement(By.cssSelector("[class='attach-previous ']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Apply']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("html[data-theme]")).
+                getAttribute("data-theme"), "dark");
+    }
+
+
+    @Test
+    public void testThemesDefault() {
+        getDriver().findElement(By.cssSelector("[href='/manage']")).click();
+        getDriver().findElement(By.cssSelector("[href='appearance']")).click();
+        getDriver().findElement(By.xpath("//label[@for='radio-block-2']")).click();
+
+        getDriver().findElement(By.cssSelector("[class='attach-previous ']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Apply']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("html[data-theme]")).
+                getAttribute("data-theme"), "none");
+    }
+
+
+    @Test
+    public void testThemesSystem() {
+        getDriver().findElement(By.cssSelector("[href='/manage']")).click();
+        getDriver().findElement(By.cssSelector("[href='appearance']")).click();
+        getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
+
+        getDriver().findElement(By.cssSelector("[class='attach-previous ']")).click();
+        getDriver().findElement(By.xpath("//button[@name='Apply']")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.cssSelector("html[data-theme]")).
+                getAttribute("data-theme").contains("system"));
+    }
 }
