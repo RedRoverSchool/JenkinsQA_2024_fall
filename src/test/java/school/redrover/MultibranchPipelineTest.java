c33c4a683d5875921afaa6767d0b1a090686baed
SavinovMA <savinov.norton@yandex.ru>
2024-12-11 22:28:08 +0300

    FIX testSelectingTriggersScanPeriodFromConfigPage ERR link: https://github.com/RedRoverSchool/JenkinsQA_2024_fall/issues/1270

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.page.home.HomePage;
 import school.redrover.runner.BaseTest;
