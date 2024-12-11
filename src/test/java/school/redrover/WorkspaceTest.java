3daf385cd14f561f175de5009e094257fd65a8d6
NikitaS-95 <nserov95@gmail.com>
2024-12-11 10:07:30 +0500

    Refactor to POM
    https://github.com/orgs/RedRoverSchool/projects/3/views/1?filterQuery=luts&pane=issue&itemId=90485258

--- src/test/java/school/redrover/WorkspaceTest.java
+++ src/test/java/school/redrover/WorkspaceTest.java
@@ -1,97 +1,39 @@
 package school.redrover;
 
-import java.time.Duration;
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import school.redrover.page.home.HomePage;
 import school.redrover.runner.BaseTest;
 
 public class WorkspaceTest extends BaseTest {
 
-    @Ignore
-    @BeforeMethod
-    public void testCreateWorkspace() throws InterruptedException {
+    final private String PROJECT_NAME = "TestJobWorkspace";
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-
-        getDriver().findElement(By.xpath("//*[@id='tasks']/div[1]/span/a")).click();
-
-        WebElement jobNameField = getDriver().findElement(By.xpath("//*[@id='name']"));
-        jobNameField.sendKeys("TestJobWorkspace");
-
-        WebElement freestyleProjectOption = getDriver().findElement(
-                By.xpath("//*[@id='j-add-item-type-standalone-projects']/ul/li[1]"));
-        freestyleProjectOption.click();
-
-        WebElement okButton = getDriver().findElement(By.id("ok-button"));
-        okButton.click();
-
-        WebElement saveButton = getDriver().findElement(
-                By.xpath("//*[@id='bottom-sticker']/div/button[1]"));
-        saveButton.click();
-
-        WebElement build1 = getDriver().findElement(By.xpath("//*[@id='tasks']/div[4]/span/a"));
-        build1.click();
-        Thread.sleep(3000);
-
-        WebElement build2 = getDriver().findElement(By.xpath("//*[@id='tasks']/div[4]/span/a"));
-        build2.click();
-        Thread.sleep(3000);
-
-        WebElement workspace = getDriver().findElement(By.xpath("//*[@id='tasks']/div[3]/span/a"));
-        workspace.click();
-
-        WebElement workspaceTitle = getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[5]"));
-        assert (workspaceTitle.isDisplayed());
-
-        System.out.println("Workspace of TestJobWorkspace on Built-In Node");
-
-    }
-
-    @Ignore
     @Test
-    public void testWorkspaceViewable() {
+    public void testWorkspaceIsOpened() {
+        String workspace = new HomePage(getDriver())
+                .clickNewItem()
+                .enterItemName(PROJECT_NAME)
+                .selectFreestyleProject()
+                .selectFreestyleProjectAndClickOk()
+                .clickSaveButton()
+                .clickBuildNowSidebar()
+                .clickWorkspaceSidebar()
+                .getBreadCrumb();
 
-        List<WebElement> directories = getDriver().findElements(By.cssSelector("#breadcrumbs > li:nth-child(5)"));
-        List<WebElement> files = getDriver().findElements(
-                By.cssSelector("#tasks > div:nth-child(3) > span > a > span.task-link-text"));
-
-        Assert.assertTrue(directories.size() > 0, "Directories are displayed in the workspace.");
-        Assert.assertTrue(files.size() > 0, "Files are displayed in the workspace.");
+        Assert.assertEquals(workspace, "Workspace of " + PROJECT_NAME + " on Built-In Node");
     }
 
-    @Ignore
-    @Test
-    public void testBuildNavigation() {
+    @Test(dependsOnMethods = "testWorkspaceIsOpened")
+    public void testLastBuildIsOpened() {
 
-        WebElement buildTwo = getDriver().findElement(By.cssSelector("#buildHistory tr:nth-child(2) a"));
-        buildTwo.click();
+        String secondBuild = new HomePage(getDriver())
+                .openFreestyleProject(PROJECT_NAME)
+                .clickBuildNowSidebar()
+                .clickWorkspaceSidebar()
+                .clickOnSuccessBuildIconForLastBuild()
+                .getBreadCrumb();
 
-        Assert.assertEquals(
-                getDriver().findElement(By.cssSelector("#breadcrumbs > li:nth-child(5)")).getText(), "#2");
-    }
-
-    @Ignore
-    @Test
-    public void testWorkspaceNavigation() {
-
-        WebElement buildTwo = getDriver().findElement(By.cssSelector("#buildHistory tr:nth-child(2) a"));
-        buildTwo.click();
-        WebElement breadCrumbs = getDriver().findElement(By.xpath("//*[@id='breadcrumbs']/li[3]/a"));
-        breadCrumbs.click();
-
-        Assert.assertEquals(getDriver().findElement(
-                By.xpath("//*[@id='breadcrumbs']/li[3]/a")).getText(), "TestJobWorkspace");
-    }
-
-    @Ignore
-    @AfterMethod
-    public void tearDown() {
-
-        getDriver().quit();
+        Assert.assertEquals(secondBuild, "#2");
     }
 }
