fd59288eea0af406e94348e90207953007dac61a
YevgenyN <evgenii.tst@gmail.com>
2024-12-13 23:53:32 +0300

    Refactoring

--- src/test/java/school/redrover/page/home/HomePage.java
+++ src/test/java/school/redrover/page/home/HomePage.java
@@ -445,7 +445,7 @@ public class HomePage extends BasePage {
     }
 
     public HomePage addDescription(String description) {
-        getWait10().until(ExpectedConditions.visibilityOf(descriptionTextarea)).sendKeys(description);
+        TestUtils.pasteTextWithJavaScript(getDriver(), descriptionTextarea, description);
 
         return this;
     }
