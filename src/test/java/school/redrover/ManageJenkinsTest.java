d489c4f235361e2fc50efa6bdae044911a604b21
AllaBird <146133797+AllaBird@users.noreply.github.com>
2024-12-15 01:23:49 -0500

    Fix Description problem (#1562)
    * Fix Description problem
    
    * change base to check all tests

--- src/test/java/school/redrover/ManageJenkinsTest.java
+++ src/test/java/school/redrover/ManageJenkinsTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -219,28 +218,6 @@ public class ManageJenkinsTest extends BaseTest {
         Assert.assertEquals(currentUrl, ProjectUtils.getUrl() + "manage/configure",
                 "Current URL does not meet requirement. ");
         Assert.assertTrue(getSystemText.contains("system"), "Current page does not contain the word 'System'");
-    }
-
-    @Test
-    public void testSystemMessagePreview() {
-        getDriver().findElement(By.xpath("//a[contains(@href, '/manage')]")).click();
-        getWait10().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href, 'configureSecurity')]"))).click();
-        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//option[text() = 'Plain text']/..")));
-
-        new Select(getDriver().findElement(By.xpath("//option[text() = 'Plain text']/..")))
-                .selectByVisibleText("Safe HTML");
-
-        getDriver().findElement(By.name("Apply")).click();
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
-
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[contains(@href, 'configure')])[1]"))).click();
-
-        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@name = 'system_message']/.."))).click();
-        getDriver().findElement(By.xpath("//div[@class = 'CodeMirror']/div/textarea")).sendKeys("<h2>Hello</h2>");
-
-        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@previewendpoint = '/markupFormatter/previewDescription']"))).click();
-
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class = 'textarea-preview']/h2")).getText(), "Hello");
     }
 
     @Test
