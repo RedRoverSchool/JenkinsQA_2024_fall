9e819aef60f2fb0966c5eef6c21bce6e3b6a417d
AnnaLimutina <zip1061@mail.ru .>
2024-12-15 16:46:07 -0600

    New tests in 'BuildHistoryTest':

--- src/test/java/school/redrover/page/home/BuildHistoryPage.java
+++ src/test/java/school/redrover/page/home/BuildHistoryPage.java
@@ -3,6 +3,7 @@ package school.redrover.page.home;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import school.redrover.page.ConsoleOutputPage;
 import school.redrover.page.base.BasePage;
 
 import java.util.List;
@@ -17,6 +18,12 @@ public class BuildHistoryPage extends BasePage {
 
     @FindBy(xpath = "//div[@class='jenkins-table__cell__button-wrapper']/*[@id]")
     private WebElement buildStatusSign;
+
+    @FindBy(xpath = "//a[@class='jenkins-table__link jenkins-table__badge model-link inside']")
+    private WebElement buildDisplayName;
+
+    @FindBy(xpath = "//span[text()='Console output']/..")
+    private WebElement consoleOutputButton;
 
     public BuildHistoryPage(WebDriver driver) {
         super(driver);
@@ -33,6 +40,16 @@ public class BuildHistoryPage extends BasePage {
 
     public String getBuildStatusSignColor() {
         return buildStatusSign.getAttribute("id");
+    }
+
+    public String getBuildDisplayName() {
+        return buildDisplayName.getText();
+    }
+
+    public ConsoleOutputPage clickConsoleOutput() {
+        consoleOutputButton.click();
+
+        return new ConsoleOutputPage(getDriver());
     }
 
 }
