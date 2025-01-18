ad3103f116f614e91e558264ef229211826a240e
JulieVPetrova <m.smn103@gmail.com>
2025-01-18 14:08:24 +0300

    add allure annotations

--- src/test/java/school/redrover/PluginTest.java
+++ src/test/java/school/redrover/PluginTest.java
@@ -1,13 +1,20 @@
 package school.redrover;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.page.home.HomePage;
 import school.redrover.page.systemConfiguration.PluginsPage;
 import school.redrover.runner.BaseTest;
 
+@Epic("09 Manage Jenkins")
 public class PluginTest extends BaseTest {
 
     @Test
+    @Story("US_09.006 Plugin")
+    @Description("TC_09.006.01 Check number of update plugin")
     public void testNumberOfUpdatePlugin() {
 
         int indicatorCount = new HomePage(getDriver())
@@ -18,11 +25,14 @@ public class PluginTest extends BaseTest {
         int countPluginsFromUpdateTable = new PluginsPage(getDriver())
                 .getPluginsCountFromUpdateTable();
 
+        Allure.step("Expected result: Check number of update plugin");
         Assert.assertEquals(indicatorCount, countPluginsFromUpdateTable);
 
     }
 
     @Test(dependsOnMethods = "testNumberOfUpdatePlugin")
+    @Story("US_09.006 Plugin")
+    @Description("TC_09.006.02 Check number of all update plugin")
     public void testNumberOfAllUpdatePlugin() {
 
         int countAvailablePlugins = new HomePage(getDriver())
@@ -30,11 +40,14 @@ public class PluginTest extends BaseTest {
                 .openPluginsPage()
                 .getCountAvailablePlugins();
 
+        Allure.step("Expected result: Check number of all update plugin");
         Assert.assertEquals(countAvailablePlugins, 50);
 
     }
 
     @Test (dependsOnMethods = "testNumberOfAllUpdatePlugin")
+    @Story("US_09.006 Plugin")
+    @Description("TC_09.006.03 Search plugin via tag")
     public void testSearchPluginViaTag() {
 
         int countOfPluginsFound = new HomePage(getDriver())
@@ -43,6 +56,7 @@ public class PluginTest extends BaseTest {
                 .searchInstalledPlugin("Theme Manager")
                 .getCountOfPluginsFound();
 
+        Allure.step("Expected result: Search plugin via tag");
         Assert.assertEquals(countOfPluginsFound, 1);
 
     }
