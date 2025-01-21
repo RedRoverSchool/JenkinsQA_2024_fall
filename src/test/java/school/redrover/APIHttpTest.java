e0bdaa3a5e268709e5391769d4ce428c115ef1d4
AllaBird <allavorobieva@yahoo.com>
2025-01-21 01:01:36 -0500

    Add Create Pipeline with XML config

--- src/test/java/school/redrover/APIHttpTest.java
+++ src/test/java/school/redrover/APIHttpTest.java
@@ -1,41 +1,70 @@
 package school.redrover;
 
 import com.google.common.net.HttpHeaders;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import io.qameta.allure.Epic;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseAPIHttpTest;
 import school.redrover.runner.HttpLogger;
 import school.redrover.runner.ProjectUtils;
+import school.redrover.runner.TestUtils;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Epic("Http API  Requests")
 public class APIHttpTest extends BaseAPIHttpTest {
 
     private static final String PIPELINE_NAME = "Pipeline";
+    private static final String PIPELINE_NAME_BY_XML_CREATED = "PipelineXML";
+
+    private List<String> getAllProjectNamesFromJsonResponseList() throws IOException {
+        HttpGet httpGet = new HttpGet(ProjectUtils.getUrl() + "api/json?pretty=true");
+        httpGet.addHeader("Authorization", getBasicAuthWithToken());
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(httpGet)) {
+
+            Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+
+            String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            JsonObject jsonResponse = JsonParser.parseString(responseBody).getAsJsonObject();
+
+            JsonArray jobs = jsonResponse.getAsJsonArray("jobs");
+
+            List<String> projectNames = new ArrayList<>();
+
+            for (JsonElement jobElement : jobs) {
+                JsonObject jobObject = jobElement.getAsJsonObject();
+                String projectName = jobObject.get("name").getAsString();
+                projectNames.add(projectName);
+            }
+
+            return projectNames;
+        }
+    }
 
     @Test
     public void testCreatePipeline() throws IOException {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", PIPELINE_NAME);
-        params.put("mode", "org.jenkinsci.plugins.workflow.job.WorkflowJob");
-
-        List<NameValuePair> nameValuePairs = params.entrySet()
-                .stream()
-                .map(entry -> new BasicNameValuePair(entry.getKey(), entry.getValue()))
-                .collect(Collectors.toList());
+        final List<NameValuePair> nameValuePairs = new ArrayList<>();
+        nameValuePairs.add(new BasicNameValuePair("name", PIPELINE_NAME));
+        nameValuePairs.add(new BasicNameValuePair("mode", "org.jenkinsci.plugins.workflow.job.WorkflowJob"));
 
         HttpPost httpPost = new HttpPost(ProjectUtils.getUrl() + "view/all/createItem/");
         httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
@@ -48,7 +77,34 @@ public class APIHttpTest extends BaseAPIHttpTest {
             HttpLogger.logRequestAndResponse(httpPost, response);
 
             Assert.assertEquals(response.getStatusLine().getStatusCode(), 302);
+            Assert.assertListContainsObject(
+                    getAllProjectNamesFromJsonResponseList(),
+                    PIPELINE_NAME,
+                    "The project is not created");
+
+        }
+    }
+
+    @Test
+    public void testCreatePipelineXML() throws IOException {
+        String queryString = "name=" + TestUtils.encodeParam(PIPELINE_NAME_BY_XML_CREATED);
+
+        HttpPost httpPost = new HttpPost(ProjectUtils.getUrl() + "view/all/createItem?" + queryString);
+        httpPost.setEntity(new StringEntity(TestUtils.readFileFromResources("create-empty-pipeline-project.xml")));
+
+        httpPost.addHeader(HttpHeaders.CONTENT_TYPE, "application/xml");
+        httpPost.addHeader(HttpHeaders.AUTHORIZATION, getBasicAuthWithToken());
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(httpPost)) {
+
+            HttpLogger.logRequestAndResponse(httpPost, response);
+
+            Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+            Assert.assertListContainsObject(
+                    getAllProjectNamesFromJsonResponseList(),
+                    PIPELINE_NAME_BY_XML_CREATED,
+                    "The project is not created");
         }
     }
 }
-
