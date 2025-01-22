780ed7db559b0252b1ad01e0054886aa85d612d8
AllaBird <allavorobieva@yahoo.com>
2025-01-21 19:31:58 -0500

    Add addDescription test

--- src/test/java/school/redrover/runner/HttpLogger.java
+++ src/test/java/school/redrover/runner/HttpLogger.java
@@ -3,6 +3,7 @@ package school.redrover.runner;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.qameta.allure.Allure;
@@ -14,13 +15,13 @@ public class HttpLogger {
 
     private static final Logger logger = LoggerFactory.getLogger(HttpLogger.class);
 
-    public static void logRequestAndResponse(HttpPost httpPost, CloseableHttpResponse response) throws IOException {
-        String requestUri = httpPost.getURI().toString();
+    public static void logRequestAndResponse(HttpRequestBase httpRequest, CloseableHttpResponse response) throws IOException {
+        String requestUri = httpRequest.getURI().toString();
 
         logger.info("Sending request to: {}", requestUri);
         Allure.addAttachment("Request URI", requestUri);
 
-        for (Header header : httpPost.getAllHeaders()) {
+        for (Header header : httpRequest.getAllHeaders()) {
             if ("Authorization".equalsIgnoreCase(header.getName())) {
                 String maskedAuthorization = "*".repeat(header.getValue().length() - 5) + header.getValue().substring(header.getValue().length() - 5);
                 logger.info("{}: {}", header.getName(), maskedAuthorization);
@@ -30,10 +31,13 @@ public class HttpLogger {
                 Allure.addAttachment(header.getName(), header.getValue());
             }
         }
-        if (httpPost.getEntity() != null) {
-            String entityContent = EntityUtils.toString(httpPost.getEntity());
-            logger.info("Request body: {}", entityContent);
-            Allure.addAttachment("Request Body", entityContent);
+
+        if (httpRequest instanceof HttpPost httpPost) {
+            if (httpPost.getEntity() != null) {
+                String entityContent = EntityUtils.toString(httpPost.getEntity());
+                logger.info("Request body: {}", entityContent);
+                Allure.addAttachment("Request Body", entityContent);
+            }
         }
 
         logger.info("Response status: {}", response.getStatusLine());
