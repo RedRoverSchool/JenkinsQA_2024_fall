317f891655d8af21c0dcbaa7696e965aae8eba95
pakhomova_ev <pakhomova.e@gmail.com>
2025-01-25 12:47:28 +0300

    add test Rename folder

--- src/test/java/school/redrover/APIHTTPFolderTest.java
+++ src/test/java/school/redrover/APIHTTPFolderTest.java
@@ -27,6 +27,7 @@ public class APIHTTPFolderTest extends BaseAPIHttpTest {
 
     private static final String FOLDER_NAME = "FolderUn";
     private static final String FOLDER_CREATE_MODE = "com.cloudbees.hudson.plugins.folder.Folder";
+    private static final String FOLDER_NEW_NAME = "FolderNewName";
 
 
     private static final class Folder {
@@ -43,12 +44,48 @@ public class APIHTTPFolderTest extends BaseAPIHttpTest {
         private String url;
         private String color;
 
+        public Project(String _class, String url, String color, String  name) {
+            this._class = _class;
+            this.url = url;
+            this.color = color;
+            this.name = name;
+        }
+
+        public String get_class() {
+            return _class;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public String getColor() {
+            return color;
+        }
     }
 
     private static final class Projects {
         private String _class;
         private List<Project> jobs;
+
+        public Projects(String _class, List<Project> jobs) {
+            this._class = _class;
+            this.jobs = jobs;
+        }
+
+        public String get_class() {
+            return _class;
+        }
+
+        public List<Project> getJobs() {
+            return jobs;
+        }
     }
+
     private String getCreateFolderURL() {
         return ProjectUtils.getUrl() + "createItem";
     }
@@ -65,8 +102,15 @@ public class APIHTTPFolderTest extends BaseAPIHttpTest {
     private String getDeleteItemURL() {
         return String.format(ProjectUtils.getUrl() + "job/%s/", APIHTTPFolderTest.FOLDER_NAME);
     }
+
+    private String getRenameItemURL() {
+        return ProjectUtils.getUrl() + String.format("job/%s/confirmRename",FOLDER_NAME);
+    }
     private String getCreateFolderBody() {
         return "name="+ APIHTTPFolderTest.FOLDER_NAME +"&mode=" + APIHTTPFolderTest.FOLDER_CREATE_MODE;
+    }
+    private String getRenameFolderBody(String name) {
+        return "newName=" + name;
     }
 
     @Test
@@ -106,16 +150,54 @@ public class APIHTTPFolderTest extends BaseAPIHttpTest {
         Allure.step("Expected result: description is null");
         Assert.assertNull(folder.description);
 
-        HttpRequest deleteRequest = HttpRequest.newBuilder()
-                .uri(new URI(getDeleteItemURL()))
+//        HttpRequest deleteRequest = HttpRequest.newBuilder()
+//                .uri(new URI(getDeleteItemURL()))
+//                .header(HttpHeaders.AUTHORIZATION, getBasicAuthWithToken())
+//                .DELETE()
+//                .build();
+//
+//        Allure.step("Send DELETE request -> Delete Folder");
+//        HttpResponse<String> deleteResponse = httpClient.send(deleteRequest, HttpResponse.BodyHandlers.ofString());
+//        Allure.step("Expected result: Delete item status code is 204");
+//        Assert.assertEquals(deleteResponse.statusCode(), 204);
+    }
+
+    @Test(dependsOnMethods = "testCreateFolderWithValidName")
+    @Story("Folder")
+    @Description("008 Rename Folder")
+    public void testRenameFolder() throws IOException, InterruptedException, URISyntaxException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI(getRenameItemURL()))
                 .header(HttpHeaders.AUTHORIZATION, getBasicAuthWithToken())
-                .DELETE()
+                .header(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(getRenameFolderBody(FOLDER_NEW_NAME)))
                 .build();
 
-        Allure.step("Send DELETE request -> Delete Folder");
-        HttpResponse<String> deleteResponse = httpClient.send(deleteRequest, HttpResponse.BodyHandlers.ofString());
-        Allure.step("Expected result: Delete item status code is 204");
-        Assert.assertEquals(deleteResponse.statusCode(), 204);
+
+        Allure.step("Send POST request -> Rename folder");
+        HttpResponse<String> postRenameResponse = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+        Assert.assertEquals(postRenameResponse.statusCode(), 302);
+
+        HttpRequest getItemList = HttpRequest.newBuilder()
+                .uri(new URI(getItemListURL()))
+                .header(HttpHeaders.AUTHORIZATION, getBasicAuthWithToken())
+                .GET()
+                .build();
+
+        Allure.step("Send GET request -> Get project list from Dashboard");
+        HttpResponse<String> getItemListResponse = httpClient.send(getItemList, HttpResponse.BodyHandlers.ofString());
+        String body = getItemListResponse.body();
+        Projects projects = new Gson().fromJson(body, Projects.class);
+        boolean findNewProjectNameInList = projects.getJobs().stream()
+                .anyMatch(project -> project.getName().equals(FOLDER_NEW_NAME));
+
+        boolean findOriginProjectNameInList = projects.getJobs().stream()
+                .anyMatch(project -> project.getName().equals(FOLDER_NAME));
+
+        Assert.assertTrue(findNewProjectNameInList, "project name was not found in the list");
+        Assert.assertFalse(findOriginProjectNameInList, "Project name was found in the list");
     }
 }
 
