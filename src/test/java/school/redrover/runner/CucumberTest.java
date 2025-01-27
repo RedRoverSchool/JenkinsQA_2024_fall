88df7c180e1d91282bba28eb47863abd5b05dcce
YevgenyN <evgenii.tst@gmail.com>
2025-01-27 14:36:19 +0300

    Refactoring allure

--- src/test/java/school/redrover/runner/CucumberTest.java
+++ src/test/java/school/redrover/runner/CucumberTest.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/cucumber",
         glue = {"school.redrover.cucumber", "school.redrover.runner"},
-        plugin = {"pretty"},
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         tags = "not @ignore")
 public class CucumberTest extends AbstractTestNGCucumberTests {
 }
