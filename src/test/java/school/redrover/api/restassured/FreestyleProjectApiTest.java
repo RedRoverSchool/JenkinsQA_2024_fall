95f0975d192a01f6c88df15487cb17beadb1e641
Natasha <natashashimkiv@gmail.com>
2025-02-19 16:11:31 +0400

    Added tests

--- src/test/java/school/redrover/api/restassured/FreestyleProjectApiTest.java
+++ src/test/java/school/redrover/api/restassured/FreestyleProjectApiTest.java
@@ -15,8 +15,7 @@ import school.redrover.runner.WireMockStubs;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
-import static school.redrover.runner.TestApiUtils.requestSpec;
-import static school.redrover.runner.TestApiUtils.responseSpec;
+import static school.redrover.runner.TestApiUtils.*;
 import static school.redrover.runner.TestUtils.loadPayload;
 
 @Epic("API")
@@ -43,15 +42,14 @@ public class FreestyleProjectApiTest extends BaseApiTest {
                 .contentType(ContentType.XML)
                 .body(loadPayload("create-empty-freestyle-project.xml"))
                 .when()
-                .post("createItem")
+                .post(getCreateItemPath())
                 .then()
                 .spec(responseSpec(200, 1000L));
 
         Response response = given()
                 .spec(requestSpec())
-                .basePath("job/%s".formatted(PROJECT_NAME))
                 .when()
-                .get("api/json")
+                .get(getItemByNamePath(PROJECT_NAME))
                 .then()
                 .spec(responseSpec(200, 500L))
                 .body(matchesJsonSchema(TestUtils.loadSchema("freestyle-project-schema.json")))
@@ -65,23 +63,24 @@ public class FreestyleProjectApiTest extends BaseApiTest {
         Assert.assertNull(freestyleAndPipelineResponse.getDescription());
         Assert.assertEquals(freestyleAndPipelineResponse.get_class(), "hudson.model.FreeStyleProject");
     }
-    @Test(dependsOnMethods = "testCreateFreestyleProjectAndGetByName")
+    @Test
     @Description("01.002.01 Rename FreestyleProject with correct name")
     public void testRename() {
+        testCreateFreestyleProjectAndGetByName();
 
         given()
                 .spec(requestSpec())
                 .contentType("application/x-www-form-urlencoded")
                 .queryParam("newName", RENAMED_FREESTYLE_PROJECT)
                 .when()
-                .post("job/%s/doRename".formatted(PROJECT_NAME))
+                .post(getRenameItemPath(PROJECT_NAME))
                 .then()
                 .spec(responseSpec(302, 1000L));
 
         Response response = given()
                 .spec(requestSpec())
                 .when()
-                .get("job/%s/api/json".formatted(RENAMED_FREESTYLE_PROJECT))
+                .get(getItemByNamePath(RENAMED_FREESTYLE_PROJECT))
                 .then()
                 .spec(responseSpec(200, 500L))
                 .extract().response();
@@ -101,14 +100,14 @@ public class FreestyleProjectApiTest extends BaseApiTest {
                 .spec(requestSpec())
                 .queryParam("description", PROJECT_DESCRIPTION)
                 .when()
-                .post("job/%s/submitDescription".formatted(PROJECT_NAME))
+                .post(getAddDescriptionToCreatedItemPath(PROJECT_NAME))
                 .then()
                 .spec(responseSpec(302, 500L));
 
         Response response = given()
                 .spec(requestSpec())
                 .when()
-                .get("job/%s/api/json".formatted(PROJECT_NAME))
+                .get(getItemByNamePath(PROJECT_NAME))
                 .then()
                 .spec(responseSpec(200, 500L))
                 .extract().response();
@@ -126,14 +125,14 @@ public class FreestyleProjectApiTest extends BaseApiTest {
         given()
                 .spec(requestSpec())
                 .when()
-                .delete("job/%s/".formatted(PROJECT_NAME))
+                .delete(getDeleteItemPath(PROJECT_NAME))
                 .then()
                 .spec(responseSpec(204, 500L));
 
         given()
                 .spec(requestSpec())
                 .when()
-                .get("job/%s/api/json".formatted(PROJECT_NAME))
+                .get(getItemByNamePath(PROJECT_NAME))
                 .then()
                 .spec(responseSpec(404, 500L))
                 .extract().response();
