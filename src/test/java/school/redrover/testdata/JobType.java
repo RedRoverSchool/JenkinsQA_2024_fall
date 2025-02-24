367f773e8af182e4977e7c7111e8d11c79916cc3
Elena Pakhomova <140397499+pakhomova-ev@users.noreply.github.com>
2025-02-24 17:10:12 +0300

    RF: some Folder tests  used jobController (#1795)
    * add FOLDER to enum
    * RF: testCreateFolderWithValidNameXml(), testCreateFolderWithUnsafeCharacter(), testCreateFolderWithEmptyName()

--- src/test/java/school/redrover/testdata/JobType.java
+++ src/test/java/school/redrover/testdata/JobType.java
@@ -1,8 +1,11 @@
 package school.redrover.testdata;
 
 import lombok.Getter;
+import school.redrover.models.job.Folder;
 import school.redrover.models.job.Freestyle;
 import school.redrover.models.job.Pipeline;
+
+import java.util.List;
 
 @Getter
 public enum JobType {
@@ -31,6 +34,30 @@ public enum JobType {
             .builders("")
             .publishers("")
             .buildWrappers("")
+            .build()),
+    FOLDER(Folder.builder()
+            .description("")
+            .properties("")
+            .folderViews(Folder.FolderViews.builder()
+                    .className("com.cloudbees.hudson.plugins.folder.views.DefaultFolderViewHolder")
+                    .views(Folder.FolderViews.Views.builder()
+                            .allViews(List.of(Folder.FolderViews.Views.AllView.builder()
+                                    .owner(Folder.FolderViews.Views.AllView.Owner.builder()
+                                            .className("com.cloudbees.hudson.plugins.folder.Folder")
+                                            .reference("../../../..").build())
+                                    .name("ALL")
+                                    .filterExecutors(false)
+                                    .filterQueue(false)
+                                    .properties(Folder.FolderViews.Views.AllView.Properties.builder()
+                                              .className("hudson.model.View$PropertyList").build())
+                                    .build()))
+                            .build())
+                    .tabBar(Folder.FolderViews.TabBar.builder()
+                            .className("hudson.views.DefaultViewsTabBar").build())
+                    .build())
+            .healthMetrics("")
+            .icon(Folder.Icon.builder()
+                    .className("com.cloudbees.hudson.plugins.folder.icons.StockFolderIcon").build())
             .build());
 
     private final Object projectConfig;
