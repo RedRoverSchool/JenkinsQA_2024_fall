3843db4b328acb9117f2c009e41183d7f2541267
AllaBird <allavorobieva@yahoo.com>
2025-02-24 22:16:44 -0500

    Refactor JobType

--- src/test/java/school/redrover/testdata/JobType.java
+++ src/test/java/school/redrover/testdata/JobType.java
@@ -1,69 +1,16 @@
 package school.redrover.testdata;
 
 import lombok.Getter;
-import school.redrover.models.job.Folder;
-import school.redrover.models.job.Freestyle;
-import school.redrover.models.job.Pipeline;
-
-import java.util.List;
 
 @Getter
 public enum JobType {
-    PIPELINE(Pipeline.builder()
-            .description("")
-            .keepDependencies(false)
-            .properties("")
-            .definition(Pipeline.Definition.builder()
-                    .className("org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition")
-                    .script("")
-                    .sandbox(true)
-                    .build())
-            .triggers("")
-            .disabled(false)
-            .build()),
-    FREESTYLE(Freestyle.builder()
-            .keepDependencies(false)
-            .properties("")
-            .scm("hudson.scm.NullSCM")
-            .canRoam(false)
-            .disabled(false)
-            .blockBuildWhenDownstreamBuilding(false)
-            .blockBuildWhenUpstreamBuilding(false)
-            .triggers("")
-            .concurrentBuild(false)
-            .builders("")
-            .publishers("")
-            .buildWrappers("")
-            .build()),
-    FOLDER(Folder.builder()
-            .description("")
-            .properties("")
-            .folderViews(Folder.FolderViews.builder()
-                    .className("com.cloudbees.hudson.plugins.folder.views.DefaultFolderViewHolder")
-                    .views(Folder.FolderViews.Views.builder()
-                            .allViews(List.of(Folder.FolderViews.Views.AllView.builder()
-                                    .owner(Folder.FolderViews.Views.AllView.Owner.builder()
-                                            .className("com.cloudbees.hudson.plugins.folder.Folder")
-                                            .reference("../../../..").build())
-                                    .name("ALL")
-                                    .filterExecutors(false)
-                                    .filterQueue(false)
-                                    .properties(Folder.FolderViews.Views.AllView.Properties.builder()
-                                              .className("hudson.model.View$PropertyList").build())
-                                    .build()))
-                            .build())
-                    .tabBar(Folder.FolderViews.TabBar.builder()
-                            .className("hudson.views.DefaultViewsTabBar").build())
-                    .build())
-            .healthMetrics("")
-            .icon(Folder.Icon.builder()
-                    .className("com.cloudbees.hudson.plugins.folder.icons.StockFolderIcon").build())
-            .build());
+    PIPELINE(TestData.PIPELINE),
+    FREESTYLE(TestData.FREESTYLE),
+    FOLDER(TestData.FOLDER);
 
     private final Object projectConfig;
 
     JobType(Object projectConfig) {
         this.projectConfig = projectConfig;
     }
-
 }
--- src/test/java/school/redrover/testdata/TestData.java
+++ src/test/java/school/redrover/testdata/TestData.java
@@ -1,7 +1,11 @@
 package school.redrover.testdata;
 
+import school.redrover.models.job.Folder;
+import school.redrover.models.job.Freestyle;
+import school.redrover.models.job.Pipeline;
 import school.redrover.models.user.User;
 
+import java.util.List;
 import java.util.UUID;
 
 public class TestData {
@@ -13,4 +17,56 @@ public class TestData {
             .password("Pass@" + UUID.randomUUID().toString().substring(0, 6))
             .email(USER_NAME + "@example.com").build();
 
+    public static final Pipeline PIPELINE = Pipeline.builder()
+            .description("")
+            .keepDependencies(false)
+            .properties("")
+            .definition(Pipeline.Definition.builder()
+                    .className("org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition")
+                    .script("")
+                    .sandbox(true)
+                    .build())
+            .triggers("")
+            .disabled(false)
+            .build();
+
+    public static final Freestyle FREESTYLE = Freestyle.builder()
+            .keepDependencies(false)
+            .properties("")
+            .scm("hudson.scm.NullSCM")
+            .canRoam(false)
+            .disabled(false)
+            .blockBuildWhenDownstreamBuilding(false)
+            .blockBuildWhenUpstreamBuilding(false)
+            .triggers("")
+            .concurrentBuild(false)
+            .builders("")
+            .publishers("")
+            .buildWrappers("")
+            .build();
+
+    public static final Folder FOLDER = Folder.builder()
+            .description("")
+            .properties("")
+            .folderViews(Folder.FolderViews.builder()
+                    .className("com.cloudbees.hudson.plugins.folder.views.DefaultFolderViewHolder")
+                    .views(Folder.FolderViews.Views.builder()
+                            .allViews(List.of(Folder.FolderViews.Views.AllView.builder()
+                                    .owner(Folder.FolderViews.Views.AllView.Owner.builder()
+                                            .className("com.cloudbees.hudson.plugins.folder.Folder")
+                                            .reference("../../../..").build())
+                                    .name("ALL")
+                                    .filterExecutors(false)
+                                    .filterQueue(false)
+                                    .properties(Folder.FolderViews.Views.AllView.Properties.builder()
+                                            .className("hudson.model.View$PropertyList").build())
+                                    .build()))
+                            .build())
+                    .tabBar(Folder.FolderViews.TabBar.builder()
+                            .className("hudson.views.DefaultViewsTabBar").build())
+                    .build())
+            .healthMetrics("")
+            .icon(Folder.Icon.builder()
+                    .className("com.cloudbees.hudson.plugins.folder.icons.StockFolderIcon").build())
+            .build();
 }
