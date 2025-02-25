e6cd581e8efc3da3298fc5ef30fc9e5098d3c5d0
kas-113 <allakravtsova.113@gmail.com>
2025-02-26 01:18:24 +0300

    refactor testDeleteMultibranchPipeline in MultibranchPipelineApiTest

--- src/test/java/school/redrover/api/restassured/MultibranchPipelineApiTest.java
+++ src/test/java/school/redrover/api/restassured/MultibranchPipelineApiTest.java
@@ -3,18 +3,12 @@ package school.redrover.api.restassured;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.controllers.JobController;
-import school.redrover.models.ProjectResponse;
 import school.redrover.runner.BaseApiTest;
 import school.redrover.testdata.JobTestData;
-
-import static io.restassured.RestAssured.given;
-import static school.redrover.runner.TestApiUtils.requestSpec;
-import static school.redrover.runner.TestApiUtils.responseSpec;
 
 @Epic("API")
 public class MultibranchPipelineApiTest extends BaseApiTest {
@@ -45,22 +39,15 @@ public class MultibranchPipelineApiTest extends BaseApiTest {
         Assert.assertTrue(response.time() <= 500);
     }
 
-    @Test(dependsOnMethods = "testRenameWithValidName")
+    @Test
     @Feature("Multibranch Pipeline")
     @Description("05.005.01 Delete Multibranch pipeline")
     public void testDeleteMultibranchPipeline() {
-        given()
-                .spec(requestSpec())
-                .when()
-                .delete("job/%s/".formatted(MULTIBRANCH_PIPELINE_RENAMED))
-                .then()
-                .spec(responseSpec(204, 500L));
+        jobController.createJob(JobTestData.getDefaultMultibranchPipeline(), MULTIBRANCH_PIPELINE_NAME);
 
-        given()
-                .spec(requestSpec())
-                .when()
-                .get("job/%s/api/json".formatted(MULTIBRANCH_PIPELINE_RENAMED))
-                .then()
-                .spec(responseSpec(404, 500L));
+        Response response = jobController.deleteJob(MULTIBRANCH_PIPELINE_NAME);
+
+        Assert.assertEquals(response.statusCode(), 204);
+        Assert.assertTrue(response.time() <= 500);
     }
 }
