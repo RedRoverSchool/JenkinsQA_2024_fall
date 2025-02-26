e04eb8bc662bbc04c5fadd0349a9b0e9b4bd5d74
Natasha <natashashimkiv@gmail.com>
2025-02-26 18:57:38 +0400

    Added MultiConfiguration at models.Job

---
+++ src/test/java/school/redrover/models/job/MultiConfiguration.java
@@ -0,0 +1,77 @@
+package school.redrover.models.job;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JacksonXmlRootElement(localName = "matrix-project")
+public class MultiConfiguration implements JobType {
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String plugin;
+
+    @JacksonXmlProperty(localName = "description")
+    private String description;
+
+    @JacksonXmlProperty(localName = "keepDependencies")
+    private boolean keepDependencies;
+
+    @JacksonXmlProperty(localName = "properties")
+    private String properties;
+
+    @JacksonXmlProperty(localName = "scm")
+    private Scm scm;
+
+    @JacksonXmlProperty(localName = "canRoam")
+    private boolean canRoam;
+
+    @JacksonXmlProperty(localName = "disabled")
+    private boolean disabled;
+
+    @JacksonXmlProperty(localName = "blockBuildWhenDownstreamBuilding")
+    private boolean blockBuildWhenDownstreamBuilding;
+
+    @JacksonXmlProperty(localName = "blockBuildWhenUpstreamBuilding")
+    private boolean blockBuildWhenUpstreamBuilding;
+
+    @JacksonXmlProperty(localName = "triggers")
+    private String triggers;
+
+    @JacksonXmlProperty(localName = "concurrentBuild")
+    private boolean concurrentBuild;
+
+    @JacksonXmlProperty(localName = "axes")
+    private String axes;
+
+    @JacksonXmlProperty(localName = "builders")
+    private String builders;
+
+    @JacksonXmlProperty(localName = "publishers")
+    private String publishers;
+
+    @JacksonXmlProperty(localName = "buildWrappers")
+    private String buildWrappers;
+
+    @JacksonXmlProperty(localName = "executionStrategy")
+    private ExecutionStrategy executionStrategy;
+
+    @Builder
+    public static class Scm {
+
+        @JacksonXmlProperty(isAttribute = true, localName = "class")
+        private String className;
+    }
+
+    @Builder
+    public static class ExecutionStrategy {
+
+        @JacksonXmlProperty(isAttribute = true, localName = "class")
+        private String className;
+
+        @JacksonXmlProperty(localName = "runSequentially")
+        private boolean runSequentially;
+    }
+}
